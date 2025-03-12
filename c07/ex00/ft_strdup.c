10d8c6651bf47da81be310bd269dd532208a5295
Alice Casper <acasper@student.42berlin.de>
2025-03-12 20:51:38 +0100

    ex00 seems to work

--- c07/ex00/ft_strdup.c
+++ c07/ex00/ft_strdup.c
@@ -6,30 +6,53 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 18:56:14 by acasper           #+#    #+#             */
-/*   Updated: 2025/03/11 22:45:48 by acasper          ###   ########.fr       */
+/*   Updated: 2025/03/12 20:51:08 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
+#include <errno.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
 
 char	*ft_strdup(char *src)
 {
-	int	len;
+	char	*dest;
+	int		len;
+	int		i;
 
-	len = 0;
-	while (src[len])
-		len++;
-
-	src[] = (char*)malloc(1 + 1);
-
-	return (src);
+	len = ft_strlen(src);
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dest)
+	{
+		errno = ENOMEM;
+		return (NULL);
+	}
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
-
+/*
 #include <stdio.h>
 int	main(void)
 {
-	char	c;
+	char	*pointer = "miau";
 
-	c = "x";
-	*ft_strdup(c);
+	printf("pointer address of dest[0]: %p\n", ft_strdup(pointer));
+	printf("string of dest: %s\n", ft_strdup(pointer));
+
+	return (0);
 }
-
+*/
