ee84a92c9b5ea0c2b41508fccdb9c7117fd12bcf
Alice Casper <acasper@student.42berlin.de>
2025-03-13 19:13:03 +0100

    one more

---
+++ c08_/ex01/ft_boolean.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 22:35:15 by acasper           #+#    #+#             */
+/*   Updated: 2025/03/12 23:29:05 by acasper          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+# include <unistd.h>
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+# define EVEN(n) ((n) % 2 == 0)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+typedef int	t_bool;
+
+#endif
