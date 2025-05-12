9dc38e4393a43e0ccdc319867d0ea936ed4c6f0f
Your Name <acasper@student.42berlin.de>
2025-05-12 21:09:51 +0200

    started libft

---
+++ libft/isalpha.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   isalpha.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 20:27:10 by acasper           #+#    #+#             */
+/*   Updated: 2025/05/12 20:57:51 by acasper          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+int	isalphaa(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < 65)
+			return (0);
+		else if (str[i] > 90 && str[i] < 97)
+			return (0);
+		else if (str[i] > 122)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	printf("miau: %d\n", isalphaa("miau"));
+	printf("mia0: %d\n", isalphaa("mia0"));
+	printf("123: %d\n", isalphaa("123"));
+	printf("m m: %d\n", isalphaa("m m"));
+	return (0);
+}
