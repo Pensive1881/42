0b92df0162bdc5430b59df1b9a8e114ce265acd4
Alexander Casper <acasper@student.42berlin.de>
2025-05-14 19:17:24 +0200

    added atoi

--- libft/atoi.c
+++ libft/atoi.c
@@ -6,12 +6,33 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 22:20:33 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/12 22:20:51 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/14 19:16:13 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
-int	atoi()
+int	atoi(char str)
 {
-	return (0);
+	int	num;
+	int	neg;
+	int	i;
+
+	i = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	neg = 1;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	num = 0;
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		num *= 10;
+		num += str[i] - '0';
+		i++;
+	}
+	return (num * neg);
 }
