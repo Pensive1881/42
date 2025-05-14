37052687aecdd8c7e412d764fac0416421855fbc
Alexander Casper <acasper@student.42berlin.de>
2025-05-15 00:08:20 +0200

    started strrchr

--- libft/strrchr.c
+++ libft/strrchr.c
@@ -6,12 +6,21 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 22:49:01 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/14 22:50:16 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/15 00:07:49 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
-char	strrstr()
+char	*strrchr(const char *str, int c)
 {
+	int	i;
+
+	if (c == '\0')
+		return ((char*) str);
+	i = 0;
+	while (str[i])
+	{
+
+	}
 	return (0);
 }
