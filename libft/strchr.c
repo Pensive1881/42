be9ac75d051dca3c61853d95bd839fd7f8ebf670
Alexander Casper <acasper@student.42berlin.de>
2025-05-15 00:09:11 +0200

    added strchr

--- libft/strchr.c
+++ libft/strchr.c
@@ -6,12 +6,23 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 22:18:03 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/14 22:42:37 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/14 23:47:52 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
-char	strchr.c(const char *str, int c)
+char	*strchr.c(const char *str, int c)
 {
-	return (0);
+	int 	i;
+
+	if (c == '\0')
+		return ((char*) str);
+	i = 0;
+	while (str[i])
+	}
+		if (str[i] == c)
+			return ((char*) str[i]);
+		i++;
+	}
+	return (NULL);
 }
