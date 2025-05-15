b730d08ce40ebfc2e86c59bcdcc765542f3e4963
Alexander Casper <acasper@student.42berlin.de>
2025-05-15 18:44:06 +0200

    maybe finished bzero

--- libft/bzero.c
+++ libft/bzero.c
@@ -6,12 +6,18 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 22:13:49 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/15 18:35:18 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/15 18:43:18 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 void	bzero(void *str, size_t n)
 {
+	unsigned char *ptr = (unsigned char*) str;
+	while (n > 0)
+	{
+		*ptr = 0;
+		n--;
+	}
 	return (0);
 }
