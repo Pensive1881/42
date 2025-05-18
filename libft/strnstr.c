9c58568286cbf613d167efd6332e5c5c28de9733
Alexander Casper <acasper@student.42berlin.de>
2025-05-19 00:04:52 +0200

    made strnstr

--- libft/strnstr.c
+++ libft/strnstr.c
@@ -6,12 +6,28 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 22:20:08 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/12 22:20:24 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/18 22:06:26 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 char	*strnstr(const char *haystack, const char *needle, size_t len)
 {
-	return (0);
+	int	i;
+	int	j;
+
+	if (!*needle)
+		return ((char*) haystack);
+
+	i = 0;
+	while ()
+	{
+		j = 0;
+		while(needle[j] && hackstack[i + j] && i + j < len && haystack[i + j] == needle[j])
+			j++;
+		if (!needle[j])
+			return ((char*() haystack + i);
+		i++;
+	}
+	return (NULL);
 }
