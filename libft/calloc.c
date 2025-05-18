bcd60d8ce1721ab2a10dc604d8cd2e182e74dc29
Alexander Casper <acasper@student.42berlin.de>
2025-05-19 00:05:41 +0200

    made calloc

--- libft/calloc.c
+++ libft/calloc.c
@@ -6,13 +6,21 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 18:48:19 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/15 18:48:59 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/18 23:21:51 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
-#unclude <stdlib.h>
+#include <stdlib.h>
 
 void	*calloc(size_t count, size_t size)
 {
-	return (0);
+	void	*ptr;
+
+	if(size && count > SIZE_MAX / size)
+		return (NULL);
+	*ptr = malloc(count * size);
+	if (ptr)
+		memset(ptr, 0, count * size);
+
+	return (ptr);
 }
