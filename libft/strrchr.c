2cd9e65a08cba74723cfd8957ad5f7edef7c5e0a
Alexander Casper <acasper@student.42berlin.de>
2025-05-18 20:47:36 +0200

    made half of strrchr

--- libft/strrchr.c
+++ libft/strrchr.c
@@ -6,12 +6,21 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 20:44:19 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/18 20:45:56 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/18 20:47:08 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 char	*strrchr(const char *str, int c)
 {
+	int	i;
+
+	if (c == '\0')
+		return ((char*) str);
+	i = 0;
+	while (str[i])
+	{
+
+	}
 	return (0);
 }
