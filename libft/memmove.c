6170267bf0e7b133cb1f6fca392a9155c38d3824
Alexander Casper <acasper@student.42berlin.de>
2025-05-19 00:02:13 +0200

    memmove

--- libft/memmove.c
+++ libft/memmove.c
@@ -6,12 +6,30 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 22:15:00 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/15 18:56:44 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/18 21:26:52 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 void	*memmove(void *dest, const void *src, size_t n)
 {
-	return (0);
+	unsigned char *d = (unsigned char*) dest;
+	const unsigned char *s = (const unsigned char *) src;
+
+	if (d = s || n = 0)
+		return (dest);
+	if (d < s || d >= s + n)
+	{
+		while (n--)
+			*d++ = *s++;
+	}
+	else
+	{
+		d += n;
+		s += n;
+		while (n--)
+			*(--d) = *(--s);
+			
+	}
+	return (dest);
 }
