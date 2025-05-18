be4765b0c43c11d7bba683706128befffd006dea
Alexander Casper <acasper@student.42berlin.de>
2025-05-19 00:01:17 +0200

    made memcmp

--- libft/memcmp.c
+++ libft/memcmp.c
@@ -6,12 +6,22 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 22:19:39 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/15 19:01:33 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/18 21:49:38 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 int	memcmp(const void *str1, const void *str2, size_t n)
 {
+	const unsigned char *ptr1 = str1;
+	const unsigned char *ptr2 = str2;
+
+	while (n--)
+	{
+		if (*ptr1 != *ptr2)
+			return (ptr1 - ptr2);
+		ptr1++;
+		ptr2++;
+	}
 	return (0);
 }
