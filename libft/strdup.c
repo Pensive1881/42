dd74cf9d2f50e9c9ac691f9dcacf669b459d6485
Alexander Casper <acasper@student.42berlin.de>
2025-05-19 00:06:08 +0200

    strdup

--- libft/strdup.c
+++ libft/strdup.c
@@ -6,13 +6,38 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 18:49:09 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/15 18:49:49 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/18 23:34:11 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 #include <stdlib.h>
 
+size_t	strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
 char	*strdup(const char *str1)
 {
-	return (0);
+	size_t	len;
+	char	*copy;
+	int	i;
+
+	len = strlen(str);
+	*copy = malloc(len + 1);
+	if(!copy)
+		return (NULL);
+	i = 0;
+	while (i <= len)
+	{
+		copy[i] = str[i];
+		i++;
+	}
+
+	return (copy);
 }
