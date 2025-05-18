cb7cc12d4a5326394ad160fa52233673d08a86d6
Alexander Casper <acasper@student.42berlin.de>
2025-05-19 00:00:33 +0200

    made memchr

--- libft/memchr.c
+++ libft/memchr.c
@@ -6,12 +6,20 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 22:19:04 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/15 19:03:03 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/18 21:52:58 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 void	*memchr(const void *str, int c, size_t n)
 {
-	return (0);
+	const unsigned char *ptr = str;
+
+	while (n--)
+	{
+		if (*ptr == (unsigned char) c)
+			return ((void *)ptr);
+		ptr++;
+	}
+	return (NULL);
 }
