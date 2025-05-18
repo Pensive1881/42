bbe43412c1c0f2388ccc25f47139380cf5437884
Alexander Casper <acasper@student.42berlin.de>
2025-05-19 00:03:43 +0200

    made strlcpy

--- libft/strlcpy.c
+++ libft/strlcpy.c
@@ -6,12 +6,34 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 22:15:42 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/12 22:16:09 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/18 23:16:33 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
-size_t	strlcpy(char * restrict dest, const char * restrict src, size_t destsize)
+size_t	strlen(const char *str)
 {
-	return (0);
+	int	i;
+
+	i = 0;
+	while(str[i])
+		i++;
+	return (i);
+}
+
+size_t	strlcpy(char *dest, const char *src, size_t dest_size)
+{
+	int	i;
+
+	i = 0;
+	if (dest_size > 0)
+	{
+		while (src[i] && i < dest_size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (strlen(src));
 }
