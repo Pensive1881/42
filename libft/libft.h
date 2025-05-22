515b9188c837a869df978d16718a0edc5b59797e
Sasha Kasper <acasper@student.42berlin.de>
2025-05-22 14:16:28 +0200

    added include files to header

--- libft/libft.h
+++ libft/libft.h
@@ -6,12 +6,20 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 18:49:23 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/19 19:21:38 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/19 18:51:37 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#inndef FT_H
-#define FT_H
+#ifndef LIBFT_H
+# define LIBFT_H
 
-
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <bsd/string.h>
+# include <ctype.h>
+# include <limits.h>
+# include <stdint.h>
 
 #endif
