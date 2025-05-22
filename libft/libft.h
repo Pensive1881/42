a50f25a611cd29c5f75aee3b308f182804c6bc4f
Sasha Kasper <acasper@student.42berlin.de>
2025-05-22 16:31:24 +0200

    added all part1 functions to libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -22,4 +22,28 @@
 # include <limits.h>
 # include <stdint.h>
 
+int     ft_atoi(const char *str);
+void    ft_bzero(void *str, size_t n);
+void    *ft_calloc(size_t count, size_t size);
+int     ft_isalnum(char c);
+int     ft_isalpha(int c);
+int     ft_isascii(int c);
+int     ft_isdigit(int c);
+int     ft_isprint(int c);
+void    *ft_memchr(const void *s, int c, size_t n);
+int     ft_memcmp(const void *s1, const void *s2, size_t n);
+void    *ft_memcpy(void restricted *dst, const void restricted *src, size_t n);
+void    *ft_memmove(void *dst, const void *src, size_t len);
+void    *ft_memset(void *b, int c, size_t len);
+char    *ft_strchr(const char *s, int c);
+char    *ft_strdup(const char *s1);
+size_t  ft_strlcat(char * restrict dst, const char * restric src, size_t dstsize);
+size_t  ft_strlcpy(char * restrict dst, const char * restric src, size_t dstsize);
+size_t  ft_strlen(const char s);
+int     ft_strncmp(const char *s1, const char *s2, size_t n);
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
+char    *ft_strrchr(const char *s, int c);
+int     ft_tolower(int c);
+int     ft_toupper(int c);
+
 #endif
