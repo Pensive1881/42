83779166853a32ebbc5afbb8cd4c5e5ff3071d49
Alexander Casper <acasper@student.42berlin.de>
2025-05-25 20:54:23 +0200

    created ft_strjoin

--- libft/part2/ft_strjoin.c
+++ libft/part2/ft_strjoin.c
@@ -1,24 +1,59 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fut_strjoin.c                                      :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 23:42:26 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/18 23:43:15 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/25 20:53:38 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+int	counter(const char *s)
 {
-	return (0);
+	int	i;
+
+	i = 0;
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
 }
 
-/*
-int     main()
+char	*ft_strjoin(char const *s1, char const *s2)
 {
+	char	*str;
+	int		len;
+	int		i;
+	int		j;
+
+	len = counter(s1) + counter(s2);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+/*
+#include <stdio.h>
+int     main(void)
+{
+	printf("miau & meow: %s", ft_strjoin("miau", "meow"));
         return (0);
 }
 */
