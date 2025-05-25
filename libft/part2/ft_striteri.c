87038427fa66a2caa5b63d84bab74c2c958ce651
Sasha Kasper <acasper@student.42berlin.de>
2025-05-26 00:15:55 +0200

    completed ft_striteri

--- libft/part2/ft_striteri.c
+++ libft/part2/ft_striteri.c
@@ -13,12 +13,32 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	return (NULL);
+	unsigned int i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
+/*
+#include <stdio.h>
+#include <ctype.h>
+
+void	cap_even(unsigned int i, char *c)
+{
+	if (i % 2 == 0 && *c >= 'a' && *c <= 'z')
+		*c -= 32;
 }
 
-/*
-int     main()
+int     main(void)
 {
-        return (0);
+        char str[] = "miau meow";
+	ft_striteri(str, cap_even);
+	printf("%s\n", str);
+	return (0);
 }
 */
