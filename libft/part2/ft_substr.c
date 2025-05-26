7932d16fae46b159ce074e71f0140d669566ae3b
Alexander Casper <acasper@student.42berlin.de>
2025-05-26 22:34:02 +0200

    completed ft_substr

--- libft/part2/ft_substr.c
+++ libft/part2/ft_substr.c
@@ -6,24 +6,32 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 23:40:30 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/18 23:42:02 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/26 22:29:16 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 //#include "libft.h"
-#include <unistd.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	str;
-	
+	char	*str;
+	int	i;
 
-	return (str)
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	i = 0;
+	while (i <= len)
+	{
+		str[i] = s[start];
+		start++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
-
+/*
 #include <stdio.h>
 int     main(void)
 {
-	printf("miau meow, 5, 8: %p\n", ft_substr("miau meow", 5, 8));
+	printf("miau meow, 5, 4: %s\n", ft_substr("miau meow", 5, 4));
         return (0);
 }
-
+*/
