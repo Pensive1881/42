024830e98ddc876fa7d53df0aa2642bb09135cea
Alexander Casper <acasper@student.42berlin.de>
2025-05-26 22:55:07 +0200

    did half of ft_strtrim

--- libft/part2/ft_strtrim.c
+++ libft/part2/ft_strtrim.c
@@ -6,19 +6,39 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 23:43:57 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/26 22:02:19 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/26 22:54:02 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
+//#include "libft.h"
+#include <unistd.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	return (0);
+	char	*str;
+	int	i;
+	int	j;
+	
+	str = (char *)malloc(sizeof(char) * ());
+	i = 0;
+	j = 0;
+	while (set[i])
+	{
+		while (s1[j] != set[i])
+		{
+			str[j] = s1[j]
+			j++;
+		}
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
 
-/*
-int     main()
+#include <stdio.h>
+
+int     main(void)
 {
+	printf("%p\n", ft_strtrim("  miau meow  ", ' '));
         return (0);
 }
-*/
+
