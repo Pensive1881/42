7f93c6545f81b2de744614a34c51836a9294338c
Alexander Casper <acasper@student.42berlin.de>
2025-05-26 23:03:32 +0200

    did half of ft_split

--- libft/part2/ft_split.c
+++ libft/part2/ft_split.c
@@ -6,15 +6,19 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 23:45:27 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/25 20:20:29 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/26 23:02:36 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
+//#include "libft.h"
+#include <unistd.h>
+#include <stdlib.h>
 
 char	**ft_split(char const *s, char c)
 {
+	char 	*str;
 	int	i;
 
+	str = (char *)malloc(sizeof(char) * ());
 	i = 0;
 	while ()
 	{
@@ -24,9 +28,11 @@ char	**ft_split(char const *s, char c)
 	return (0);
 }
 
-/*
+#include <stdio.h>
+
 int     main(void)
 {
+	printf("meow miau meow, ' ': %p\n", *ft_split("meow miau meow", ' '));
         return (0);
 }
-*/
+
