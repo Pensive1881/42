f752d7a51b37daa2cd2cb901a2638d193af693f1
Alexander Casper <acasper@student.42berlin.de>
2025-05-27 00:08:42 +0200

    finished ft_lstlast w/o int main test

--- libft/bonus/ft_lstlast.c
+++ libft/bonus/ft_lstlast.c
@@ -6,18 +6,28 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 00:22:44 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/22 19:04:49 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/27 00:07:54 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
+//#include "libft.h"
 
-t_list	*ft_lstlast(t_list *list)
+typedef struct s_list
 {
-	return (0);
+	void		*content;
+	struct s_list	*next;
+} t_list
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
 }
 
 /*
-int     main()
+int     main(void)
 {
         return (0);
 }
