304c2ae2f5639df16613a67d8a6b9412d2954e1e
Alexander Casper <acasper@student.42berlin.de>
2025-05-27 00:02:30 +0200

    finished ft_lstsize w/o int main test

--- libft/bonus/ft_lstsize.c
+++ libft/bonus/ft_lstsize.c
@@ -6,19 +6,34 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 00:21:50 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/22 19:07:21 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/26 23:52:26 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
+//#include "libft.h"
+
+typedef struct s_list
+{
+	void		*content;
+	struct s_list	*next;
+} t_list;
 
 int	ft_lstsize(t_list *lst)
 {
-	return (0);
-}
+	int	i;
 
+	i = 0;
+	while (lst != NULL)
+	{
+		i++;
+		lst = lst->next;
+	{
+	return (i);
+}
 /*
-int     main()
+#include <stdio.h>
+int     main(void)
 {
+	printf("???", ft_lstsize(???);
         return (0);
 }
 */
