58481d93723bd4131e947d2bb5fcef2908637c26
Alexander Casper <acasper@student.42berlin.de>
2025-05-27 00:02:53 +0200

    finished ft_lstnew w/o int main test

--- libft/bonus/ft_lstnew.c
+++ libft/bonus/ft_lstnew.c
@@ -6,18 +6,31 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 23:56:31 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/22 19:06:37 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/26 23:55:13 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
+//#include "libft.h"
+
+typedef struct s_list
+{
+	void		*content;
+	struct s_list	*next;
+} t_list;
 
 t_list	*ft_lstnew(void *content)
 {
-	return (0);
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
 }
 
 /*
-int     main()
+int     main(void)
 {
         return (0);
 }
