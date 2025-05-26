8071761078fa6425b80324d5f3a9ad2e412996e6
Sasha Kasper <acasper@student.42berlin.de>
2025-05-26 02:42:40 +0200

    created ft_strmapi

--- libft/part2/ft_strmapi.c
+++ libft/part2/ft_strmapi.c
@@ -11,14 +11,59 @@
 /* ************************************************************************** */
 #include "libft.h"
 
+int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	return (0);
+	char	*str;
+	int	i;
+	int	len;
+
+	if (!s || !f)
+		return (NULL);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
 
 /*
-int     main()
+#include <stdio.h>
+#include <ctype.h>
+
+char	cap_odd(unsigned int i, char c)
 {
+	if (i % 2 == 1 && c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+
+int     main(void)
+{
+	char const *str = "miau meow";
+	char *mod = ft_strmapi(str, cap_odd);
+
+	if (mod)
+	{
+		printf("%s\n", mod);
+		free(mod);
+	}
+
         return (0);
 }
 */
