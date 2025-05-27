92a6db9d59578fe70ae2dfec57e2f15c09f00b9f
Alexander Casper <acasper@student.42berlin.de>
2025-05-27 21:57:11 +0200

    made half of ft_split

--- libft/part2/ft_split.c
+++ libft/part2/ft_split.c
@@ -6,26 +6,57 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 23:45:27 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/26 23:02:36 by acasper          ###   ########.fr       */
+/*   Updated: 2025/05/27 21:56:15 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 //#include "libft.h"
 #include <unistd.h>
 #include <stdlib.h>
 
+int	counter(const char *s, char c)
+{
+	int	count;
+	int	found;
+	
+	count = 0;
+	found = 0;
+	while (*s)
+	{
+		if (*s != c && found == 0)
+		{
+			found = 1;
+			count++;
+		}
+		else if (*s == c)
+			found = 0;
+		s++;
+	}
+
+	return (count);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	char 	*str;
+	char 	**str;
+	int	word_len;
+	int	word_count;
 	int	i;
 
-	str = (char *)malloc(sizeof(char) * ());
+	if (!s)
+		return (NULL);
+	word_count = counter(s, c);
+	str = (char **)malloc(sizeof(char *) * (word_count + 1));
+	if (!str)
+		return (NULL);
 	i = 0;
-	while ()
+	while (*s)
 	{
 		
 		i++;
 	}
-	return (0);
+
+	str[i] = '\0';
+	return (str);
 }
 
 #include <stdio.h>
