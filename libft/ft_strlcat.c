1eb1e71fc393898c92e65bf9b7419adee82a4b12
Alexander Casper <acasper@student.42berlin.de>
2025-06-01 21:48:52 +0200

    fixed ft_strlcat parameters

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,12 +6,12 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 22:16:34 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/19 00:15:35 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/01 21:36:41 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-size_t	ft_strlcat(char * restrict dst, const char * restric src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	dstlen;
 	size_t	srclen;
@@ -19,10 +19,10 @@ size_t	ft_strlcat(char * restrict dst, const char * restric src, size_t dstsize)
 
 	dstlen = ft_strlen(dst);
 	srclen = ft_strlen(src);
-	if(dstsize <= dstlen)
-		return (dstsize + srclen);
+	if(size <= dstlen)
+		return (size + srclen);
 	i = 0;
-	while (src[i] && dstlen + i < dstsize - 1)
+	while (src[i] && dstlen + i < size - 1)
 	{
 		dst[dstlen + i] = src[i];
 		i++;
