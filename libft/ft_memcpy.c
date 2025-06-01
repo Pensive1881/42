55bd192ab7a89929c125a93e730ded5aeec9c784
Alexander Casper <acasper@student.42berlin.de>
2025-06-01 21:52:17 +0200

    fixed ft_memcpy parameters

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,12 +6,12 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 22:14:29 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/19 00:13:07 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/01 21:51:20 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-void	*ft_memcpy(void restricted *dst, const void restricted *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned char *d = (unsigned char*) dst;
 	const unsigned char *s = (const unsigned char *) src;
