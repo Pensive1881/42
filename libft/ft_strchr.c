b5938922d6447d607a27b2f86b7edda38f9c7846
Alexander Casper <acasper@student.42berlin.de>
2025-06-03 13:40:33 +0200

    norminetted ft_strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,20 +6,20 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 20:34:34 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/01 23:26:14 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/03 13:39:41 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
-	int     i;
+	int		i;
 
 	i = 0;
 	while (s[i])
 	{
 		if (s[i] == (char)c)
-			return ((char*)&s[i]);
+			return ((char *)&s[i]);
 		i++;
 	}
 	if (c == '\n')
