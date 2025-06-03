50e49f989554a47016d6852dd020c80b516ece29
Alexander Casper <acasper@student.42berlin.de>
2025-06-03 18:04:23 +0200

    fixed ft_putendl_fd write consistency with fd

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 23:54:16 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/22 21:22:59 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/03 18:03:46 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,14 +14,13 @@
 void	ft_putendl_fd(char *s, int fd)
 {
 	ft_putstr_fd(s, fd);
-	write(1, "\n", 1);
+	write(fd, "\n", 1);
 }
 
 /*
-int     main(void)
+int	main(void)
 {
 	ft_putendl_fd("miau", 1);
-
-        return (0);
+		return (0);
 }
 */
