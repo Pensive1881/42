7ec840d0f8862af5bf8bf2347743e3d611039b38
Alexander Casper <acasper@student.42berlin.de>
2025-06-03 17:08:52 +0200

    added pointer iterator

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 22:13:49 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/01 22:45:25 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/03 17:04:56 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -19,6 +19,7 @@ void	ft_bzero(void *str, size_t n)
 	while (n > 0)
 	{
 		*ptr = 0;
+		ptr++;
 		n--;
 	}
 }
