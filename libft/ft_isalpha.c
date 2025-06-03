02fa2fdfaff4676db12d08f10d6aa078ea8f676b
Alexander Casper <acasper@student.42berlin.de>
2025-06-03 12:48:56 +0200

    fixed ft_isalpha looping error

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,31 +6,28 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 20:27:10 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/19 00:11:12 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/03 12:45:22 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 int	ft_isalpha(int c)
 {
-	while (c != '\0')
-	{
-		if (c < 65)
-			return (0);
-		else if (c > 90 && c < 97)
-			return (0);
-		else if (c > 122)
-			return (0);
-	}
+	if (c < 65)
+		return (0);
+	else if (c > 90 && c < 97)
+		return (0);
+	else if (c > 122)
+		return (0);
 	return (1);
 }
 /*
 int	main(void)
 {
-	printf("miau: %d\n", isalphaa("miau"));
-	printf("mia0: %d\n", isalphaa("mia0"));
-	printf("123: %d\n", isalphaa("123"));
-	printf("m m: %d\n", isalphaa("m m"));
+	printf("miau: %d\n", isalpha("miau"));
+	printf("mia0: %d\n", isalpha("mia0"));
+	printf("123: %d\n", isalpha("123"));
+	printf("m m: %d\n", isalpha("m m"));
 	return (0);
 }
 */
