f2b3609a98a61dcc1a47cf296bec3adf6cf40f3f
Alexander Casper <acasper@student.42berlin.de>
2025-06-03 13:27:16 +0200

    norminetted ft_strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 22:20:08 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/01 23:54:40 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/03 13:26:44 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,16 +17,15 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	j;
 
 	if (!*needle)
-		return ((char*) haystack);
-
+		return ((char *) haystack);
 	i = 0;
 	while (haystack[i] && i < len)
 	{
 		j = 0;
-		while(needle[j] && i + j < len && haystack[i + j] == needle[j])
+		while (needle[j] && i + j < len && haystack[i + j] == needle[j])
 			j++;
 		if (!needle[j])
-			return ((char*) haystack + i);
+			return ((char *) haystack + i);
 		i++;
 	}
 	return (NULL);
