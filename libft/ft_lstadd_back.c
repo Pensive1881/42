d759ba040317befc74b5d1b9faf77c2868ee5788
Alexander Casper <acasper@student.42berlin.de>
2025-06-04 15:00:05 +0200

    fixed norminette in ft_lstadd_back

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -15,16 +15,16 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*current;
 
-	if(!lst || !new)
-			return ;
+	if (!lst || !new)
+		return ;
 	if (*lst == NULL)
 	{
-			*lst = new;
-			return ;
+		*lst = new;
+		return ;
 	}
 	current = *lst;
 	while (current->next)
-			current = current->next;
+		current = current->next;
 	current->next = new;
 }
 
