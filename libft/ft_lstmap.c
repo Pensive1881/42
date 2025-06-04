0d73e2bd710ccc1240db80f4097c549e8ca450a6
Alexander Casper <acasper@student.42berlin.de>
2025-06-04 15:17:31 +0200

    fixed norminette in ft_lstmap so helper function makes functions shorter

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -1,20 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 00:28:30 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/03 15:54:27 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/04 15:15:25 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+static void	append_node(t_list **list, t_list *new_node)
+{
+	t_list	*temp;
+
+	if (!*list)
+		*list = new_node;
+	else
+	{
+		temp = *list;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new_node;
+	}
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
-	t_list	*new_node;
 	void	*new_content;
 	t_list	*temp;
 
@@ -32,15 +46,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		}
 		new_node->content = new_content;
 		new_node->next = NULL;
-		if (!new_list)
-			new_list = new_node;
-		else
-		{
-			temp = new_list;
-			while (temp->next)
-				temp = temp->next;
-			temp->next = new_node;
-		}
+		append_node(&new_list, new_node);
 		lst = lst->next;
 	}
 	return (new_list);
