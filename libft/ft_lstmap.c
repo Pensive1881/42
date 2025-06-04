670f941363f106c6b0003d163dfd81ea3b6fc45f
Alexander Casper <acasper@student.42berlin.de>
2025-06-04 15:51:51 +0200

    fixed norm in ft_lstmap & still compiles

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,50 +6,49 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 00:28:30 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/04 15:15:25 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/04 15:32:19 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-static void	append_node(t_list **list, t_list *new_node)
+static t_list	*create_node(void *content)
 {
-	t_list	*temp;
+	t_list	*node;
 
-	if (!*list)
-		*list = new_node;
-	else
-	{
-		temp = *list;
-		while (temp->next)
-			temp = temp->next;
-		temp->next = new_node;
-	}
+	node = malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
 }
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_list;
-	void	*new_content;
-	t_list	*temp;
+	t_list	*new;
+	t_list	*node;
+	t_list	*tail;
 
+	new = NULL;
+	tail = NULL;
 	if (!lst || !f)
 		return (NULL);
-	new_list = NULL;
 	while (lst)
 	{
-		new_content = f(lst->content);
-		new_node = malloc(sizeof(t_list));
-		if (!new_node)
+		node = create_node(f(lst->content));
+		if (!node)
 		{
-			ft_lstclear(&new_list, del);
+			ft_lstclear(&new, del);
 			return (NULL);
 		}
-		new_node->content = new_content;
-		new_node->next = NULL;
-		append_node(&new_list, new_node);
+		if (!tail)
+			new = node;
+		else
+			tail->next = node;
+		tail = node;
 		lst = lst->next;
 	}
-	return (new_list);
+	return (new);
 }
 
 /*
