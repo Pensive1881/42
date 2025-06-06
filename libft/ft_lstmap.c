f6cd7823b0d442bbb5e47840bc23301ece23ebb5
Alexander Casper <acasper@student.42berlin.de>
2025-06-06 14:28:29 +0200

    improved ft_lstmap

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,22 +6,10 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 00:28:30 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/04 15:32:19 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/06 14:27:14 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-
-static t_list	*create_node(void *content)
-{
-	t_list	*node;
-
-	node = malloc(sizeof(t_list));
-	if (!node)
-		return (NULL);
-	node->content = content;
-	node->next = NULL;
-	return (node);
-}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
@@ -35,7 +23,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		return (NULL);
 	while (lst)
 	{
-		node = create_node(f(lst->content));
+		node = ft_lstnew(f(lst->content));
 		if (!node)
 		{
 			ft_lstclear(&new, del);
