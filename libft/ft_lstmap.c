5e7cb1bd9830fac9c7e58b7d0fc18d19803ed24f
Alexander Casper <acasper@student.42berlin.de>
2025-06-06 14:33:51 +0200

    fixed ft_lstmap data leaks

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 00:28:30 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/06 14:27:14 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/06 14:32:44 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,25 +15,22 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 	t_list	*node;
-	t_list	*tail;
+	void	*content;
 
 	new = NULL;
-	tail = NULL;
 	if (!lst || !f)
 		return (NULL);
 	while (lst)
 	{
-		node = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		node = ft_lstnew(content);
 		if (!node)
 		{
+			del(content);
 			ft_lstclear(&new, del);
 			return (NULL);
 		}
-		if (!tail)
-			new = node;
-		else
-			tail->next = node;
-		tail = node;
+		ft_lstadd_back(&new, node);
 		lst = lst->next;
 	}
 	return (new);
