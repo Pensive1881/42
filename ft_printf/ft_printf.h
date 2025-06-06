21c943e33139b24a3cc7c9383031a695cdc0583e
Alexander Casper <acasper@student.42berlin.de>
2025-06-06 18:59:37 +0200

    updated ft_printf.h

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -1,19 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 23:51:02 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/03 23:56:30 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/06 18:20:24 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PRINTF_H
 # define PRINTF_H
 
-# include <>
+# include "./libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <stdarg.h>
 
-int	ft_printf(const char *, );
+int	ft_printf(const char *format, );
 
 #endif
