1b141aaf8dfc739d987ef8d851fdb01e11db62ac
Alexander Casper <acasper@student.42berlin.de>
2025-06-06 22:08:36 +0200

    completed putnbr function

--- ft_printf/ft_putnbr.c
+++ ft_printf/ft_putnbr.c
@@ -6,13 +6,17 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 19:29:43 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/06 19:42:57 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/06 21:47:16 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
 int	putnbr(int n)
 {
-	return (0);
+	char *str = ft_itoa(n);
+	int len = ft_strlen(str);
+	ft_putstr_fd(str, 1);
+	free(str);
+	return (len);
 }
 
