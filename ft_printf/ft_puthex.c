744e0d2059724d7d03a11b6e3516a0fd02744d0d
Alexander Casper <acasper@student.42berlin.de>
2025-06-09 15:54:17 +0200

    added temp variable holders to the body of ft_puthex.c

--- ft_printf/ft_puthex.c
+++ ft_printf/ft_puthex.c
@@ -6,13 +6,15 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 19:29:54 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/06 19:44:45 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/09 15:46:45 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
 int	ft_puthex(long n, const char capital)
 {
+	(void)n;
+	(void)capital;
+
 	return (0);
 }
-
