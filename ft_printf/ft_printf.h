6d5b5e3522a4c115b21a66f4d492ad952858c308
Alexander Casper <acasper@student.42berlin.de>
2025-06-09 15:13:55 +0200

    added functions to ft_printf.h:

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 23:51:02 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/06 19:15:28 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/09 15:12:37 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PRINTF_H
@@ -20,5 +20,11 @@
 # include <stdarg.h>
 
 int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+int	putstr(char *s);
+int	putnbr(int n);
+int	ft_putptr(unsigned long ptr);
+int	ft_puthex(long n, const char capital);
+int ft_putunsign(unsigned int n);Z
 
 #endif
