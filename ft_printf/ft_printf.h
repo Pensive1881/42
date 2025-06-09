2f6bd1cbb6346d629c4e78342d67828745cf30b6
Alexander Casper <acasper@student.42berlin.de>
2025-06-09 16:53:40 +0200

    added new functions to ft_printf.h

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,26 +6,28 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 23:51:02 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/09 15:21:33 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/09 16:45:21 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "../libft/libft.h"
 # include <stdarg.h>
 # include <stdint.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-int	ft_printf(const char *format, ...);
-int	ft_putchar(char c);
-int	putstr(char *s);
-int	putnbr(int n);
-int	ft_putptr(unsigned long ptr);
-int	ft_puthex(long n, const char capital);
-int	ft_putunsign(unsigned int n);
-Z
+int		ft_printf(const char *format, ...);
+int		ft_putchar(char c);
+int		ft_putstr(char *s);
+int		ft_putnbr(int n);
+int		ft_putptr(unsigned long ptr);
+int		ft_puthex(long n, const char capital);
+int		ft_putunsign(unsigned int n);
+char	*ft_itoa(int n);
+size_t	ft_strlen(const char *s);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
