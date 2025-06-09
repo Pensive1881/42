50341acefeb439cb5b9cb450aa07603c5d737143
Alexander Casper <acasper@student.42berlin.de>
2025-06-09 18:27:04 +0200

    corrected ft_printf.h

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 23:51:02 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/09 16:45:21 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/09 17:46:12 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FT_PRINTF_H
@@ -23,11 +23,10 @@ int		ft_putchar(char c);
 int		ft_putstr(char *s);
 int		ft_putnbr(int n);
 int		ft_putptr(unsigned long ptr);
-int		ft_puthex(long n, const char capital);
+int		ft_puthex(unsigned int n, const char capital);
 int		ft_putunsign(unsigned int n);
 char	*ft_itoa(int n);
 size_t	ft_strlen(const char *s);
-void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 
 #endif
