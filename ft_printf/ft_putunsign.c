ee542be95ab651b201f7d66b612adfcd910650d6
Alexander Casper <acasper@student.42berlin.de>
2025-06-09 17:15:04 +0200

    made ft_putunsign

--- ft_printf/ft_putunsign.c
+++ ft_printf/ft_putunsign.c
@@ -6,14 +6,18 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 19:30:40 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/09 15:47:36 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/09 17:09:13 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
 int	ft_putunsign(unsigned int n)
 {
-	(void)n;
+	int	count;
 
-	return (0);
+	count = 0;
+	if (n >= 10)
+		count += ft_putunsign(n / 10);
+	count += ft_putunsign((n % 10) + '0');
+	return (count);
 }
