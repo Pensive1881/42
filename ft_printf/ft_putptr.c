03f2fad78e54f557434a2fa72d77e74b6ef17b47
Alexander Casper <acasper@student.42berlin.de>
2025-06-09 18:36:21 +0200

    made ft_putptr

--- ft_printf/ft_putptr.c
+++ ft_printf/ft_putptr.c
@@ -6,14 +6,34 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 19:30:24 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/09 15:47:17 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/09 18:24:34 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
 int	ft_putptr(unsigned long ptr)
 {
-	(void)ptr;
+	int	count;
+	char	*base;
+	char	buffer[16];
+	unsigned long	n;
+	int	i;
 
-	return (0);
+	count = 0;
+	count += ft_putstr("0x");
+	if (ptr == 0)
+		return (count + ft_putchar('0'));
+
+	n = ptr;
+	base = "0123456789abcdef";
+	i = 0;
+	while (n > 0)
+	{
+		buffer[i++] = base[n % 16];
+		n /= 16;
+	}
+	while (--i >= 0)
+		count += ft_putchar(buffer[i]);
+
+	return (count);
 }
