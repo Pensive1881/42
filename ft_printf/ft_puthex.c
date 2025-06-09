2c11b0f0954ef1d4004d6176acdb377c66309ee2
Alexander Casper <acasper@student.42berlin.de>
2025-06-09 18:38:14 +0200

    corrected ft_puthex

--- ft_printf/ft_puthex.c
+++ ft_printf/ft_puthex.c
@@ -6,18 +6,18 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 19:29:54 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/09 17:13:44 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/09 18:12:23 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
-int	ft_puthex(long n, const char capital)
+int	ft_puthex(unsigned int n, const char capital)
 {
 	int		count;
 	char	*base;
 
 	count = 0;
-	if (capital == 'X')
+	if (capital)
 		base = "0123456789ABCDEF";
 	else
 		base = "0123456789abcdef";
