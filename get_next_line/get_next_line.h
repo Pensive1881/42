0979339b80ccd570e37b2df53012d7265964982b
Alexander Casper <acasper@student.42berlin.de>
2025-06-10 16:25:35 +0200

    updated get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,12 +6,20 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 22:00:59 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/25 22:02:10 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/10 16:24:59 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
 
+char	*get_next_line(int fd);
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchar(const char *s, int c);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_substr(const char  *s, unsigned int start, size_t len);
 
 #endif
