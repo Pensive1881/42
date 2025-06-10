93f619fcf1be6e6a873794175d807f513335f17a
Alexander Casper <acasper@student.42berlin.de>
2025-06-10 17:49:00 +0200

    started get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,15 +6,22 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 21:57:22 by acasper           #+#    #+#             */
-/*   Updated: 2025/05/25 21:59:39 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/10 17:48:34 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
-char	*get_next_line(int_fd)
+char	*get_next_line(int fd)
 {
-	
-	return ();
+	static char	*next_line;
+	char	*buffer;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+
+
+
+	return (next_line);
 }
 
 /*
