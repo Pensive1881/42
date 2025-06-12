fa7f156151b432671c98413fd99ed23e3e76d0eb
Alexander Casper <acasper@student.42berlin.de>
2025-06-12 18:31:42 +0200

    put together amend_stash

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 21:57:22 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/12 18:21:57 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/12 18:31:15 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,25 @@ static char *push_line(char *stash)
 
 static char *amend_stash(char *stash)
 {
-	//
+	int	i;
+	int j;
+	char	*return_stash;
+
+	i = 0;
+	while (stash[i])
+		i++;
+	if (!stash[i])
+	{
+		return (NULL);
+		free(stash);
+	}
+	return_stash = (char *)malloc((ft_strlen(stash) - i + 1) * sizeof(char));
+	j = 0;
+	while (stash[i])
+		return_stash[j++] = return_stash[i++];
+	return_stash[j] = '\0';
+	free(stash);
+	return (return_stash)
 }
 
 char	*get_next_line(int fd)
