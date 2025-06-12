2991c437b81cc7a74756cec13af32ea86f230b88
Alexander Casper <acasper@student.42berlin.de>
2025-06-12 18:46:40 +0200

    fixed norminette in get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,12 +6,11 @@
 /*   By: acasper <acasper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 21:57:22 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/12 18:31:15 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/12 18:45:12 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-static char	*read_stash(int_fd, char *stash)
+static char	*read_stash(int fd, char *stash)
 {
 	char	*buffer;
 	int		bytes;
@@ -25,21 +24,20 @@ static char	*read_stash(int_fd, char *stash)
 		bytes = read(fd, buffer, BUFFER_SIZE);
 		if (bytes == -1)
 		{
-			free (buffer);
+			free(buffer);
 			return (NULL);
 		}
 		stash = ft_strjoin(stash, buffer);
 	}
 	buffer[bytes] = '\0';
-	free (buffer);
-
+	free(buffer);
 	return (stash);
 }
 
-static char *push_line(char *stash)
+static char	*push_line(char *stash)
 {
-	int	i;
-	char *line;
+	int		i;
+	char	*line;
 
 	if (!stash)
 		return (NULL);
@@ -58,13 +56,13 @@ static char *push_line(char *stash)
 	if (stash[i] == '\n')
 		line[i++] == '\n';
 	line[i] = '\0';
-	return (line)
+	return (line);
 }
 
-static char *amend_stash(char *stash)
+static char	*amend_stash(char *stash)
 {
-	int	i;
-	int j;
+	int		i;
+	int		j;
 	char	*return_stash;
 
 	i = 0;
@@ -81,30 +79,27 @@ static char *amend_stash(char *stash)
 		return_stash[j++] = return_stash[i++];
 	return_stash[j] = '\0';
 	free(stash);
-	return (return_stash)
+	return (return_stash);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*next_line;
-	char	*buffer;
+	char		*buffer;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-
 	stash = read_stash(fd, stash);
 	if (!stash)
 		return (NULL);
 	next_line = push_line(stash);
 	stash = amend_stash(stash);
-
 	return (next_line);
 }
 
 /*
 int	main(void)
 {
-	
 	return (0);
 }
 */
