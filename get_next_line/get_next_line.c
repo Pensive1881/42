8fb8e8fb6ca15bb8d1ca0ce9bcca747629c61ab6
Alexander Casper <acasper@student.42berlin.de>
2025-06-12 17:13:24 +0200

    created helper functions in get_next_line.c and made move_stash()

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,13 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: acasper <acasper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 21:57:22 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/10 17:48:34 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/12 17:11:30 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "get_next_line.h"
+
+
+static char	*move_stash(int_fd, char *stash)
+{
+	char	*buffer;
+	int		bytes;
+
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	bytes = 1;
+	while ()
+	{
+		if (bytes)
+			free (buffer);
+	}
+	free (buffer);
+
+	return (stash);
+}
+
+static char *push_line(char *stash)
+{
+	//
+}
+
+static char *amend_stash(char *stash)
+{
+	//
+}
 
 char	*get_next_line(int fd)
 {
@@ -19,7 +48,11 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 
-
+	stash = move_stash(fd, stash);
+	if (!stash)
+		return (NULL);
+	next_line = push_line(stash);
+	stash = amend_stash(stash);
 
 	return (next_line);
 }
