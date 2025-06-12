d14ea37604aea532291bee202eb431757df31490
Alexander Casper <acasper@student.42berlin.de>
2025-06-12 18:33:51 +0200

    fixed norminette in get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -20,7 +20,7 @@ char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 char	*ft_strchar(const char *s, int c);
 char	*ft_strjoin(const char *s1, const char *s2);
-char	*ft_substr(const char  *s, unsigned int start, size_t len);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *s);
 
 #endif
