412393daf1ac05a43b3c66e3baaabafccca94c20
Alexander Casper <acasper@student.42berlin.de>
2025-06-12 18:47:07 +0200

    fixed norminette in main.c

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -9,22 +9,22 @@
 /*   Updated: 2025/06/11 14:35:30 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <fcnl.h>
-#include <unistd.h>
 #include "get_next_line.h"
+#include <fcnl.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int	main()
+int	main(void)
 {
-	int	fd = open("text.txt", O_RDONLY);
+	int		fd;
 	char	*line;
 
+	fd = open("text.txt", O_RDONLY);
 	if (fd < 0)
 	{
 		printf("Error");
 		return (1);
 	}
-
 	line = get_next_line;
 	while (line != NULL)
 	{
@@ -33,6 +33,5 @@ int	main()
 		line = get_next_line(fd);
 	}
 	close(fd);
-
 	return (0);
 }
