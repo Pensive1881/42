f5568e27998c891a2b6360a0a46b260a0c439c64
Alexander Casper <acasper@student.42berlin.de>
2025-06-17 21:40:07 +0200

    fixed get_next_line_bonus.h issues & norminette

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 21:02:15 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/17 21:15:26 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/17 21:39:29 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef GET_NEXT_LINE_BONUS_H
@@ -15,19 +15,18 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-typedef struct s_fd_list
+typedef struct s_list
 {
 	int				fd;
 	char			*stash;
-	struct s_d_list	*next;
-}
+	struct s_list	*next;
+}					t_list;
 
-char *
-get_next_list(int fd);
+char				*get_next_line(int fd);
 
 size_t				ft_strlen(const char *s);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strjoin(char s1, const char s2);
-char				*ft_strdup(cont char *s);
+char				*ft_strdup(const char *s);
 
 #endif
