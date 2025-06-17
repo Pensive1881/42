7d9a85b1ec950073af96ea317b4b1ad3454847f3
Alexander Casper <acasper@student.42berlin.de>
2025-06-17 20:24:56 +0200

    cleaned comments out of get_next_line_utils.c

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 22:03:23 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/12 22:20:03 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/17 20:10:57 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -39,35 +39,6 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *)&s[i]);
 	return (NULL);
 }
-/*
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*str;
-	int		len;
-	int		i;
-	int		j;
-
-	len = ft_strlen(s1) + ft_strlen(s2);
-	str = (char *)malloc((len + 1) * sizeof(char));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		str[i] = s2[j];
-		i++;
-		j++;
-	}
-	str[i] = '\0';
-	return (str);
-}
-*/
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
