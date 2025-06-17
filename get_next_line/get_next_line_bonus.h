7649feab4c25cf79b2ee774da230c970c9ce3f1a
Alexander Casper <acasper@student.42berlin.de>
2025-06-17 21:17:56 +0200

    implemented general outline for get_next_line_bonus.h

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -6,8 +6,27 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 21:02:15 by acasper           #+#    #+#             */
-/*   Updated: 2025/06/17 21:02:16 by acasper          ###   ########.fr       */
+/*   Updated: 2025/06/17 21:15:26 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
+# include <stdlib.h>
+# include <unistd.h>
 
+typedef struct s_fd_list
+{
+	int	fd;
+	char	*stash;
+	struct s_d_list	*next;
+}
+
+char	*get_next_list(int fd);
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char s1, const char s2);
+char	*ft_strdup(cont char *s);
+
+#endif
