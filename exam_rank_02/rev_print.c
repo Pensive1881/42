066aac6f075a51b09a3fc0039ec78f83d67b619b
Alexander Casper <acasper@student.42berlin.de>
2025-08-20 15:59:48 +0200

    finished exam_rank_02: rev_print

--- exam_rank_02/rev_print.c
+++ exam_rank_02/rev_print.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 15:48:21 by acasper           #+#    #+#             */
-/*   Updated: 2025/08/20 15:48:24 by acasper          ###   ########.fr       */
+/*   Updated: 2025/08/20 15:59:13 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,19 @@ void	rev_print(char *str)
 	i = 0;
 	while (str[i])
 		i++;
-	while (str[i] >= 0)
+	while (i >= 0)
 	{
-		write(1, &str[i] 1);
+		write(1, &str[i], 1);
 		i--;
 	}
-	write(1, '\n', 1);
+	write(1, "\n", 1);
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc != 2)
-		write(1, '\n', 1);
+		write(1, "\n", 1);
+	else
+		rev_print(argv[1]);
 	return (0);
 }
