c0e24b495de22c3c665814a6b8e548cf5dc8d407
Sasha Kasper <acasper@student.42berlin.de>
2025-08-20 13:32:17 +0200

    exam_rank_02: rebv_print

---
+++ exam_rank_02/rev_print.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void	rev_print(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+		i++;
+	while(str[i] >= 0)
+	{
+		write(1, &str[i] 1);
+		i--;
+	}
+	write(1, '\n', 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if(argc != 2)
+		write(1, '\n', 1);
+	return (0);
+}
