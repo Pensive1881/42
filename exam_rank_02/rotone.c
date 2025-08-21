4426f647ac2549938980b0be5774ae8b07ae8e61
Alexander Casper <acasper@student.42berlin.de>
2025-08-21 17:05:05 +0200

    finished exam_rank_02: rotone

--- exam_rank_02/rotone.c
+++ exam_rank_02/rotone.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 16:55:26 by acasper           #+#    #+#             */
-/*   Updated: 2025/08/20 18:12:01 by acasper          ###   ########.fr       */
+/*   Updated: 2025/08/21 17:03:48 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -18,7 +18,17 @@ void	rotone(char *str)
 	i = 0;
 	while(str[i])
 	{
-		
+		if ((str[i] >= 'A' && str[i] <= 'Y') || (str[i] >= 'a' && str[i] <= 'y'))
+		{
+			str[i] += 1;	
+			write(1, &str[i], 1);
+		}
+		else if (str[i] == 'Z')
+			write(1, "A", 1);
+		else if (str[i] == 'z')
+			write(1, "a", 1);
+		else
+			write(1, &str[i], 1);
 		i++;
 	}
 	write(1, "\n", 1);
