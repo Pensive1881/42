f49f340668a234775c235e274dbbb912892afe1f
Alexander Casper <acasper@student.42berlin.de>
2025-08-21 18:07:11 +0200

    started exam_rank_02: alpha_mirror

---
+++ exam_rank_02/alpha_mirror.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   alpha_mirror.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/21 18:01:09 by acasper           #+#    #+#             */
+/*   Updated: 2025/08/21 18:06:53 by acasper          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	alpha_mirror(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i])
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		write(1, "\n", 1);
+	else
+		alpha_mirror(argv[1]);
+	return (0);
+}
+
