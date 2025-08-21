b14e315201da1eb9c97009c6c5ee314665a2eea4
Alexander Casper <acasper@student.42berlin.de>
2025-08-21 17:47:06 +0200

    finished exam_rank_02: ulstr

--- exam_rank_02/ulstr.c
+++ exam_rank_02/ulstr.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 17:11:06 by acasper           #+#    #+#             */
-/*   Updated: 2025/08/20 17:13:27 by acasper          ###   ########.fr       */
+/*   Updated: 2025/08/21 17:45:23 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -16,9 +16,13 @@ void	ulstr(char *str)
 	int	i;
 
 	i = 0;
-	while ()
+	while (str[i])
 	{
-		
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		write(1, &str[i], 1);
 		i++;
 	}
 	write(1, "\n", 1);
@@ -27,7 +31,7 @@ void	ulstr(char *str)
 int	main(int argc, char **argv)
 {
 	if (argc != 2)
-		write(1, "\n", 1)
+		write(1, "\n", 1);
 	else
 		ulstr(argv[1]);
 
