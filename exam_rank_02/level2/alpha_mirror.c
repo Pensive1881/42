31bb77145af0978a456279833df0356832878742
Sasha Kasper <acasper@student.42berlin.de>
2025-08-23 23:54:00 +0200

    finished exam_rank_02: alpha_mirror

--- exam_rank_02/level2/alpha_mirror.c
+++ exam_rank_02/level2/alpha_mirror.c
@@ -18,15 +18,12 @@ void	alpha_mirror(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if ((str[i] >= 'A' && str[i] <= 'M') || (str[i] >= 'a' && str[i] <= 'm'))
-			str[i] = 'M' + ('N' - str[i]);
-		else if (str[i] >= 'a' && str[i] <= 'm'))
-			str[i] = 'm' + ('n' - str[i]);
-		else if ((str[i] >= 'N' && str[i] <= 'Z')
-			str[i] = 'N' - ('M' - str[i]);
-		else if (str[i] >= 'n' && str[i] <= 'z'))
-			str[i] = ;
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = 'Z' - str[i] + 'A';
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = 'z' - str[i] + 'a';
 		write(1, &str[i], 1);
+		i++;
 	}
 	write(1, "\n", 1);
 }
