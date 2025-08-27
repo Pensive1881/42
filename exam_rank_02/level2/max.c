d64406b3ae5980099c4a2bf307329fc2994aefb6
Sasha Kasper <acasper@student.42berlin.de>
2025-08-27 22:57:48 +0200

    finished exam_rank_02: max

--- exam_rank_02/level2/max.c
+++ exam_rank_02/level2/max.c
@@ -5,11 +5,11 @@ int	max(int* tab, unsigned int len)
 	int	i;
 	int	bubble;
 
-	bubble = 0;
+	bubble = tab[0];
 	i = 0;
-	if (!tab[i])
+	if (len == 0)
 		return (0);
-	while(tab[i])
+	while(i < len)
 	{
 		if (tab[i] > bubble)
 			bubble = tab[i];
@@ -22,8 +22,14 @@ int	max(int* tab, unsigned int len)
 
 int	main(void)
 {
-	int arr[] = {1, 2, 3, 4, 5};
-	printf("1, 2, 3, 4, 5: %d\n", max(arr, 5));
+	int arr1[] = {1, 2, 3, 4, 5};
+	printf("1, 2, 3, 4, 5: %d\n", max(arr1, 5));
+        int arr2[] = {9, 0, 1, 4, 5, 8, 8, 20};
+        printf("9, 0, 1, 4, 5, 8, 8, 20: %d\n", max(arr2, 8));
+        int arr3[] = {7};
+        printf("7: %d\n", max(arr3, 1));
+        int arr4[] = {2, 9, 3, 3, 8, 7, 7};
+        printf("2, 9, 3, 3, 8, 7, 7: %d\n", max(arr4, 7));
 
 	return (0);
 }
