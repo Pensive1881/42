e69a84905821c692bf3fccb1428916b43acc1ea7
Sasha Kasper <acasper@student.42berlin.de>
2025-08-27 23:09:06 +0200

    started exam_rank_02: inter

---
+++ exam_rank_02/level2/inter.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+void	inter(char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (str[0][i])
+	{
+		j = 0;
+		while ([str[1][j])
+		{
+			if ()
+		}
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3)
+		write(1, "\n", 0);
+	else
+		inter(argv[1]);
+
+	return (0);
+}
+
