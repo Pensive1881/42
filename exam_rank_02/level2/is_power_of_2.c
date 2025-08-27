642357d5a8e8152fb41ea72ec0970e1206076257
Sasha Kasper <acasper@student.42berlin.de>
2025-08-27 21:49:44 +0200

    finished exam_rank_02: is_power_of_2

--- exam_rank_02/level2/is_power_of_2.c
+++ exam_rank_02/level2/is_power_of_2.c
@@ -2,13 +2,25 @@
 
 int	is_power_of_2(unsigned int n)
 {
-	return ();
+	unsigned int p;
+
+	p = 1;
+	if (n == 0)
+		return (0);
+	while (p < n)
+		p *= 2;
+
+	return (p == n);
 }
 
 
 int	main(void)
 {
-	printf();
+	printf("16: %d\n", is_power_of_2(16));
+	printf("4: %d\n", is_power_of_2(4));
+	printf("0: %d\n", is_power_of_2(0));
+	printf("1: %d\n", is_power_of_2(1));
+	printf("42: %d\n", is_power_of_2(42));
 
 	return (0);
 }
