5d6b48c3b3cdbe7da70b6007e7d52c5ed2f14962
Alexander Casper <acasper@student.42berlin.de>
2025-09-03 15:15:44 +0200

    started exam_rank_02: ft_strrev

---
+++ exam_rank_02/level2/ft_strrev.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrev.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/03 14:43:14 by acasper           #+#    #+#             */
+/*   Updated: 2025/09/03 15:15:15 by acasper          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+char	*ft_strrev(char *str)
+{
+	int	i;
+
+	i = 0;
+	while ()
+	{
+		
+	}
+	return (str);
+}
+
+
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("miaumeow: %s", ft_strrev("miaumeow"));
+
+	return (0);
+}
