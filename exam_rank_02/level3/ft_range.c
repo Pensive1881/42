f8ce36d39388d4f6d9c5ffb7be2b5672ced49df0
Alexander Casper <acasper@student.42berlin.de>
2025-09-12 16:50:07 +0200

    started working on exam_rank_02: ft_range

--- exam_rank_02/level3/ft_range.c
+++ exam_rank_02/level3/ft_range.c
@@ -6,14 +6,28 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 18:56:23 by acasper           #+#    #+#             */
-/*   Updated: 2025/08/28 18:58:55 by acasper          ###   ########.fr       */
+/*   Updated: 2025/09/12 16:49:31 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	*ft_range(int start, int end);
 {
-	
-	return ();
+	int	len;
+	int	*arr;
+	int	i;
+
+	len = (end >= start) ? (end - start + 1) : (start - end + 1)
+	arr = (int *)malloc(sizeof(int) * len);
+	if (!arr)
+		return (NULL);
+
+	i = 0;
+	while (i < len)
+	{
+		
+		i++;
+	}
+	return (arr);
 }
 
 #include <stdio.h>
