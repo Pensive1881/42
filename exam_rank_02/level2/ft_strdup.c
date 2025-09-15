bd21a1c8a3254882711ee49b9ab0f48cfe96d79a
Sasha Kasper <acasper@student.42berlin.de>
2025-09-15 17:00:08 +0200

    finished exam_rank_02: ft_strdup

--- exam_rank_02/level2/ft_strdup.c
+++ exam_rank_02/level2/ft_strdup.c
@@ -7,11 +7,15 @@ char *ft_strdup(char *src)
 	size_t	i;
 
 	len = 0;
-	
+	while(src[len])
+		len++;
+	dup = (char *)malloc(sizeof(char) * len+1);
+	if (!dup)
+		return (NULL);
 	i = 0;
-	while (i< len)
+	while (i < len)
 	{
-		
+		dup[i] = src[i]
 		i++;
 	}
 	dup[i] = '\0';
