f8bac0694bc0725b8ecdfa2ff09283cb1363c5c6
Alexander Casper <acasper@student.42berlin.de>
2025-09-16 21:34:42 +0200

    finished exam_rank_02: ft_strrev

--- exam_rank_02/level2/ft_strrev.c
+++ exam_rank_02/level2/ft_strrev.c
@@ -6,17 +6,27 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 14:43:14 by acasper           #+#    #+#             */
-/*   Updated: 2025/09/03 15:15:15 by acasper          ###   ########.fr       */
+/*   Updated: 2025/09/16 21:34:03 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 char	*ft_strrev(char *str)
 {
+	char	temp;
+	int len;
 	int	i;
 
+	len = 0;
+	while (str[len])
+		len++;
+	len--;
 	i = 0;
-	while ()
+	while (i > j)
 	{
-		
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+		i++;
+		j--;
 	}
 	return (str);
 }
