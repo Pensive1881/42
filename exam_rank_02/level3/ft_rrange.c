a17085f93d01049f87fed5e5acf4f478df61f8b7
Alexander Casper <acasper@student.42berlin.de>
2025-09-16 22:12:42 +0200

    norminetted exam_rank_02: ft_rrange

--- exam_rank_02/level3/ft_rrange.c
+++ exam_rank_02/level3/ft_rrange.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 21:41:57 by acasper           #+#    #+#             */
-/*   Updated: 2025/09/16 22:11:03 by acasper          ###   ########.fr       */
+/*   Updated: 2025/09/16 22:12:06 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -33,30 +33,33 @@ int	*ft_rrange(int start, int end)
 			arr[i] = end + i;
 		i++;
 	}
-		
 	return (arr);
 }
-
-#include <unistd.h>
+/*
 #include <stdio.h>
+#include <unistd.h>
 
 int	main(int argc, char **argv)
 {
 	int	*arr;
+	int	start;
+	int	end;
+	int	len;
+	int	i;
+
 	if (argc != 3)
 	{
 		write(1, "\n", 1);
 		return (0);
 	}
-	int	start = atoi(argv[1]);
-	int	end = atoi(argv[2]);
+	start = atoi(argv[1]);
+	end = atoi(argv[2]);
 	arr = ft_rrange(start, end);
-	int len = (end >= start) ? (end - start + 1) : (start - end + 1);
-	
+	len = (end >= start) ? (end - start + 1) : (start - end + 1);
 	if (!arr)
 		return (1);
 	printf("ft_range(%d, %d): ", start, end);
-	int	i = 0;
+	i = 0;
 	while (i < len)
 	{
 		printf("%d ", arr[i]);
@@ -66,3 +69,4 @@ int	main(int argc, char **argv)
 	free (arr);
 	return (0);
 }
+*/
