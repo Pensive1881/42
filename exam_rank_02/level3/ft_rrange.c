d5c1ba63662a93ced90145e04f63fbba279e3752
Alexander Casper <acasper@student.42berlin.de>
2025-09-16 21:49:41 +0200

    started exam_rank_02: ft_rrange

--- exam_rank_02/level3/ft_rrange.c
+++ exam_rank_02/level3/ft_rrange.c
@@ -6,8 +6,32 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 21:41:57 by acasper           #+#    #+#             */
-/*   Updated: 2025/09/16 21:41:58 by acasper          ###   ########.fr       */
+/*   Updated: 2025/09/16 21:48:57 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdlib.h>
 
+int	*ft_rrange(int start, int end)
+{
+	int	len;
+	int	*arr;
+	int	i;
 
+	i = 0;
+	while ()
+	{
+		
+		i++;
+	}
+		
+	return (arr);
+}
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	
+	return (0);
+}
