fbe9c97b465307a1d12ebb4005cd642188304cc2
Alexander Casper <acasper@student.42berlin.de>
2025-09-16 21:55:45 +0200

    finished exam_rank_02: ft_rrange

--- exam_rank_02/level3/ft_rrange.c
+++ exam_rank_02/level3/ft_rrange.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 21:41:57 by acasper           #+#    #+#             */
-/*   Updated: 2025/09/16 21:48:57 by acasper          ###   ########.fr       */
+/*   Updated: 2025/09/16 21:55:35 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -17,10 +17,20 @@ int	*ft_rrange(int start, int end)
 	int	*arr;
 	int	i;
 
+	if (end >= start)
+		len = end - start + 1;
+	else
+		len = start - end + 1;
+	arr = (int *)malloc(sizeof(int) * len);
+	if (!arr)
+		return (NULL);
 	i = 0;
-	while ()
+	while (i < len)
 	{
-		
+		if (start <= end)
+			arr[i] = end + i;
+		else
+			arr[i] = end - i;
 		i++;
 	}
 		
