4e8d607fd8416cd41d99492aee56cdc224d39e83
Alexander Casper <acasper@student.42berlin.de>
2025-09-16 22:06:21 +0200

    created test for exam_rank_02: ft_rrange

--- exam_rank_02/level3/ft_rrange.c
+++ exam_rank_02/level3/ft_rrange.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 21:41:57 by acasper           #+#    #+#             */
-/*   Updated: 2025/09/16 21:55:35 by acasper          ###   ########.fr       */
+/*   Updated: 2025/09/16 22:06:08 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -42,6 +42,27 @@ int	*ft_rrange(int start, int end)
 
 int	main(int argc, char **argv)
 {
+	int	*arr;
+	if (argc != 3)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	int	start = atoi(argv[1]);
+	int	end = atoi(argv[2]);
+	arr = ft_rrange(start, end);
+	int len = (end >= start) ? (end - start + 1) : (start - end + 1);
 	
+	if (!arr)
+		return (1);
+	printf("ft_range(%d, %d): ", start, end);
+	int	i = 0;
+	while (i < len)
+	{
+		printf("%d", arr[i]);
+		i++;
+	)
+	write(1, "\n", 1);
+	free (arr);
 	return (0);
 }
