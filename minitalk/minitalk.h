725fb2e36621bb4137b3366e81f4511870ff8b42
Alexander Casper <acasper@student.42berlin.de>
2025-09-17 17:05:25 +0200

    finished minitalk header

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -6,15 +6,15 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 15:54:28 by acasper           #+#    #+#             */
-/*   Updated: 2025/09/17 15:56:39 by acasper          ###   ########.fr       */
+/*   Updated: 2025/09/17 17:05:05 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include <>
-# include <>
-# include <>
-# include ""
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
 
 #endif
