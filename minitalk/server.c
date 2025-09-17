a583ed5a6f5a28ff766f9b9fb47d49e8e712e500
Alexander Casper <acasper@student.42berlin.de>
2025-09-17 18:01:25 +0200

    started working on Minitalk: server.c

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,13 +6,21 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 19:00:30 by acasper           #+#    #+#             */
-/*   Updated: 2025/09/11 20:08:11 by acasper          ###   ########.fr       */
+/*   Updated: 2025/09/17 18:00:55 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "minitalk.h"
+
+static void	sig_handler(int signo, sigingo_t *info, void *content)
+{
+	
+}
 
 int	main(void)
 {
-	
-	return (0);
+	struct sigaction	sa;
+
+	while (1)
+		pause();
 }
 
