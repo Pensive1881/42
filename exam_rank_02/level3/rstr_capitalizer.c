e5878be5ce23e0c5477c5de2c10795d79d4b0a52
Sasha Kasper <acasper@student.42berlin.de>
2025-09-22 14:30:43 +0200

    made rstr_capitalizer.c

---
+++ exam_rank_02/level3/rstr_capitalizer.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+
+void	capitalizer(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i+1] == ' '))
+			str[i] -=32;
+		write (1, &str[i], 1);
+		i++;
+	}
+	
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc < 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	i = 0;
+	while (i < argc)
+	{
+		capitalizer(argv[i]);
+		write (1, "\n", 1);
+		i++;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
