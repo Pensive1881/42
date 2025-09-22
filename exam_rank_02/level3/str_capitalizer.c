325fe9f67b7606b3f7fe749f7a629f1a67804386
Sasha Kasper <acasper@student.42berlin.de>
2025-09-22 14:43:09 +0200

    made exam_rank_02: str_capitalizerg

--- exam_rank_02/level3/str_capitalizer.c
+++ exam_rank_02/level3/str_capitalizer.c
@@ -2,7 +2,18 @@
 
 void	capitalizer(char *str)
 {
-	
+	int	i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 'a' - 'A';
+		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i] == ' '))
+			str[i] -= 'a'- 'A';
+		write(1, &str[i], 1);
+		i++;
+	}
 }
 
 int     main(int argc, char **argv)
