5631b762d133b162dca9fbdd733e70917cbf042b
Alexander Casper <acasper@student.42berlin.de>
2025-10-10 11:15:57 +0200

    continued 42_fractol: fractol.h

--- fract-ol/fractol.h
+++ fract-ol/fractol.h
@@ -6,11 +6,21 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 17:50:25 by acasper           #+#    #+#             */
-/*   Updated: 2025/10/10 11:11:56 by acasper          ###   ########.fr       */
+/*   Updated: 2025/10/10 11:15:30 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FRACTOL_H
 # define FRACTOL_H
+
+# include "libft/libft.h"
+# include "mlx/mlx.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <math.h>
+
+# define WIDTH 800
+# define HEIGHT 800
+# define MAX_ITER 100
 
 
 
