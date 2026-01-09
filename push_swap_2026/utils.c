f9c9efe2c4ec012117350cc0fd24c306c15249c9
Alexander Casper <acasper@student.42berlin.de>
2026-01-09 16:32:30 +0100

    finished building utils.c

--- push_swap_2026/utils.c
+++ push_swap_2026/utils.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 15:30:56 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/09 16:06:54 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/09 16:14:52 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	free_stack(t_stack **stack)
 	t_stack *tmp;
 
 	if (!stack || !*stack)
-		return 0;
+		return ;
 	while (*stack)
 	{
 		tmp = (*stack)->next;
@@ -35,15 +35,45 @@ void	free_stack(t_stack **stack)
 
 int	get_min(t_stack *stack)
 {
-	
+	int min;
+
+	if (!stack)
+		return (0);
+	min = stack->value;
+	while (stack)
+	{
+		if (stack->value < min)
+			min = stack->value;
+		stack = stack->next;
+	}
+	return (min);	
 }
 
 int get_max(t_stack *stack)
 {
-	
+	int	max;
+
+	if (!stack)
+		return (0);
+	max = stack->value;
+	while (stack)
+	{
+		if (stack->value > max)
+			max = stack->value;
+		stack = stack->next;
+	}
+	return (max);
 }
 
 int get_position(t_stack *stack, int value)
 {
-	
+	int	pos;
+
+	pos = 0;
+	while (stack)
+	{
+		if (stack->value == value)
+			return (pos);
+		pos++;
+		stack = stack->next;
 }
