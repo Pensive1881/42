dfd96b48b20483d44aedf818c0969b38a2be3acf
Sasha Kasper <acasper@student.42berlin.de>
2026-01-12 22:58:08 +0100

    created and started parse.c for push_swap_2026

---
+++ push_swap_2026/parse.c
@@ -0,0 +1,27 @@
+#include "push_swap.h"
+
+long	ft_atol(const char *str)
+{
+	
+}
+
+int	is_valid_numver(char *str)
+{
+	
+}
+
+int	has_duplicates(t_stack *stack)
+{
+	
+}
+
+int	is_sorted(t_stack *stack)
+{
+	
+}
+
+t_stack *parse_args)(int argc, char **argv)
+{
+	
+}
+
