35b17397f2bcd40abe2f31870bfcf013c19b04f6
Alexander Casper <acasper@student.42berlin.de>
2026-01-13 19:25:24 +0100

    started building stack_utils.c for push_swap

--- push_swap_2026/stack_utils.c
+++ push_swap_2026/stack_utils.c
@@ -6,8 +6,32 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:19:01 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/13 17:19:03 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/13 19:25:11 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "push_swap.h"
 
+t_stack	*stack_new(int value)
+{
+	
+}
 
+void	stack_add_front(t_stack **stack, t_stack *new)
+{
+	
+}
+
+void	stack_add_back(t_stack **stack, t_stack *new)
+{
+	
+}
+
+t_stack	*stack_last(t_stack *stack)
+{
+	
+}
+
+int	stack_size(t_stack *stack)
+{
+	
+}
