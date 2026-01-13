093e8ca35bb8a91428fee2f7fdc9506a17238d5f
Alexander Casper <acasper@student.42berlin.de>
2026-01-13 18:32:01 +0100

    finished operations_push.c for push_swap

--- push_swap_2026/operations_push.c
+++ push_swap_2026/operations_push.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:14:51 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/13 17:27:52 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/13 18:31:30 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,26 @@
 
 static void	push(t_stack **src, t_stack **dst)
 {
-	
+	t_stack *tmp;
+
+	if (!src || !*src)
+		return;
+	tmp = *src;
+	*src = (*src)->next;
+	tmp->next = *dst;
+	*dst = tmp;
 }
 
 void	pa(t_stack **a, t_stack **b, int print)
 {
-	
+	push(b, a);
+	if (print)
+		write(1, "pa\n", 3);
 }
 
 void	pb(t_stack **a, t_stack **b, int print)
 {
-	
+	push(a, b);
+	if (print)
+		write(1, "pb\n", 3);
 }
