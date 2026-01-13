03a2449877a2b624fbb58946c8b9244080f72063
Alexander Casper <acasper@student.42berlin.de>
2026-01-13 19:40:40 +0100

    continued building sort.c functions for push_swap

--- push_swap_2026/sort.c
+++ push_swap_2026/sort.c
@@ -6,19 +6,60 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:18:31 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/13 19:21:51 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/13 19:40:16 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
 void	index_stack(t_stack **stack)
 {
-	
+	t_stack	*current;
+	t_stack *compare;
+	int	index;
+
+	current = *stack;
+	while (current)
+	{
+		index = 0;
+		compare = *stack;
+		while (compare)
+		{
+			if (compare->value < current->value)
+				index++;
+			compare = compare->next;
+		}
+		current->index = index;
+		current = current->next;
+	}
 }
 
 void	sort_three(t_stack **a)
 {
-	
+	int	first;
+	int	second;
+	int	third;
+
+	if (stack_size(*a) != 3)
+		return ;
+	first = (*a)->value;
+	second = (*a)->next->value;
+	third = (*a)->next->next->value;
+	if (first > second && second < third && first < third)
+		sa(a, 1);
+	else if (first > second && second > third)
+	{
+		sa(a, 1);
+		rra(a, 1);
+	}
+	else if (first > second && second < third && first > third)
+		ra(a, 1);
+	else if (first < second && second > third && first < third)
+	{
+		sa(a, 1);
+		ra(a. 1);
+	}
+	else if (first < second && second > third && first > third)
+		rra(a, 1);
 }
 
 void	sort_small(t_stack **a, t_stack **b)
