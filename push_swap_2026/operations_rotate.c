ca3144b647f4705afeee0c456bcf0630b3c7e16b
Alexander Casper <acasper@student.42berlin.de>
2026-01-13 18:42:37 +0100

    finished operations_rotate.c for push_swap

--- push_swap_2026/operations_rotate.c
+++ push_swap_2026/operations_rotate.c
@@ -6,27 +6,43 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:15:38 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/13 18:36:44 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/13 18:42:10 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
 static void	rotate(t_stack **stack)
 {
-	
+	t_stack	*first;
+	t_stack *last;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return ;
+	first = *stack;
+	last = stack_last(*stack);
+	*stack = first->next;
+	first->next = NULL;
+	last->next = first;
 }
 
 void	ra(t_stack **a, int print)
 {
-	
+	rotate(a);
+	if (print)
+		write(1, "ra\n" 3);
 }
 
 void	rb(t_stack **b, int print)
 {
-	
+	rotate(b);
+	if (print)
+		write(1, "rb\n", 3):
 }
 
 void	rr(t_stack **a, t_stack **b, int print)
 {
-	
+	rotate(a);
+	rotate(b);
+	if (print)
+		write(a, "rr\n", 3)
 }
