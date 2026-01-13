011aa88c62f455e6c32604721a52ad8ad84ae379
Alexander Casper <acasper@student.42berlin.de>
2026-01-13 17:10:42 +0100

    finished push_swap.c

--- push_swap_2026/push_swap.c
+++ push_swap_2026/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 18:45:46 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/09 18:46:43 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/13 17:10:24 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,40 @@
 
 static void choose_sort(t_stack **a, t_stack **b)
 {
-	
+	int	size;
+
+	size = stack_size(*a);
+	if (size == 2)
+		sa(a, 1);
+	else if (size == 3)
+		sort_three(a);
+	else if (size <= 5)
+		sort_small(a, b);
+	else
+	{
+		index_stack(a);
+		sort_large(a, b);
+	}
 }
 
 int	main(int argc, char **argv)
 {
-	
+	t_stack *a;
+	t_stack *b;
+
+	if (argc < 2)
+		return (0);
+	a = parse_args(Argc, argv);
+	if (!a)
+		error_exit();
+	if (is_sorted(a))
+	{
+		free_stack(&a);
+		return (0);
+	}
+	b = NULL;
+	choose_sort(&a, &b);
+	free_stack(&a);
+	free_Stack(&b);
+	return (0);
 }
