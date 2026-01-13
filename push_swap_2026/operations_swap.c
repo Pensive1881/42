6756df0741ed54f0616e5bcac9fe8c3eccb295d4
Alexander Casper <acasper@student.42berlin.de>
2026-01-13 19:17:14 +0100

    finished operations_swap.c for push_swap

--- push_swap_2026/operations_swap.c
+++ push_swap_2026/operations_swap.c
@@ -6,27 +6,43 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:17:59 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/13 19:13:26 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/13 19:17:00 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
 static void	swap(t_stack **stack)
 {
-	
+	t_stack	*first;
+	t_Stack	*second;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return ;
+	first = *stack;
+	second = first->next;
+	first->next = second->next;
+	second->next = first;
+	*stack = second;
 }
 
 void	sa(t_stack **a, int print)
 {
-	
+	swap(a);
+	if (print)
+		write(1, "sa\n", 3);
 }
 
 void	sb(t_stack **b, int print)
 {
-	
+	swap(b);
+	if (print)
+		write(1, "sb\n", 3);
 }
 
 void ss(t_stack **a, t_stack **b, int print)
 {
-	
+	swap(a);
+	swap(b);
+	if (print)
+		write(1, "ss\n", 3);
 }
