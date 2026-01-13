fb77157bda1ad51d82b6c247f44c60f3ac1e095f
Alexander Casper <acasper@student.42berlin.de>
2026-01-13 19:22:07 +0100

    started building sort.c for push_swap

--- push_swap_2026/sort.c
+++ push_swap_2026/sort.c
@@ -6,8 +6,27 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:18:31 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/13 17:18:32 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/13 19:21:51 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "push_swap.h"
 
+void	index_stack(t_stack **stack)
+{
+	
+}
 
+void	sort_three(t_stack **a)
+{
+	
+}
+
+void	sort_small(t_stack **a, t_stack **b)
+{
+	
+}
+
+void	sort_large(t_stack **a, t_stack **b)
+{
+	
+}
