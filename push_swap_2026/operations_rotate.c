1437a76cb405f28944c5df4c71abb2ba83734b91
Alexander Casper <acasper@student.42berlin.de>
2026-01-13 18:37:22 +0100

    started building operations_rotate.c

--- push_swap_2026/operations_rotate.c
+++ push_swap_2026/operations_rotate.c
@@ -6,8 +6,27 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:15:38 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/13 17:15:40 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/13 18:36:44 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "push_swap.h"
 
+static void	rotate(t_stack **stack)
+{
+	
+}
 
+void	ra(t_stack **a, int print)
+{
+	
+}
+
+void	rb(t_stack **b, int print)
+{
+	
+}
+
+void	rr(t_stack **a, t_stack **b, int print)
+{
+	
+}
