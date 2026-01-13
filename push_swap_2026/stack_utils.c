02dbe10987718046d50de9ec8d2072b7df169868
Alexander Casper <acasper@student.42berlin.de>
2026-01-13 19:54:45 +0100

    finished stack_utils.c for push_swap

--- push_swap_2026/stack_utils.c
+++ push_swap_2026/stack_utils.c
@@ -6,32 +6,65 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:19:01 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/13 19:25:11 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/13 19:54:20 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
 t_stack	*stack_new(int value)
 {
-	
+	t_stack	*new;
+
+	new = (t_stack *)malloc(sizeof(t_stack));
+	if (!new)
+		return (NULL);
+	new->value = value;
+	new->index = -1;
+	new->next = NULL;
+	return (new);
 }
 
 void	stack_add_front(t_stack **stack, t_stack *new)
 {
-	
+	if (!stack || !new)
+		return ;
+	new->next = *stack;
+	*stack = new;
 }
 
 void	stack_add_back(t_stack **stack, t_stack *new)
 {
-	
+	t_stack *last;
+
+	if (!stack || !new)
+		return ;
+	if (!*stack)
+	{
+		*stack = new;
+		return ;
+	}
+	last = stack_last(*stack);
+	last->next = new;
 }
 
 t_stack	*stack_last(t_stack *stack)
 {
-	
+	if (!stack)
+		return (NULL);
+	while (stack->next)
+		stack = stack->next;
+	return (stack);
 }
 
 int	stack_size(t_stack *stack)
 {
-	
+	int	size;
+
+	size = 0;
+	while (stack)
+	{
+		size++;
+		stack = stack->next;
+	}
+	return (size);
 }
