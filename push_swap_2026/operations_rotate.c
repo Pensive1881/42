45aa2303f6cd689060534aee0d31fb2b72ee5444
Alexander Casper <acasper@student.42berlin.de>
2026-01-13 18:59:52 +0100

    fixed operations_rotate.c typos

--- push_swap_2026/operations_rotate.c
+++ push_swap_2026/operations_rotate.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:15:38 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/13 18:42:10 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/13 18:59:27 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -29,14 +29,14 @@ void	ra(t_stack **a, int print)
 {
 	rotate(a);
 	if (print)
-		write(1, "ra\n" 3);
+		write(1, "ra\n", 3);
 }
 
 void	rb(t_stack **b, int print)
 {
 	rotate(b);
 	if (print)
-		write(1, "rb\n", 3):
+		write(1, "rb\n", 3);
 }
 
 void	rr(t_stack **a, t_stack **b, int print)
@@ -44,5 +44,5 @@ void	rr(t_stack **a, t_stack **b, int print)
 	rotate(a);
 	rotate(b);
 	if (print)
-		write(a, "rr\n", 3)
+		write(1, "rr\n", 3);
 }
