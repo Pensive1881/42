d5c9a30fc809335dcd4a47c6f713ebee9e24f04c
Alexander Casper <acasper@student.42berlin.de>
2026-01-13 19:13:46 +0100

    started building operations_swap.c

--- push_swap_2026/operations_swap.c
+++ push_swap_2026/operations_swap.c
@@ -6,8 +6,27 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:17:59 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/13 17:18:01 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/13 19:13:26 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "push_swap.h"
 
+static void	swap(t_stack **stack)
+{
+	
+}
 
+void	sa(t_stack **a, int print)
+{
+	
+}
+
+void	sb(t_stack **b, int print)
+{
+	
+}
+
+void ss(t_stack **a, t_stack **b, int print)
+{
+	
+}
