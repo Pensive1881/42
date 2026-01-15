e83267576aba3de098a5ba12198bd1eab887d5a3
Alexander Casper <acasper@student.42berlin.de>
2026-01-15 18:49:57 +0100

    made correction in operations_reverse_rotate.c for function reverse_rotate()

--- push_swap_2026/operations_reverse_rotate.c
+++ push_swap_2026/operations_reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:16:55 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/14 22:35:08 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/15 18:49:00 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -25,6 +25,8 @@ static void	reverse_rotate(t_stack **stack)
 		second_last = last;
 		last = last->next;
 	}
+	second_last->next = NULL;
+	last->next = *stack;
 	*stack = last;
 }
 
