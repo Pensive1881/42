23ce1d95fe0c54512c676e760b2eca14c4825180
Alexander Casper <acasper@student.42berlin.de>
2026-01-20 18:23:09 +0100

    continued working on textures.c for so_long_2026

--- so_long_2026/src/textures.c
+++ so_long_2026/src/textures.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 16:34:22 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/20 18:02:53 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/20 18:22:54 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
@@ -20,11 +20,32 @@ static void	create_colored_square(t_game *game, t_img *img, int color)
 	img->img = mlx_new_image(game->mlx, TILE_SIZE, TILE_SIZE);
 	if (!img->img)
 		return ;
-	img->addr = 
+	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
+	y = 0;
+	while (y < TILE_SIZE)
+	{
+		x = 0;
+		while (x < TILE_SIZE)
+		{
+			pixel = y * img->line_length + x * (img->bits_per_pixel / 8);
+			*(unsigned int *)(img->addr + pixel) = color;
+			x++;
+		}
+		y++;
+	}
 }
 
 int	load_textures(t_game *game)
 {
+	create_colored_square();
+	create_colored_square();
+	create_colored_square();
+	create_colored_square();
+	create_colored_square();
+	if ()
+	{
+		
+	}
 	
 }
 
