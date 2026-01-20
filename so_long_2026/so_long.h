014aec96a80638d1cc4e45199d5baa7d7512b4f7
Sasha Kasper <acasper@student.42berlin.de>
2026-01-20 09:43:49 +0100

    Fix case-sensitivity issues in _piscine paths
    Remove duplicate uppercase path entries (C04, C05, C07) that conflict
    with lowercase versions on case-insensitive filesystems like macOS.

--- so_long_2026/so_long.h
+++ so_long_2026/so_long.h
@@ -13,6 +13,68 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include 
+# include "libft/libft.h"
+# include "../src/get_next_line.h"
+# include <mlx.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcnhtl.h>
+# include <math.h>
+
+# define TILE_SIZE 32
+# define ESC_KEY 65307
+# define W_KEY 119
+# define A_KEY 97
+# define S_KEY 115
+# define D_KEY 100
+
+# define WALL '1'
+# define EMPTY '0'
+# define COLLECTIBLE 'C'
+# define EXIT 'E'
+# define PLAYER 'P'
+
+typedef struct	s_point
+{
+	
+}	t_point;
+
+typedef struct	s_img
+{
+	
+}	t_imgl;
+
+typedef struct	s_textures
+{
+	
+}	t_textures;
+
+typedef struct	s_map
+{
+	
+}	t_map;
+
+typedef struct	s_game
+{
+	
+}	t_game;
+
+// Map Parsing
+
+
+// Map Vaslidation
+
+
+// Game Initialization
+
+
+// Game Loop
+
+
+// Player Movement
+
+
+// Utils
+
 
 #endif
