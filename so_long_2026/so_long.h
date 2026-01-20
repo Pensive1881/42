505922bb0c3473515aee5a06e16ecb27f0183c3d
Sasha Kasper <acasper@student.42berlin.de>
2026-01-20 11:56:18 +0100

    continued building so_long.h

--- so_long_2026/so_long.h
+++ so_long_2026/so_long.h
@@ -42,40 +42,76 @@ typedef struct	s_point
 
 typedef struct	s_img
 {
-	
-}	t_imgl;
+	void	*img;
+	char	*addr;
+	int	bits_per_pixel;
+	int	line_length;
+	int	endian;
+	int	width;
+	int	height;
+}	t_img;
 
 typedef struct	s_textures
 {
-	
+	t_img	wall;
+	t_img	floor;
+	t_img	collectible;
+	t_img	exit;
+	t_img	player;
 }	t_textures;
 
 typedef struct	s_map
 {
-	
+	char	**grid;
+	int	width;
+	int	height;
+	int	collectibles;
+	int	exit;
+	int	players;
+	t_point	player_pos;
+	t_point	exit_pos;
 }	t_map;
 
 typedef struct	s_game
 {
-	
+	void	*mix;
+	void	*win;
+	t_map	map;
+	t_textures	textures;
+	int	moves;
+	int	collected;
 }	t_game;
 
 // Map Parsing
+int	parse_map();
+int	validate_map();
+int	check_path();
+void	free_map();
 
-
-// Map Vaslidation
-
+// Map Validation
+int	is_rectangular();
+int	is_surrounded_by_walls();
+int	count_map_elements();
 
 // Game Initialization
-
+int	init_game();
+int	load_textures();
+void	free_textures();
 
 // Game Loop
-
+int	game_loop();
+void	render_map();
+int	handle_keypress();
+int	handle_close();
 
 // Player Movement
-
+int	move_player();
+void	update_player_position();
 
 // Utils
-
+void	error_exit();
+void	cleanup_game();
+int	get_map_width();
+int	get_map_height();
 
 #endif
