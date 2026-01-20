517dd5a992556ae7fdc0d1847553bd029fd3e2a4
Alexander Casper <acasper@student.42berlin.de>
2026-01-20 16:18:35 +0100

    norminetted error.c for so_long

--- so_long_2026/src/error.c
+++ so_long_2026/src/error.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 15:58:46 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/20 16:05:58 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/20 16:17:55 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
@@ -28,6 +28,7 @@ void	cleanup_game(t_game *game)
 			mlx_destroy_window(game->mlx, game->win);
 		mlx_destroy_window(game->mlx, game->win);
 		free(game->mlx);
+	}
 }
 
 void	free_map(char **map)
