8fcc5a65f70e6f113a31aa45b042e4ba81cb7ff2
Alexander Casper <acasper@student.42berlin.de>
2026-01-20 16:02:24 +0100

    started building error.c in so_long_2026

--- so_long_2026/src/error.c
+++ so_long_2026/src/error.c
@@ -6,9 +6,33 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 15:58:46 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/20 15:59:10 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/20 16:02:00 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
 
+void	error_exit(char *message)
+{
+	ft_putendl_fd(mesage, 2);
+	exit(1);
+}
 
+void	cleanup_game(t_game *game)
+{
+	
+}
+
+void	free_map(char **map)
+{
+	int	i;
+
+	if (!map)
+		return ;
+	i = 0;
+	while (map[i])
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
+}
