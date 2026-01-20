d564a0edec4851ca02f0903a44605cbfa827eb90
Sasha Kasper <acasper@student.42berlin.de>
2026-01-20 15:36:58 +0100

    finished building main.c for push_swap

--- so_long_2026/src/main.c
+++ so_long_2026/src/main.c
@@ -13,6 +13,26 @@
 
 int	main(int argc, char **argv)
 {
-	
+	t_game	game;
+
+	if (argc != 2)
+		error_exit("Usage: ./so_long <map.ber>");
+	if (ft_strlen(argv[1]) < 5 || ft_strncmp(argv[1] + ft_strlen(argv[1] - 4, ".ber", 4) !=0)
+		error_exit("error\nMap file must have .ber extension");
+	ft_memset(&game, 0,, sizeof(t_game));
+	if (!parse_map(Argv[1], &game))
+		error_exit("Error\nFailed to parse map");
+	if (!validate_map(&game))
+	{
+		cleanup_game(&game);
+		error_exit("Error\nFailed to parse map");
+	}
+	if (!check_path(&game))
+	{
+		cleanup_game(&game);
+		error_exit("Error\nNo valid path in map");
+	}
+	game_loop(&game);
+	cleanup_game(&game);
 	return (0);
 }
