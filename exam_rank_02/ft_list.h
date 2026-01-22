06a571d9226d349ce35d73d955aa8e4df9a8cad2
Alexander Casper <acasper@student.42berlin.de>
2026-01-22 18:54:28 +0100

    put together ft_list.h typedef struct

--- exam_rank_02/ft_list.h
+++ exam_rank_02/ft_list.h
@@ -6,8 +6,16 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 17:49:09 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/22 17:49:10 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/22 18:54:00 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef FT_LIST_H
+# define FT_LIST_H
 
+typedef struct	s_list
+{
+	struct s_list	*next;
+	void			*data;
+}				t_list;
 
+#endif
