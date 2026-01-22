04e406e6b57d5340e368def00eda5e0c9e8f62c3
Alexander Casper <acasper@student.42berlin.de>
2026-01-22 19:00:35 +0100

    put together ft_list_size.c typedef struct

--- exam_rank_02/ft_list_size.c
+++ exam_rank_02/ft_list_size.c
@@ -6,8 +6,20 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 18:50:30 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/22 18:50:31 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/22 19:00:04 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include ""
 
+int	ft_list_size(t_list *begin_list)
+{
+	int	i;
 
+	i = 0;
+	while(begin_list)
+	{
+		i++;
+		begin_list = begin_list->next;
+	}
+	return (i);
+}
