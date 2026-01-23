14640869df357954cd0d2ab137649d862c81bbd9
Alexander Casper <acasper@student.42berlin.de>
2026-01-23 17:06:20 +0100

    finished building game_init.c

--- so_long_2026/src/game_init.c
+++ so_long_2026/src/game_init.c
@@ -6,22 +6,38 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 16:20:16 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/23 16:22:51 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/23 17:06:02 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
 
 static int	init mlx(t_game *game)
 {
-	
+	game->mlx = mlx_init();
+	if (!game->mlx)
+		return (0);
+	game->win = mlx_new_window(game_mlx), game->map.width * TILE_SIZE, map.height * TILE_SIZE, "so_long");
+	if (!game->mlx)
+		return (0);
+	return (1);
 }
 
 statis int	setup_hooks(t_game *game)
 {
-	
+	mlx_hook(game->win, 2, 1L << 0, handle_keypress, game);
+	mlx_hook(game->win, 17, 0, handle_close, game);
+	return (1);
 }
 
 int	init_game(t_game *game)
 {
-	
+	game->moves = 0;
+	game-collected = 0;
+	if (!init_mlx(game))
+		return (0);
+	if (!load_textures(game))
+		return (0);
+	if (!setup_hooks(game))
+		return (0);
+	return (1);
 }
