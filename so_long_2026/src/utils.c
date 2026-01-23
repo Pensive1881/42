01dba8e3b4d0eafa482cb91a2e2673c379d94f53
Alexander Casper <acasper@student.42berlin.de>
2026-01-23 16:08:52 +0100

    finished building utils.c

--- so_long_2026/src/utils.c
+++ so_long_2026/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 15:59:06 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/23 16:00:34 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/23 16:07:18 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,31 @@
 
 int	get_map_width(char *line)
 {
-	
+	int	width;
+
+	width = 0;
+	while (line[width] && line[width] != '\n')
+		width++;
+	return (width);
 }
 
 int	get_map_height(char *filename)
 {
-	
+	int	fd;
+	int	height;
+	char	*line;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	height = 0;
+	line = get_next_line(fd);
+	while (line)
+	{
+		height++;
+		free(line);
+		line = get_next_line(fd);
+	}	
+	close(fd);
+	return (height);
 }
