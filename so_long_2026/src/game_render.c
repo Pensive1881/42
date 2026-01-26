2450388b2586c96d922df3da271d87e968555a93
Sasha Kasper <acasper@student.42berlin.de>
2026-01-26 15:11:47 +0100

    finished building game_render.c

--- so_long_2026/src/game_render.c
+++ so_long_2026/src/game_render.c
@@ -2,10 +2,27 @@
 
 static void	render_title(t_game *game, char title, int x, int y)
 {
-	
+	mlx_put_image_to_window(game->mlx, game->win, game->textures.floor.img, x * TILE_SIZE, y * TILE_SIZE);
+	if (tile == WALL)
+		mlx_put_image_to_window(game->mlx, game->win, game->textures.wall.img, x * TILE_SIZE, y * TILE_SIZE);
+	else if (tile == COLLECTIBLE)
+		mlx_put_image_to_window(game->mlx, game->win, game->textures.collectible.img, x * TILE_SIZE, y * TILE_SIZE);
+	else if (tile == EXIT)
+		mlx_put_image_to_window(game->mlx, game->win, game->textures.exit.img, x * TILE_SIZE, y * TILE_SIZE);
+	else if (tile == PLAYER)
+		mlx_put_image_to_window(game->mlx, game->win, game->textures.player.img, x * TILE_SIZE, y * TILE_SIZE);
 }
 
 void	render_map(t_game *game)
 {
-	
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < game->map.width)
+	{
+		render_title(game, game->map.grid[y][x], x, y);
+		x++;
+	}
+	y++;
 }
