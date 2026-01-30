df1004057a88a9d03f8179b7200d6d13b27bee33
Alexander Casper <acasper@student.42berlin.de>
2026-01-30 19:14:19 +0100

    made solution for sort_list.c

--- exam_rank_02/past_questions/sort_list.c
+++ exam_rank_02/past_questions/sort_list.c
@@ -6,13 +6,29 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 16:03:24 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/30 16:06:25 by acasper          ###   ########.fr       */
+/*   Updated: 2026/01/30 19:14:04 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "list.h"
 
 t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
 {
-	
-	return ();
+	int	swap;
+	t_list	*curr = lst;
+
+	while (lst->next)
+	{
+		if (((*cmp)(lst->data, lst->next->data)) == 0)
+		{
+			swap = lst->data;
+			lst->data = lst->next->data;
+			lst->next->data = swap;
+			lst = curr;
+		}
+		else
+			lst = lst->next;
+	}
+	lst = curr;
+
+	return (lst);
 }
