954be3463cf00ce9ff7ec51d7e797d8789c7a794
Sasha Kasper <acasper@student.42berlin.de>
2026-02-12 18:02:13 +0100

    started building map_parse.c for so_long_2026

--- so_long_2026/src/map_parse.c
+++ so_long_2026/src/map_parse.c
@@ -11,3 +11,23 @@
 /* ************************************************************************** */
 #include "so_long.h"
 
+static char	*read_line_trim(int fd)
+{
+	
+}
+
+static int	allocate_map(t_game *game, int height)
+{
+	
+}
+
+static int	read_map_line(int fd, t_game *game))
+{
+	
+}
+
+int	parse_map(char *filename, t_game *game)
+{
+	
+}
+
