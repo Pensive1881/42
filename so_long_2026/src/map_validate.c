2ada20c8b7e5fdc3b75a83e5beaf8c6d546d267c
Sasha Kasper <acasper@student.42berlin.de>
2026-02-13 09:59:18 +0100

    started building map_validate.c for so_long_2026

--- so_long_2026/src/map_validate.c
+++ so_long_2026/src/map_validate.c
@@ -11,3 +11,46 @@
 /* ************************************************************************** */
 #include "so_long.h"
 
+int	count_map_elements(t_map *map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while ()
+	{
+		j = 0;
+		while ()
+		{
+			
+			j++;
+		}
+		i++;
+	}
+	if ()
+		return (0);
+	return (1);
+}
+
+int	is_surrounded_by_walls(t_map *map)
+{
+	int	i;
+
+	i = 0;
+	while ()
+	{
+		
+	}
+	i = 0;
+	while ()
+	{
+		
+	}
+	return (1);
+}
+
+int	map_validate(t_game *game)
+{
+	return (1);
+}
+
