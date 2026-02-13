d3cf0d6b5bbd4972cad986de39073f5f8c3fd12c
Sasha Kasper <acasper@student.42berlin.de>
2026-02-13 10:11:48 +0100

    continued working on map_validate.c for so_long_2026

--- so_long_2026/src/map_validate.c
+++ so_long_2026/src/map_validate.c
@@ -17,12 +17,28 @@ int	count_map_elements(t_map *map)
 	int	j;
 
 	i = 0;
+	map->collectibles = 0;
+	map->exits = 0;
+	map->player = 0;
 	while ()
 	{
 		j = 0;
 		while ()
 		{
-			
+			if ()
+			{
+				
+			}
+			else if ()
+			{
+				
+			}
+			else if ()
+			{
+				
+			}
+			else if ()
+				return (0);
 			j++;
 		}
 		i++;
@@ -39,18 +55,26 @@ int	is_surrounded_by_walls(t_map *map)
 	i = 0;
 	while ()
 	{
-		
+		if ()
+			return (0);
+		i++;
 	}
 	i = 0;
 	while ()
 	{
-		
+		if ()
+			return (0);
+		i++;
 	}
 	return (1);
 }
 
 int	map_validate(t_game *game)
 {
+	if (gmae->map.height < 3 || game->map.width < 3)
+		return (0);
+	if (!is_surrounded_by_walls(&game->map))
+		return (0);
 	return (1);
 }
 
