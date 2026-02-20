cdbfc3d3637dd3adb4e123709fc617c6c0694712
Alexander Casper <acasper@student.42berlin.de>
2026-02-20 15:07:30 +0100

    started working on player_move.c in so_long_2026

--- so_long_2026/src/player_move.c
+++ so_long_2026/src/player_move.c
@@ -6,8 +6,46 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 20:01:57 by acasper           #+#    #+#             */
-/*   Updated: 2026/02/05 20:01:59 by acasper          ###   ########.fr       */
+/*   Updated: 2026/02/20 15:07:04 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include ""
+
+static void	check_win_condition(t_game *game)
+{
+	if ()
+	{
+		
+	}
+}
+
+void	update_player_position(t_game *game, int new_x, int new_y)
+{
+	
+}
+
+int	move_player(t_game *game, int new_x, int new_y)
+{
+	char	target;
+
+	if ()
+		
+	target = game->map.grid[][];
+	if ()
+		
+	if ()
+		
+	if ()
+	{
+		if ()
+		{
+			
+		}
+		return (0);
+	}
+	update_player_position();
+	render_map(game);
+	return (1);
+}
 
