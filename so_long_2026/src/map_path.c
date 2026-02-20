0b6aecd45425164cf496d4c32f3519854d0dadf0
Sasha Kasper <acasper@student.42berlin.de>
2026-02-20 18:49:12 +0100

    started building map_path.c for so_long_2026

--- so_long_2026/src/map_path.c
+++ so_long_2026/src/map_path.c
@@ -13,7 +13,20 @@
 
 static char	**copy_map(t_map *map)
 {
-	
+	char	**copy;
+	int	i;
+
+	copy = ()malloc();
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (i < map->height)
+	{
+		
+		i++;
+	}
+	copy[i] = NULL;
+	return (copy);
 }
 
 static void	flood_fill(char **map, int x, int y, int *collectibles)
@@ -23,10 +36,24 @@ static void	flood_fill(char **map, int x, int y, int *collectibles)
 
 static int	can_reach_exit(char **map, int exit_x, int ext_y)
 {
-	
+	return (map[exit_y][exit_x] == 'V');
 }
 
 int	check_path(t_game *game)
 {
-	
+	char	**map_copy;
+	int	collectibles;
+	int	result;
+
+	map_copy = copy_map(&game->map);
+	if (!map_copy)
+		return (0);
+	collectibles = 0;
+	flood_fill(map_copy, game->map.player_pos.x,
+		game->map.player_pos.y, &collectibles);
+	result = (collectibles == game->map.collectibles
+			&& can_reach_exit(map_copy, game->map.exit_pos.x,
+				game->map.exit_pos_y));
+	free_map(map_copy);
+	return (result);
 }
