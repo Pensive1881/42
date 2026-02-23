c83d860ce5080ca4a882be333ca26a426f1a2559
Alexander Casper <acasper@student.42berlin.de>
2026-02-23 11:54:44 +0100

    fixed typos in error.c for so_long_2026

--- so_long_2026/src/error.c
+++ so_long_2026/src/error.c
@@ -6,14 +6,14 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 15:58:46 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/20 16:17:55 by acasper          ###   ########.fr       */
+/*   Updated: 2026/02/23 11:54:34 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
 
 void	error_exit(char *message)
 {
-	ft_putendl_fd(mesage, 2);
+	ft_putendl_fd(message, 2);
 	exit(1);
 }
 
@@ -26,7 +26,7 @@ void	cleanup_game(t_game *game)
 		free_textures(game);
 		if (game->win)
 			mlx_destroy_window(game->mlx, game->win);
-		mlx_destroy_window(game->mlx, game->win);
+		mlx_destroy_display(game->mlx);
 		free(game->mlx);
 	}
 }
