2f985dea8bbe996e6f95fe83cd333eedb90a924f
Alexander Casper <acasper@student.42berlin.de>
2026-02-23 11:47:31 +0100

    fixed typos in game_init.c for so_long_2026

--- so_long_2026/src/game_init.c
+++ so_long_2026/src/game_init.c
@@ -6,24 +6,24 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 16:20:16 by acasper           #+#    #+#             */
-/*   Updated: 2026/01/23 17:07:07 by acasper          ###   ########.fr       */
+/*   Updated: 2026/02/23 11:47:20 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
 
-static int init	mlx(t_game *game)
+static int init_mlx(t_game *game)
 {
 	game->mlx = mlx_init();
 	if (!game->mlx)
 		return (0);
 	game->win = mlx_new_window(game->mlx, game->map.width * TILE_SIZE,
-			map.height * TILE_SIZE, "so_long");
+			game->map.height * TILE_SIZE, "so_long");
 	if (!game->mlx)
 		return (0);
 	return (1);
 }
 
-statis int	setup_hooks(t_game *game)
+static int	setup_hooks(t_game *game)
 {
 	mlx_hook(game->win, 2, 1L << 0, handle_keypress, game);
 	mlx_hook(game->win, 17, 0, handle_close, game);
@@ -33,7 +33,7 @@ statis int	setup_hooks(t_game *game)
 int	init_game(t_game *game)
 {
 	game->moves = 0;
-	game - collected = 0;
+	game->collected = 0;
 	if (!init_mlx(game))
 		return (0);
 	if (!load_textures(game))
