c6effc7602aa81453a9683ff96e78f36962f1b4b
Alexander Casper <acasper@student.42berlin.de>
2026-02-26 13:26:07 +0100

    cleaned up commented old code in textures.c for so_long

--- so_long/src/textures.c
+++ so_long/src/textures.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 16:34:22 by acasper           #+#    #+#             */
-/*   Updated: 2026/02/26 12:34:30 by acasper          ###   ########.fr       */
+/*   Updated: 2026/02/26 13:25:34 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
@@ -18,29 +18,6 @@ static int	load_xpm(t_game *game, t_img *img, char *path)
 			&img->width,
 			&img->height);
 	return (img->img != NULL);
-/*
-	int	x;
-	int	y;
-	int	pixel;
-
-	img->img = mlx_new_image(game->mlx, TILE_SIZE, TILE_SIZE);
-	if (!img->img)
-		return ;
-	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel,
-			&img->line_length, &img->endian);
-	y = 0;
-	while (y < TILE_SIZE)
-	{
-		x = 0;
-		while (x < TILE_SIZE)
-		{
-			pixel = y * img->line_length + x * (img->bits_per_pixel / 8);
-			*(unsigned int *)(img->addr + pixel) = color;
-			x++;
-		}
-		y++;
-	}
-*/
 }
 
 int	load_textures(t_game *game)
@@ -56,17 +33,6 @@ int	load_textures(t_game *game)
 	if (!load_xpm(game, &game->textures.player, "assets/player_front.xpm"))
 		return(0);
 	return (1);
-/*
-	create_colored_square(game, &game->textures.wall, 0x808080);
-	create_colored_square(game, &game->textures.floor, 0xFFFFFF);
-	create_colored_square(game, &game->textures.collectible, 0xFFD700);
-	create_colored_square(game, &game->textures.exit, 0x00FF00);
-	create_colored_square(game, &game->textures.player, 0x0000FF);
-	if (!game->textures.wall.img || !game->textures.floor.img
-		|| !game->textures.collectible.img || !game->textures.exit.img
-		|| !game->textures.player.img)
-		return (0);
-*/
 }
 
 void	free_textures(t_game *game)
