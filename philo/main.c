9dcd7d6b21357f1219ba69cc091a7bf7f8a525f1
Sasha Kasper <acasper@student.42berlin.de>
2026-03-04 13:35:53 +0100

    started working on main.c for philosophers

--- philo/main.c
+++ philo/main.c
@@ -9,5 +9,22 @@
 /*   Updated: 2026/03/03 15:21:06 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "philo.h"
 
+int	main(int argc, char **argv)
+{
+	t_table	d;
+
+	d.forks = NULL;
+	d.philos = NULL;
+	if ()
+		return ;
+	if ()
+		return ;
+	if()
+		return ;
+	join_threads(&d);
+	cleanup(&d);
+	return (0);
+}
 
