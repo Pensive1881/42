998ca3d95c63758084dfbabc365b98b6d78e7b6e
Alexander Casper <acasper@student.42berlin.de>
2026-03-04 17:47:36 +0100

    started building exec.c for minishell

--- minishell/src/exec.c
+++ minishell/src/exec.c
@@ -6,8 +6,32 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 17:28:08 by acasper           #+#    #+#             */
-/*   Updated: 2026/03/04 17:28:09 by acasper          ###   ########.fr       */
+/*   Updated: 2026/03/04 17:47:19 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "minishell.h"
 
+void	execute_command(char *input, char **envp)
+{
+	pid_t	pid;
+	char	**args;
+
+	args = split_args(input);
+	if (!args || !args[0])
+	{
+		free_args(args);
+		return ;
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(args[0], args, envp);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else
+		waitpid(pid, NULL, 0);
+	free_args(args);
+}
 
