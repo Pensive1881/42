ac69cb03898015f0a8d467eb94d58d9f9774c4a3
Alexander Casper <acasper@student.42berlin.de>
2026-03-04 17:59:37 +0100

    continued working on utils.c for minishell

--- minishell/src/utils.c
+++ minishell/src/utils.c
@@ -6,23 +6,58 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 17:28:35 by acasper           #+#    #+#             */
-/*   Updated: 2026/03/04 17:52:31 by acasper          ###   ########.fr       */
+/*   Updated: 2026/03/04 17:59:27 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
 
 static int	count_words(char *s)
 {
-	
+	int	i;
+	int	count;
+
+	count = 0;
+	i = 0;
+	while (s[i] == ' ')
+	{
+		
+	}
+	return (count);
 }
 
 char	**split_args(char *input)
 {
-	
+	char	**args;
+	int i;
+	int	j;
+	int	start;
+
+	args = malloc(sizeof(char *) * (count_words(input) + 1));
+	if (!args)
+		return (NULL);
+
+	i = i;
+	while (input[i])
+	{
+		
+	}
+
+	args[j] = NULL;
+	RETURN (args);
 }
 
 void	free_args(char **args)
 {
-	
-|
+	int	i;
+
+	if (!args)
+		return ;
+	i = 0;
+	while (args[i])
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
+}
 
