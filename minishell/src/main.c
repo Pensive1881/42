2f552fa66b5ae134047190a9d50f8f699345408d
Alexander Casper <acasper@student.42berlin.de>
2026-03-04 18:26:51 +0100

    fixed envp parameter issue in main.c for minishell

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,12 +6,12 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 17:27:22 by acasper           #+#    #+#             */
-/*   Updated: 2026/03/04 17:52:05 by acasper          ###   ########.fr       */
+/*   Updated: 2026/03/04 18:26:27 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
 	(void)argc;
 	(void)argv;
