b9d198be9bebdcfe7380fe442454ae61f82b81a0
Alexander Casper <acasper@student.42berlin.de>
2026-03-04 17:30:45 +0100

    started working on main.c for minishell

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,8 +6,17 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 17:27:22 by acasper           #+#    #+#             */
-/*   Updated: 2026/03/04 17:27:23 by acasper          ###   ########.fr       */
+/*   Updated: 2026/03/04 17:30:34 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <minishell.h>
 
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	prompt_loop(envp);
+	return (0);
+}
 
