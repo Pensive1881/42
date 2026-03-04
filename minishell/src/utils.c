9114a7c0037ff31b6338a6f743cc00c2165a390c
Alexander Casper <acasper@student.42berlin.de>
2026-03-04 17:34:16 +0100

    started working on utils.c for minishell

--- minishell/src/utils.c
+++ minishell/src/utils.c
@@ -6,8 +6,23 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 17:28:35 by acasper           #+#    #+#             */
-/*   Updated: 2026/03/04 17:28:36 by acasper          ###   ########.fr       */
+/*   Updated: 2026/03/04 17:34:05 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <minishell.h>
 
+static int	count_words(char *s)
+{
+	
+}
+
+char	**split_args(char *input)
+{
+	
+}
+
+void	free_args(char **args)
+{
+	
+|
 
