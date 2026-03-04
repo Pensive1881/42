4ec68a4f7b647d4b8a3a9a13e3f660cd617f12e5
Alexander Casper <acasper@student.42berlin.de>
2026-03-04 17:55:26 +0100

    started working on minishell.h

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -6,8 +6,23 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 17:26:02 by acasper           #+#    #+#             */
-/*   Updated: 2026/03/04 17:26:03 by acasper          ###   ########.fr       */
+/*   Updated: 2026/03/04 17:55:16 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+void	prompt_loop(char **envp);
+void	execute_command(char *input, char **envp);
+char	**split_args(char *input);
+void	free_args(char **args);
+
+#endif
 
