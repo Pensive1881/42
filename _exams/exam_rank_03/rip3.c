6fe33edfff66b5a896a067471ab5e8e94439ef5d
Alexander Casper <acasper@student.42berlin.de>
2026-03-13 13:24:48 +0100

    make rip3.c in exam_rank_03

--- _exams/exam_rank_03/rip3.c
+++ _exams/exam_rank_03/rip3.c
@@ -6,8 +6,35 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 13:13:36 by acasper           #+#    #+#             */
-/*   Updated: 2026/03/13 13:13:37 by acasper          ###   ########.fr       */
+/*   Updated: 2026/03/13 13:24:17 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
+#include <fctnl.h>
 
+int	main(int argc, char **argv)
+{
+	int fd1;
+	int fd2;
+	char buffer[1024];
+	int	bytes;
+
+	if (argc != 3)
+		return (1);
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 < 0)
+		return (1);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | 0_TRUNC, 0644);
+	if (fd2 < 0)
+	{
+		close(fd2);
+		return (1);
+	}
+	while ((bytes = read(fd1, buffer, 1024)) > 0)
+		write(fd2, buffer, bytes);
+	close(fd1);
+	close(fd2);
+
+	return (0);
+}
 
