7d66accbf8300cd44db8d7560fe0d915fbaf0fd8
Pensive <lumberjackparade@hotmail.com>
2026-03-19 09:52:47 +0100

    made get_next_line.c for exam_rank_03

---
+++ _exams/exam_rank_03/get_next_line.c
@@ -0,0 +1,45 @@
+
+#include <unistd.h>
+#include <stdlib.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10000
+#endif
+
+char *get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE + 1];
+	static int	i = 0;
+	static int	size = 0;
+	char	*line;
+	int	j = 0;
+
+	if (fd < 0)
+		return (NULL);
+
+	line = malloc(10000);
+	if (!line)
+		return (NULL);
+
+	while (1)
+	{
+		if (i >= size)
+		{
+			size = read(fd, buffer, BUFFER_SIZE);
+			i = 0;
+			if (size <= 0)
+				break;
+		}
+		line[j++] = buffer[i];
+		if (buffer[i++] == '\n')
+			break;
+	}
+	if (j == 0)
+	{
+		free(line);
+		return (NULL);
+	}
+	line[j] = '\0';
+	return (line);
+}
+
