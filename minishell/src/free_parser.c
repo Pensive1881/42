278207b75e5157d2335e2729d4299e347a573d4d
Pensive <lumberjackparade@hotmail.com>
2026-03-19 17:25:11 +0100

    created free_parser.c for minishell

---
+++ minishell/src/free_parser.c
@@ -0,0 +1,39 @@
+#include "../includes/parser.h"
+
+void	free_redirs(t_redir *redirs)
+{
+	t_redir	*tmp;
+
+	while (redirs)
+	{
+		tmp = redirs->next;
+		free(redirs->file);
+		free(redirs);
+		redirs = tmp;
+	}
+}
+
+void	free_cmds(t_cmd *cmds)
+{
+	t_cmd	*tmp;
+	int	i;
+
+	while (cmds)
+	{
+		tmp = cmds->next;
+		if (cmds->argv)
+		{
+			i = 0;
+			while (cmds->argv[i])
+			{
+				free(cmds->argv[i]);
+				i++;
+			}
+			free(cmds->redirs);
+		}
+		free_redirs(cmds->redirs);
+		free(cmds);
+		cmds = tmp;
+	}
+}
+
