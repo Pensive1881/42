bee5f63f4c025cc8ff50317eea4f732186bb796d
Pensive <lumberjackparade@hotmail.com>
2026-03-19 17:45:36 +0100

    created parse_command.c for minishell

---
+++ minishell/src/parse_command.c
@@ -0,0 +1,32 @@
+#include "../includes/parser.h"
+
+t_cmd	*parse_command(t_token **cur)
+{
+	t_cmd	*cmd;
+	t_redir	*redir;
+	
+	if (!cur || !*cur)
+		return (NULL);
+	cmd = init_cmd();
+	if (!cmd)
+		return (NULL);
+	while (*cur && (*cur)->type != TOKEN_PIPE)
+	{
+		if ((*cur)->type == TOKEN_WORD)
+		{
+			if (!add_word_to_cmd(cmd, (*cur)->value))
+				return (free_cmd_and_null(cmd));
+			*cur = (*cur)->next;
+		}
+		else if (is_redirection((*cur)->type))
+		{
+			redir = parse_redirection(cur);
+			if (!redir || !add_redir_to_cmd(cmd, redir))
+				return (free_cmd_and_null(cmd));
+		}
+		else
+			return (free_cmd_and_null(cmd));
+	}
+
+	return (cmd);
+}
