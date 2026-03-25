dcf15ae4fcb3097ff1678874cb50b126ceffa783
Pensive <lumberjackparade@hotmail.com>
2026-03-25 16:59:49 +0100

    updated parser.h with validate_syntax function for minishell

--- minishell/includes/parser.h
+++ minishell/includes/parser.h
@@ -55,6 +55,7 @@ int	add_word_to_cmd(t_cmd *cmd, char *word, t_arena *arena);
 t_cmd	*parse_command(t_token **cur);
 t_cmd	*parse_pipeline(t_token **cur);
 t_cmd	*parse_tokens(t_token *tokens);
+int	validate_syntax(t_token *token);
 /*
 t_cmd	*parse_command(t_token **cur, t_arena *arena);
 t_cmd	*parse_pipeline(t_token **cur, t_arena *arena);
