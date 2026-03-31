25240adba94fa21fe7082f3b6dc895c2856da550
Pensive <lumberjackparade@hotmail.com>
2026-03-31 20:35:33 +0200

    built path.c for minishell

---
+++ minishell/src/exec/path.c
@@ -0,0 +1,66 @@
+#include "../../includes/minishell.h"
+
+char	*get_env_value(char **envp, char *name)
+{
+	int	i;
+	size_t	len;
+
+	if (!envp || !name)
+		return (NULL);
+	len = ft_strlen(name);
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], name, len) == 0 && envp[i][len] == '=')
+			return (envp[i] + len + 1);
+		i++;
+	}
+	return (NULL);
+}
+
+static char	**join_path_cmd(char *dir, char *cmd)
+{
+	char	*tmp;
+	char	*full;
+
+	tmp = ft_strjoin(dir, "/");
+	if (!tmp)
+		return (NULL);
+	full = ft_strjoin(tmp, cmd);
+	free(tmp);
+	return (full);
+}
+
+char	*find_command_path(char *cmd, char **envp)
+{
+	char	**paths;
+	char	*path_env;
+	char	*full;
+	int	i;
+
+	if (!cmd)
+		return (NULL);
+	if (ft_strchr(cmd, '/'))
+	{
+		if (access(cmd, X_OK) == 0)
+			return (ft_strdup(cmd));
+		return (NULL);
+	}
+	path_env = get_env_value(envp, "PATH");
+	if (!paths)
+		return (NULL);
+	paths = ft_split(path_env, ':');
+	if (!paths)
+		return (NULL);
+	i = 0;
+	while (paths[i])
+	{
+		full = join_path_cmd(path[i], cmd);
+		if (full && access(full, X_OK) == 0)
+			return (free_args(paths), full);
+		free(full);
+		i++;
+	}
+	free_args(paths);
+	return (NULL);
+}
