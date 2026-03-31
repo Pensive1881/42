b6c9daedf689d917bacb685e3b89a6796291e369
Pensive <lumberjackparade@hotmail.com>
2026-03-31 19:57:01 +0200

    built execute_multi.c for minishell

---
+++ minishell/src/exec/execute_multi.c
@@ -0,0 +1,71 @@
+#include "../../includes/minishell.h"
+
+static int	count_cmds(t_cmd *cmds)
+{
+	int	count;
+
+	count = 0;
+	while (cmds)
+	{
+		count++;
+		cmds = cmds->next;
+	}
+	return (count);
+}
+
+static void	wait_all(pid_t *pids, int count)
+{
+	int	i;
+	int	status;
+
+	i = 0;
+	while (i < count)
+	{
+		waitpid(pids[i], &status, 0);
+		i++;
+	}
+}
+
+void	execute_multi_command(t_cmd *cmds, char **envp)
+{
+	int	prev_read;
+	int	pipefd[2];
+	pid_t	*pids;
+	int	i;
+	int	count;
+	t_cmd	*cur;
+
+	count = count_cmds(cmds);
+	pids = malloc(sizeof(pid_t) * count);
+	if (!pids)
+		return ;
+	prev_read = -1;
+	i = 0;
+	cur = cmds;
+	while (cur)
+	{
+		if (cur->next && pipe(pipefd) < 0)
+			return (free(pids), perror("pipe"));
+		pids[i] = fork();
+		if (pids[i] < 0)
+			return (free(pids), perror("fork"));
+		if (pids[i] == 0)
+		{
+			if (cur->next)
+				child_exec(cur, prev_read, pipefd[1], envp);
+			else
+				child_exec(cur, prev_Read, STDOUT_FILENO, envp);
+		}
+		close_fd_if_needed(prev_read);
+		if (cur->next)
+		{
+			close(pipefd[1]);
+			prev_read = pipefd[0];
+		}
+		cur = cur->next;
+		i++;
+	}
+	close_fd_if_needed(prev_read);
+	wait_all(pids, count);
+	free(pids);
+}
