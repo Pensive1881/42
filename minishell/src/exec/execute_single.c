35128c8e1d5c578d975edfa31b4f60ab826792ee
Pensive <lumberjackparade@hotmail.com>
2026-03-31 13:25:54 +0200

    updated execute_pipeline.c for minishell

--- minishell/src/exec/execute_single.c
+++ minishell/src/exec/execute_single.c
@@ -4,26 +4,40 @@ static void	exec_external(t_cmd *cmd, char **envp)
 {
 	char *path;
 
+	if (!cmd || !cmd->argv || !cmd->argv[0])
+		exit(1);
 	path = find_command_path(cmd->argv[0], envp);
-
+	if (!path)
+	{
+		print_exec_error(cmd->argv[0], envp);
+		exit(127);
+	}
+	execve(path, cmd->argv, envp);
+	perror("execve");
 	free(path);
 	exit(126);
 }
 
 static void    execute_single_command(t_cmd *cmd, char **envp)
 {
-        pid_t pid;
+	pid_t pid;
 	int	status;
 
-        if (!cmd || !cmd->argv || !cmd->argv[0])
-                return ;
-        pid = fork();
-        if (pid == 0)
-        {
-                execve(cmd->argv[0], cmd->argv, envp);
-                perror("execve");
-                exit(EXIT_FAILURE);
+	if (!cmd || !cmd->argv || !cmd->argv[0])
+		return ;
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork");
+		return ;
+	}
+	if (pid == 0)
+	{
+		if (!apply_redirections(cmd->redirs))
+			exit(1);
+		if (!is_buildin(cmd->argv[0]))
+			exit(execute_builtin(cmd, envp));
 		exec_external(cmd, envp);
-        }
-        waitpid(pid, &status, 0);
+	}
+	waitpid(pid, &status, 0);
 }
