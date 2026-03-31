4d50e28c7f97e0e85deee0446d1f5e8b876b44ec
Pensive <lumberjackparade@hotmail.com>
2026-03-31 20:08:57 +0200

    finished building redirections.c for minishell

--- minishell/src/exec/redirections.c
+++ minishell/src/exec/redirections.c
@@ -5,7 +5,17 @@ static int	open_redir_file(t_redir *redir)
 	int	fd;
 
 	fd = -1;
-	
+	if (redir->type == RE_IN)
+		fd = open(redir->file, O_RDONLY);
+	else if (redir->type == R_OUT)
+		fd = open(redir->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (redir->type == R_APPEND)
+		fd = open(redir->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else if (redir->type == R_HEREDOC)
+	{
+		ft_putendl_fd("heredoc not implemented yet", 2);
+		return (-1);
+	}
 	if (fd < 0)
 		perror(redir->file);
 	return (fd);
@@ -20,13 +30,15 @@ int	apply_redirections(t_redir *redirs)
 		fd = open_redir_file(redirs);
 		if (fd < 0)
 			return (0);
-		if ()
+		if (redirs->type == RE_IN || redirs->type == R_HEREDOC)
 		{
-			
+			if (dup2(fd, STDIN_FILEO) < 0)
+				return (close(fd), perror("dup2"), 0);
 		}
 		else
 		{
-			
+			if (dup2(fd, STDOUT_FILEO) < 0)
+				return (close(fd), perror("dup2"), 0);
 		}
 		close(fd);
 		redirs = redirs->next;
