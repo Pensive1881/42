f141aadfa35a27d90e27ec0edd9af30f37693a94
Pensive <lumberjackparade@hotmail.com>
2026-04-07 18:25:07 +0200

    built signals.c for minishell

--- minishell/src/signals.c
+++ minishell/src/signals.c
@@ -1,3 +1,22 @@
 #includes "../includes/minishell.h"
 
+static void signal_handler(int signo)
+{
+	(void)signo;
+	write(1, "\n", 1);
+	rl_one_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
 
+void	setup_signals(void)
+{
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+void	setup_child_signals(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+}
