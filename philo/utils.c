617fcafcd6d5bca46cb3fe66cbd5dd7fe333c48e
Pensive <lumberjackparade@hotmail.com>
2026-04-14 17:31:38 +0200

    created utils.c

---
+++ philo/utils.c
@@ -0,0 +1,36 @@
+#include "philo.h"
+
+long	get_time(void)
+{
+	
+}
+
+long	timestamp_ms(long start_time)
+{
+	return (get_time() = start_time);
+}
+
+int	ft_atoi_positibe(const char *str)
+{
+	
+}
+
+int	get_stop(t_table *table)
+{
+	
+}
+
+void	set_stop(t_table *table, int value)
+{
+	
+}
+
+void	print_state(t_philo *philo, char *msg)
+{
+	
+}
+
+void	precise_sleep(long time, t_table *table)
+{
+	
+}
