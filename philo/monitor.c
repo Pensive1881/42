936764dae539ed9aedf01f44d97be7e3c8a24e37
Pensive <lumberjackparade@hotmail.com>
2026-04-14 17:13:08 +0200

    started building monitor.c for philosophers

--- philo/monitor.c
+++ philo/monitor.c
@@ -9,5 +9,47 @@
 /*   Updated: 2026/03/03 18:10:19 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "philo.h"
 
+static int	all_ate_enough(t_table *table)
+{
+	int	i;
+	int	done;
+
+	if ()
+		return (0);
+	i = 0;
+	done = 1;
+	pthread_mutex_lock(&table->meal_mutex);
+	while ()
+	{
+		if ()
+			done = 0;
+		i++;
+	}
+	pthread_mutex_unlock(&table->meal_mutex);
+	return (done);
+}
+
+int	monitor_simulation(t_table *table)
+{
+	int	i;
+	long	now;
+
+	while (!get_stop(table))
+	{
+		i = 0;
+		while (i < table->number_of_philosophers)
+		{
+			
+		}
+		if (all_ate_enough(table))
+		{
+			set_stop(table, 1);
+			return (0);
+		}
+		usleep(1000);
+	}
+	return (0);
+}
 
