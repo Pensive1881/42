89c6b927f8a41c905dc5844ace6257c4adedeeb0
Alexander Casper <acasper@student.42berlin.de>
2026-04-16 17:56:47 +0200

    finished norminetting monitor.c for philosophers

--- philo/monitor.c
+++ philo/monitor.c
@@ -6,7 +6,7 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 18:10:18 by acasper           #+#    #+#             */
-/*   Updated: 2026/04/16 17:21:07 by acasper          ###   ########.fr       */
+/*   Updated: 2026/04/16 17:56:05 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "philo.h"
@@ -31,36 +31,41 @@ static int	all_ate_enough(t_table *table)
 	return (done);
 }
 
+static int	check_death(t_table *table, int i)
+{
+	long	now;
+
+	pthread_mutex_lock(&table->meal_mutex);
+	now = get_time();
+	if (now - table->philos[i].last_meal_time > table->time_to_die)
+	{
+		pthread_mutex_unlock(&table->meal_mutex);
+		pthread_mutex_lock(&table->print_mutex);
+		printf("%ld %d died\n", timestamp_ms(table->start_time),
+			table->philos[i].id);
+		pthread_mutex_unlock(&table->print_mutex);
+		set_stop(table, 1);
+		return (1);
+	}
+	pthread_mutex_unlock(&table->meal_mutex);
+	return (0);
+}
+
 int	monitor_simulation(t_table *table)
 {
-	int		i;
-	long	now;
+	int	i;
 
 	while (!get_stop(table))
 	{
 		i = 0;
 		while (i < table->number_of_philosophers)
 		{
-			pthread_mutex_lock(&table->meal_mutex);
-			now = get_time();
-			if (now - table->philos[i].last_meal_time > table->time_to_die)
-			{
-				pthread_mutex_unlock(&table->meal_mutex);
-				pthread_mutex_lock(&table->print_mutex);
-				printf("%ld %d died\n", timestamp_ms(table->start_time),
-					table->philos[i].id);
-				pthread_mutex_unlock(&table->print_mutex);
-				set_stop(table, 1);
+			if (check_death(table, i))
 				return (1);
-			}
-			pthread_mutex_unlock(&table->meal_mutex);
 			i++;
 		}
 		if (all_ate_enough(table))
-		{
-			set_stop(table, 1);
-			return (0);
-		}
+			return (set_stop(table, 1), 0);
 		usleep(1000);
 	}
 	return (0);
