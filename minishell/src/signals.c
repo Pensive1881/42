e85d9a3bd9406ae054b0a1b19d6f93cf4b8010de
Pensive <lumberjackparade@hotmail.com>
2026-04-22 16:35:23 +0200

    commented out rl_replace_line in signals.c for minishell

--- minishell/src/signals.c
+++ minishell/src/signals.c
@@ -11,7 +11,8 @@ static void sigint_handler(int signo)
 	g_signal = signo;
 	write(1, "\n", 1);
 	rl_on_new_line();
-	rl_replace_line("", 0);
+// removed for mac compiling, readline.h linking
+//	rl_replace_line("", 0);
 	rl_redisplay();
 }
 
