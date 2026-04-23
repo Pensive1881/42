0a054dbf50c1d9a81efccf0bd85938169af81c7b
Pensive <lumberjackparade@hotmail.com>
2026-04-23 10:06:06 +0200

    updated redirections to comply with heredoc changes in execution pipel ine

--- minishell/src/exec/redirections.c
+++ minishell/src/exec/redirections.c
@@ -22,32 +22,6 @@ static int	open_redir_file(t_redir *redir)
 		fd = open(redir->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	else if (redir->type == R_APPEND)
 		fd = open(redir->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-	else if (redir->type == R_HEREDOC)
-	{
-		int pipefd[2];
-		char	*line;
-
-		if (pipe(pipefd) < 0)
-			return (perror("pipe"), -1);
-
-		while (1)
-		{
-			ft_putstr_fd("> ", 2);
-			line = readline("");
-			if (!line)
-				break;
-			if (ft_strncmp(line, redir->file, ft_strlen(redir->file) + 1) == 0)
-			{
-				free(line);
-				break;
-			}
-			write(pipefd[1], line, ft_strlen(line));
-			write(pipefd[1], "\n", 1);
-			free(line);
-		}
-		close(pipefd[1]);
-		return (pipefd[0]);
-	}
 	if (fd < 0)
 		perror(redir->file);
 	return (fd);
@@ -62,7 +36,7 @@ int	apply_redirections(t_redir *redirs)
 		fd = open_redir_file(redirs);
 		if (fd < 0)
 			return (0);
-		if (redirs->type == R_IN || redirs->type == R_HEREDOC)
+		if (redirs->type == R_IN)
 		{
 			if (dup2(fd, STDIN_FILENO) < 0)
 				return (close(fd), perror("dup2"), 0);
