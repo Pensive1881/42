2137dde6a5eee1e22696571b7f634b8fa960363e
Pensive <lumberjackparade@hotmail.com>
2026-04-24 16:54:24 +0200

    made memcmp in exam_rank_03

---
+++ _exams/exam_rank_03/ft_memcmp.c
@@ -0,0 +1,17 @@
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	const unsigned char	*a;
+	const unsigned char	*b;
+
+	a = s1;
+	b = s2;
+	i = 0;
+	while(i < n)
+	{
+		if (a[i] != b[i])
+			return (a[i] - b[i]);
+		i++;
+	}
+	return (0);
+}
