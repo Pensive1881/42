7e7b62f4db14b5cea2bbc6c78611e8ac85bff873
Pensive <lumberjackparade@hotmail.com>
2026-05-22 18:23:23 +0200

    split oversized function in execute_multi.c for norminette

--- minishell/src/exec/execute_multi.c
+++ minishell/src/exec/execute_multi.c
@@ -3,13 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   execute_multi.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: akasper <akasper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 21:00:27 by acasper           #+#    #+#             */
-/*   Updated: 2026/03/31 21:00:31 by acasper          ###   ########.fr       */
+/*   Updated: 2026/05/22 18:21:12 by akasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "../../includes/minishell.h"
+
 
 static int	count_cmds(t_cmd *cmds)
 {
@@ -44,16 +44,55 @@ static void	wait_all(t_shell *shell, pid_t *pids, int count)
 	}
 }
 
+static int	create_pipe_if_needed(t_cmd *cmd, int pipefd[2])
+{
+	if (cmd->next && pipe(pipefd) < 0)
+	{
+		perror("pipe");
+		return (0);
+	}
+	return (1);
+}
+
+static void	run_child(t_shell *shell, t_cmd *cmd, int prev_read, int pipefd[2])
+{
+	if (cmd->next)
+		child_exec(shell, cmd, prev_read, pipefd[1]);
+	else
+		child_exec(shell, cmd, prev_read, STDOUT_FILENO);
+}
+
+static int	fork_one(t_shell *shell, t_cmd *cmd, int prev_read, int pipefd[2], pid_t *pid)
+{
+	*pid = fork();
+	if (*pid < 0)
+	{
+		perror("fork");
+		return (0);
+	}
+	if (*pid == 0)
+		run_child(shell, cmd, prev_read, pipefd);
+	return (1);
+}
+
+static void	parent_close_fds(t_cmd *cmd, int *prev_read, int pipefd[2])
+{
+	close_fd_if_needed(*prev_read);
+	if (cmd->next)
+	{
+		close(pipefd[1]);
+		*prev_read = pipefd[0];
+	}
+}
+
 void	execute_multi_command(t_shell *shell, t_cmd *cmds)
 {
 	int		prev_read;
 	int		pipefd[2];
 	pid_t	*pids;
 	int		i;
-	int		count;
 	t_cmd	*cur;
 
-	count = count_cmds(cmds);
 	pids = malloc(sizeof(pid_t) * count);
 	if (!pids)
 		return ;
@@ -62,43 +101,15 @@ void	execute_multi_command(t_shell *shell, t_cmd *cmds)
 	cur = cmds;
 	while (cur)
 	{
-		if (cur->next && pipe(pipefd) < 0)
-		{
-			close_fd_if_needed(prev_read);
-			free(pids);
-			perror("pipe");
-			return ;
-		}
-		pids[i] = fork();
-		if (pids[i] < 0)
-		{
-			close_fd_if_needed(prev_read);
-			if (cur->next)
-			{
-				close(pipefd[0]);
-				close(pipefd[1]);
-			}
-			free(pids);
-			perror("fork");
-			return ;
-		}
-		if (pids[i] == 0)
-		{
-			if (cur->next)
-				child_exec(shell, cur, prev_read, pipefd[1]);
-			else
-				child_exec(shell, cur, prev_read, STDOUT_FILENO);
-		}
-		close_fd_if_needed(prev_read);
-		if (cur->next)
-		{
-			close(pipefd[1]);
-			prev_read = pipefd[0];
-		}
+		if (!create_pipe_if_needed(cur, pipefd))
+			return (free(pids));
+		if (!fork_one(shell, cur, prev_read, pipefd, &pids[i]))
+			return (free(pids));
+		parent_close_fds(cur, &prev_read, pipefd);
 		cur = cur->next;
 		i++;
 	}
 	close_fd_if_needed(prev_read);
-	wait_all(shell, pids, count);
+	wait_all(shell, pids, i);
 	free(pids);
 }
