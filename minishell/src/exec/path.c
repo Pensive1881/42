e8c8874735c17707e0971a43c711d5ec24075beb
Pensive <lumberjackparade@hotmail.com>
2026-05-25 18:00:05 +0200

    split path.c oversized function to comply with norminette for minishell

--- minishell/src/exec/path.c
+++ minishell/src/exec/path.c
@@ -6,31 +6,10 @@
 /*   By: akasper <akasper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 21:01:12 by acasper           #+#    #+#             */
-/*   Updated: 2026/05/22 20:30:14 by akasper          ###   ########.fr       */
+/*   Updated: 2026/05/25 17:57:49 by akasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
-
-/*
-// removing duplicate function also found in src/buildins/env_utils.c"
-char	*get_env_value(char **envp, char *name)
-{
-	int		i;
-	size_t	len;
-
-	if (!envp || !name)
-		return (NULL);
-	len = ft_strlen(name);
-	i = 0;
-	while (envp[i])
-	{
-		if (ft_strncmp(envp[i], name, len) == 0 && envp[i][len] == '=')
-			return (envp[i] + len + 1);
-		i++;
-	}
-	return (NULL);
-}
-*/
 
 static char	*join_path_cmd(char *dir, char *cmd)
 {
@@ -56,12 +35,29 @@ static char	*check_direct_path(char *cmd)
 	return ((char *)1);
 }
 
+static char	*search_paths(char **paths, char *cmd)
+{
+	char	*full;
+	int	*i;
+
+	i = 0;
+	while (pahts[i])
+	{
+		full = join_path_cmd(paths[i], cmd);
+		if (full && access(full, X_OK) == 0)
+			return (free_args(paths, full));
+		free(full);
+		i++;
+	}
+	free_args(paths);
+	return (NULL);
+}
+
 char	*find_command_path(char *cmd, char **envp)
 {
 	char	**paths;
 	char	*path_env;
 	char	*full;
-	int		i;
 
 	if (!cmd)
 		return (NULL);
@@ -74,15 +70,5 @@ char	*find_command_path(char *cmd, char **envp)
 	paths = ft_split(path_env, ':');
 	if (!paths)
 		return (NULL);
-	i = 0;
-	while (paths[i])
-	{
-		full = join_path_cmd(paths[i], cmd);
-		if (full && access(full, X_OK) == 0)
-			return (free_args(paths), full);
-		free(full);
-		i++;
-	}
-	free_args(paths);
-	return (NULL);
+	return (search_paths(paths, cmd));
 }
