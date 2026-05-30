7fd7ed37f39db84e9e4c893857eb3c43168a745f
Pensive <lumberjackparade@hotmail.com>
2026-05-30 23:00:08 +0200

    updated minishell.h includes to compile with Makefile changers

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -13,15 +13,14 @@
 # define MINISHELL_H
 
 # include "../libft/includes/libft.h"
-# include "arena.h"
 # include "lexer.h"
 # include "parser.h"
+# include <stdio.h>
 # include <errno.h>
 # include <fcntl.h>
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <signal.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <sys/wait.h>
 # include <unistd.h>
