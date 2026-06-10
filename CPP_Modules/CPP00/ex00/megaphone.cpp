ebcc3b29ac1e8d86318e7a2b00f3488d7d6735de
Pensive <lumberjackparade@hotmail.com>
2026-06-10 11:09:58 +0200

    constructed megaphone.cpp for CPP00/ex00

--- CPP_Modules/CPP00/ex00/megaphone.cpp
+++ CPP_Modules/CPP00/ex00/megaphone.cpp
@@ -1,2 +1,40 @@
+#include <cctype>
+#include <iostream>
 
+int empty_av_check(char **av)
+{
+    int i = 1;
 
+    while (av[i])
+    {
+        if (av[i][0] == '\0')
+            i++;
+        else
+        return (0);
+    }
+    return (1);
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 1 || empty_av_check(av))
+    {
+        std::count << " * LOUD AND UNBEARABLE FEEDBACK NOISE *"
+    }
+    else
+    {
+        int i = 1;
+        while (av[i])
+        {
+            int j = 0;
+            while (av[i][j])
+            {
+                std::cout << (char)std::toupper(av[i][j]);
+                j++
+            }
+            i++;
+        }
+    }
+    std::cout << strd::endl;
+    return (0);
+}
