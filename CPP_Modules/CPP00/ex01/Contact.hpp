65d4818b2a35be0a40d92a6159b30d6267cd11ed
Pensive <lumberjackparade@hotmail.com>
2026-06-11 18:39:40 +0200

    made Contact.hpp for C00/ex01/

--- CPP_Modules/CPP00/ex01/Contact.hpp
+++ CPP_Modules/CPP00/ex01/Contact.hpp
@@ -6,10 +6,27 @@
 class Contact
 {
     private:
-
+        std::string _firstName;
+        std::string _lastName;
+        std::string _nickName;
+        std::string _phoneNumber;
+        std::string _darkSecret;
 
     public:
+        Contact();
+        ~Contact();
 
+        voide setFirstName(std::string str)_;
+        void setLastName(std::string str);
+        void setNickName(std::string str);
+        void setPhoneNumber(std::string str);
+        void setDarkSecret(std::string str);
+
+        std::string getFirstName(void) const;
+        std::string getLastName(void) const;
+        std::string getNickName(void) const;
+        std::string getPhoneNumber(void) const;
+        std::string getDarkSecret(void) const;
 }
 
 #endif
