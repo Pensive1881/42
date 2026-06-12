8e34fd116f0d0403657b94963eec2ab70dbace8e
Pensive <lumberjackparade@hotmail.com>
2026-06-12 12:14:58 +0200

    created microshell.c and started working on solution

---
+++ _exams/exam_rank_04/microshell.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int	main(int ac, char **av, char **env)
+{
+	int	i;
+	int	fd[2];
+	int	tmp_fd;
+	int	pid;
+	int	status;
+	int	ret;
+
+
+
+	return (ret);
+}
