38c3291634014f6488740982d9ef3a69569c9406
Pensive <lumberjackparade@hotmail.com>
2026-06-18 08:52:43 +0200

    built solution to ft_popen.c for exam_rank_04

---
+++ _exams/exam_rank_04/ft_popen.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+int	ft_popen(const char *file, char *const argv[], char type)
+{
+	int	fd[2];
+	pid_t	pid;
+
+	if (!file || !argv || (type != 'r' && type != 'w'))
+		return (-1);
+	if (pipe(fd) == -1)
+		return (-1);
+	pid = fork();
+	if (pid == -1)
+	{
+		close(fd[0]);
+		close(fd[1]);
+		return (-1);
+	}
+	if (pid == 0)
+	{
+		if (type == 'r')
+		{
+			close(fd[0]);
+			dup2(fd[1], STDOUT_FILENO);
+			close(fd[1]);
+		}
+		else
+		{
+			close(fd[1]);
+			dup2(fd[0], STDIN_FILENO);
+			close(fd[0]);
+		}
+		execvp(file, argv);
+		exit(1);
+	}
+	if (type == 'r')
+	{
+		close(fd[1]);
+		return (fd[0]);
+	}
+	close(fd[0]);
+	return (fd[1]);
+}
