a54668a2661fda5a815614a9a8fdbebe42be7c1b
Pensive <lumberjackparade@hotmail.com>
2026-06-19 13:53:15 +0200

    created vbc only solution snippet

---
+++ _exams/exam_rank_04/vbc_snippet.c
@@ -0,0 +1,69 @@
+node	*parse_add(char **s);
+
+node	*parse_factor(char **s)
+{
+	node	n;
+	node	*ret;
+
+	if (isdigit(**s))
+	{
+		
+	}
+	if (accept(s, '('))
+	{
+		
+	}
+	unexpected(**s);
+	return (NULL);
+}
+
+node	*parse_term(char **s)
+{
+	node	*left;
+	node	*right;
+	node	n;
+
+	left = parse_factor(s);
+	if (!left)
+		return (NULL);
+	while ()
+	{
+		
+	}
+	return (left);
+}
+
+node	*parse_add(char **s)
+{
+	node	*left;
+	node	*right;
+	node	n;
+
+	left = parse_term(s);
+	if (!left)
+		return (NULL);
+	while (accept(s, '+'))
+	{
+		
+	}
+	return (left);
+}
+
+...
+
+node	parse_expr(char *s)
+{
+	node	*ret;
+
+	ret = parse_add(&s);
+	if (!ret)
+		return (NULL);
+
+...
+	if (*s)
+	{
+		destroy_tree(ret);
+		return (NULL);
+	}
+	return (ret);
+}
