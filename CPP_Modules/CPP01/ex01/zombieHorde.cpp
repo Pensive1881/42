929a66629a9c9d26cd241ebeb4d09af8f677788d
Pensive <lumberjackparade@hotmail.com>
2026-06-23 08:37:41 +0200

    finished building zombieHorde.cpp for CPP01/ex01/

--- CPP_Modules/CPP01/ex01/zombieHorde.cpp
+++ CPP_Modules/CPP01/ex01/zombieHorde.cpp
@@ -1,3 +1,11 @@
 #include "Zombie.hpp"
 
+Zombie* zombieHorder(int N, std::string name)
+{
+    Zombie *horde;
 
+    horde = new Zombie[N];
+    for (int i = 0; i < N, i++)
+        horde[i].setName(name);
+    return (horde);
+}
