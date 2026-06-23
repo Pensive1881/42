ae67d5d443c261102acb223a502e263833ca6769
Pensive <lumberjackparade@hotmail.com>
2026-06-23 08:43:26 +0200

    finished building main.cpp for CPP01/ex01/

--- CPP_Modules/CPP01/ex01/main.cpp
+++ CPP_Modules/CPP01/ex01/main.cpp
@@ -2,5 +2,14 @@
 
 int main(void)
 {
-    
+    Zombie *horde;
+
+    horde = zombieHorde(5, "Walker");
+
+    for (int i = 0; i < 5; i++)
+        horde[i].annouce();
+
+    delete[] horde;
+
+    return (0);
 }
