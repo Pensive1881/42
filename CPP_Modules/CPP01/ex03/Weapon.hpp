d7d3686033b5367520e07a271b460392adada291
Pensive <lumberjackparade@hotmail.com>
2026-06-24 20:55:40 +0200

    finished building Weapon.hpp for CPP01/ex02/

--- CPP_Modules/CPP01/ex03/Weapon.hpp
+++ CPP_Modules/CPP01/ex03/Weapon.hpp
@@ -5,7 +5,15 @@
 
 class Weapon
 {
-    
-}
+    private:
+        std::string type;
+
+    public:
+        Weapon(std::string type);
+        ~Weapon();
+
+        const std::string& getType(void) const;
+        void setType(std::string type);
+};
 
 #endif
