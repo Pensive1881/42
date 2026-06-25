0671ad6ebaec264b4998582f9b2e3d1062c49356
Pensive <lumberjackparade@hotmail.com>
2026-06-25 15:15:13 +0200

    finished building HumanA.hpp for CP01/ex03/

--- CPP_Modules/CPP01/ex03/HumanA.hpp
+++ CPP_Modules/CPP01/ex03/HumanA.hpp
@@ -6,7 +6,15 @@
 
 class HumanA
 {
-    
+    private:
+        std::string name;
+        Weapon &weapon;
+        
+    public:
+        HumanA(std::string name, Weapon &weapon);
+        ~HumanA();
+
+        void attack(void);
 };
 
 #endif
