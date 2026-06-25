315e1945d1b4d7bfb40263ca1bc54c0d049bcbb3
Pensive <lumberjackparade@hotmail.com>
2026-06-25 18:38:29 +0200

    finished building HumanB.cpp for CP01/ex03/

--- CPP_Modules/CPP01/ex03/HumanB.cpp
+++ CPP_Modules/CPP01/ex03/HumanB.cpp
@@ -1,3 +1,23 @@
 #include "HumanB.hpp"
 #include <iostream>
 
+HumanB::HumanB(std::string name) : name(name), weapon(NULL)
+{
+}
+
+HumanB::~HumanB()
+{
+}
+
+void HumanB::setWeapon(Weapon &weapon)
+{
+    this->weapon = &weapon;
+}
+
+void HumanB::attack(void)
+{
+    if (weapon)
+        std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+    else
+        std::cout << name << " has no weapon." << std::endl;
+}
