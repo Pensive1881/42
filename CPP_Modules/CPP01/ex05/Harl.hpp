f26676af1046929d6ca27366e9ef57420d85e574
Pensive <lumberjackparade@hotmail.com>
2026-06-30 10:23:58 +0200

    finished building Harl.hpp for CPP01/ex05/

--- CPP_Modules/CPP01/ex05/Harl.hpp
+++ CPP_Modules/CPP01/ex05/Harl.hpp
@@ -5,7 +5,17 @@
 
 class Harl
 {
+    private:
+        void    debug(void);
+        void    info(void);
+        void    warning(void);
+        void    error(void);
 
+    public:
+        Harl();
+        ~Harl();
+
+        void    complain(std::string level);
 };
 
 #endif
