c298573c899b8384e01738ec79d074e12ef5d10a
Pensive <lumberjackparade@hotmail.com>
2026-06-30 09:45:19 +0200

    finished building main.cpp for CPP01/ex04

--- CPP_Modules/CPP01/ex04/main.cpp
+++ CPP_Modules/CPP01/ex04/main.cpp
@@ -2,8 +2,23 @@
 #include <iostream>
 #include <string>
 
-int main()
+int main(int argc, char **argv)
 {
-    
+    if (argc != 4)
+    {
+        std::cout << "Usage: ./replace <filename> >s1> <s2>" << std::endl;
+        return (1);
+    }
+
+    std::string filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    if (s1.empty())
+    {
+        std:cout << "Error: s1 cannot be empty." << std::endl;
+        return (1);
+    }
+
     return (0);
 }
