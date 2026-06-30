e5708f6a1148d51d79675c5ceb45cefb159cb839
Pensive <lumberjackparade@hotmail.com>
2026-06-30 18:46:37 +0200

    finished making main.cpp for CPP01/ex05

--- CPP_Modules/CPP01/ex05/main.cpp
+++ CPP_Modules/CPP01/ex05/main.cpp
@@ -2,6 +2,12 @@
 
 int main(void)
 {
-    
+    Harl harl;
+
+    harl.complain("DEBUG");
+    harl.compalin("INFO");
+    harl.compalin("WARNING");
+    harl.complain("ERROR");
+
     return (0);
 }
