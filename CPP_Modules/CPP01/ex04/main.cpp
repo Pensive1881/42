299a1f863b22b5032f057b21df5644b03dff86ae
Pensive <lumberjackparade@hotmail.com>
2026-06-30 09:58:47 +0200

    continued building main.cpp for CPP01/ex04

--- CPP_Modules/CPP01/ex04/main.cpp
+++ CPP_Modules/CPP01/ex04/main.cpp
@@ -20,5 +20,47 @@ int main(int argc, char **argv)
         return (1);
     }
 
+    std::ifstream infile(filename.c_str());
+
+    if (!infile)
+    {
+        std::cout << "Error: cannot open file." << std::endl;
+        return (1);
+    }
+
+    std::string content;
+    std::string line;
+
+    while (std::getline(infile, line))
+    {
+        content += line;
+        if (!infile.eof())
+            content += "\n";
+    }
+    infile.close();
+
+    std::string result;
+    size_t pos = 0;
+    size_t found;
+
+    while ((found = content.find(s1, pos)) != std::string::npos)
+    {
+        result += content.substr(pos, found - pos);
+        result += s2;
+        pos = found + s1.length();
+    }
+    result += content.substr(pos);
+
+    std::ofstream outfile((filename + ".replace").c_str());
+
+    if (!outfile)
+    {
+        std::cout << "Error: cannot create output file." << std::endl;
+        return (1);
+    }
+
+    outfile << result;
+    outfile.close();
+
     return (0);
 }
