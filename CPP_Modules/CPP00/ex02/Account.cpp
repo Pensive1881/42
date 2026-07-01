0f1ee681d8d956c619e0e3dba4cbfc50afb4f6e3
Pensive <lumberjackparade@hotmail.com>
2026-07-01 12:14:15 +0200

    continued making Account::_displayTimestamp function for Account.cpp for CPP00/ex02/

--- CPP_Modules/CPP00/ex02/Account.cpp
+++ CPP_Modules/CPP00/ex02/Account.cpp
@@ -10,7 +10,21 @@ int Account::_totalNbWithddrawals = 0;
 
 Account:Account(int initial_deposit)
 {
+    std::time_t now;
+    std::tm *timeinfo;
 
+    now = std::time(NULL);
+    timeinfo = std::localtime(&now);
+    std::cout << "["
+              << std::setfill('0') << std::setw(4) <<timeinfo->tm_year + 1900
+              << std::setw(2) << timeinfo->tm_mon + 1
+              << std::setw(2) << timeinfo->tm_mday
+              << "_"
+              << std::setw(2) << timeinfo->tm_hour
+              << std::setq(2) << timeinfo->tm_min
+              << std::setw(2) << timeinfo->tm_sec
+              << "] ";
+    std::cout << std:setfill(' ');
 }
 
 Account::~Account(void)
@@ -30,5 +44,5 @@ void    Account:makeWithdrawal(int withdrawal)
 
 int Account::checkAmount(void) const
 {
-    
+
 }
