91b3e878ea6e58e0e111ec4fa1695c5f5a31bb5f
Pensive <lumberjackparade@hotmail.com>
2026-07-01 11:43:46 +0200

    started function implementation without provided headerfile Account.cpp for CPP00/ex02/

--- CPP_Modules/CPP00/ex02/Account.cpp
+++ CPP_Modules/CPP00/ex02/Account.cpp
@@ -8,3 +8,27 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithddrawals = 0;
 
+Account:Account(int initial_deposit)
+{
+
+}
+
+Account::~Account(void)
+{
+
+}
+
+void    Account::makeDeposit(int deposit)
+{
+
+}
+
+void    Account:makeWithdrawal(int withdrawal)
+{
+
+}
+
+int Account::checkAmount(void) const
+{
+    
+}
