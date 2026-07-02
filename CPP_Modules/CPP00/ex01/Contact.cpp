03a11c69c26887f42dade643c92e096e0d329f48
Pensive <lumberjackparade@hotmail.com>
2026-07-02 18:19:40 +0200

    started working on Contact.cpp for CPP00/ex01/

---
+++ CPP_Modules/CPP00/ex01/Contact.cpp
@@ -0,0 +1,55 @@
+#include "Contact.hpp"
+
+Contact:Contact() {}
+
+Contact::~Contact() {}
+
+void Contact::setFristName()
+{
+
+}
+
+void Contact::setLastName()
+{
+
+}
+
+void Contact::setNickName()
+{
+
+}
+
+void Contact::setPhoneNumber()
+{
+
+}
+
+void Contact::setDarkSecret()
+{
+
+}
+
+std::string Contact::getFirstName(void) const
+{
+    
+}
+
+std::string Contact::getLastName(void) const
+{
+
+}
+
+std::strint Contact::getNickName(void) const
+{
+
+}
+
+std::string Contact:getPhoneNumber(void) const
+{
+
+}
+
+std::string Contact::getDarkSecret(void) const
+{
+    
+}
