b44d83bf99f2631b24eec7be64c6c29cd0ab0ec6
Pensive <lumberjackparade@hotmail.com>
2026-07-04 09:06:38 +0200

    finished building Fixed.hpp for CPP02/ex00/

--- CPP_Modules/CPP02/ex00/Fixed.hpp
+++ CPP_Modules/CPP02/ex00/Fixed.hpp
@@ -3,7 +3,19 @@
 
 class Fixed
 {
-    
+    private:
+        int _value
+        static const in _fractionalBits = 8;
+
+    public:
+        Fixed();
+        Fixed(const Fixed& other);
+        Fixed& operator =(const Fixed& other);
+        ~Fixed();
+
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+
 };
 
 #endif
