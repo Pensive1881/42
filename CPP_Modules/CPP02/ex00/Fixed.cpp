26587238435c4af0974736c1125e389acbd24f39
Pensive <lumberjackparade@hotmail.com>
2026-07-06 13:56:40 +0200

    corrected errors in Fixed.cpp for CPP02/ex00'

--- CPP_Modules/CPP02/ex00/Fixed.cpp
+++ CPP_Modules/CPP02/ex00/Fixed.cpp
@@ -1,14 +1,14 @@
 #include "Fixed.hpp"
 #include <iostream>
 
-Fixed::Fixed()
+Fixed::Fixed() : _value(0)
 {
     std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed& other)
 {
-    std::cout << "Copy constructor called" << std:endl;
+    std::cout << "Copy constructor called" << std::endl;
     *this = other;
 }
 
@@ -28,7 +28,7 @@ Fixed::~Fixed()
 int Fixed::getRawBits(void) const
 {
     std::cout << "getRawBits member function called" << std::endl;
-    return (this->value);
+    return (this->_value);
 }
 
 void Fixed::setRawBits(int const raw)
