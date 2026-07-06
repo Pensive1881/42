e5471a57cfc6562d901020511bd8fb85daf067fc
Pensive <lumberjackparade@hotmail.com>
2026-07-06 13:50:54 +0200

    constructed skeletal outline for Fixed.cpp for CPP02/ex00'

--- CPP_Modules/CPP02/ex00/Fixed.cpp
+++ CPP_Modules/CPP02/ex00/Fixed.cpp
@@ -1,3 +1,32 @@
 #include "Fixed.hpp"
 #include <iostream>
 
+Fixed::Fixed()
+{
+    
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+
+}
+
+Fixed& Fixed::operator=(const Fixed& other)
+{
+
+}
+
+Fixed::~Fixed()
+{
+
+}
+
+int Fixed::getRawBits(void) const
+{
+
+}
+
+void Fixed::setRawBits(int const raw)
+{
+
+}
