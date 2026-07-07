ef080f4298c4c4a7635c1c0b63b45ea742f20e82
Pensive <lumberjackparade@hotmail.com>
2026-07-07 20:14:52 +0200

    finished building Fixed.hpp for CPP02/ex02

--- CPP_Modules/CPP02/ex02/Fixed.hpp
+++ CPP_Modules/CPP02/ex02/Fixed.hpp
@@ -23,6 +23,28 @@ class Fixed
 
         float toFloat(void) const;
         int toInt(void) const;
+
+        bool operator>(const Fixed& other) const;
+        bool operator<(const Fixed& other) const;
+        bool operator>=(const Fixed& other) const;
+        bool operator<=(const Fixed& other) const;
+        bool operator== (const Fixed& other) const;
+        bool operator!=(const Fixed& other) const;
+
+        Fixed opeator+(const Fixed& other) const;
+        Fixed operator-(const Fixed& other) const;
+        Fixed operator*(const Fixed& other) const;
+        Fixed operator/(const Fixed& otehr) const;
+
+        Fixed& operator++();
+        Fixed operator++(int);
+        FIxed operator--();
+        Fixed operator--(int);
+
+        static Fixed& min(Fixed& a, Fixed& b);
+        static const Fixed& min(const Fixed& a, onst Fixed& b);
+        static Fixed& mac(Fixed& a, Fixed b);
+        static const Fixed& max(const Fixed& a, const Fixed& b);
 };
 
 std::ostream& operator<<(std::ostream& out, const Fixed& fixed);
