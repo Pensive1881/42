19bcbcc9f1387d438dbe49f5427d68e5bd0a6d50
Pensive <lumberjackparade@hotmail.com>
2026-07-07 20:18:38 +0200

    copied Fixed.cpp from ex01 to ex02

---
+++ CPP_Modules/CPP02/ex02/Fixed.cpp
@@ -0,0 +1,66 @@
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+Fixed::Fixed() : _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed::Fixed(const int value)
+{
+    std::cout << "Int constructor called" << std::endl;
+    this->_value = value * (1 << _fractionalBits);
+}
+
+Fixed::Fixed(const float value)
+{
+    std::cout << "Float constructor called" << std::endl;
+    this->_value = roundf(value * (1 << _fractionalBits));
+}
+
+Fixed& Fixed::operator=(const Fixed& other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other)
+        this->_value = other.getRawBits();
+    return (*this);
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (this->_value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->_value = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+    return (static_cast<float>(this->_value) / (1 << _fractionalBits));
+}
+
+int Fixed::toInt(void) const
+{
+    return (this->_value >> _fractionalBits);
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed)
+{
+    out << fixed.toFloat();
+    return (out);
+}
