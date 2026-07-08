3f3c5cb1c80625a354f6eb71edb39c9255cd1bc0
Pensive <lumberjackparade@hotmail.com>
2026-07-08 19:32:11 +0200

    commented out string outputsin Fixed.cpp from ex02

--- CPP_Modules/CPP02/ex03/Fixed.cpp
+++ CPP_Modules/CPP02/ex03/Fixed.cpp
@@ -4,30 +4,30 @@
 
 Fixed::Fixed() : _value(0)
 {
-    std::cout << "Default constructor called" << std::endl;
+//    std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed& other)
 {
-    std::cout << "Copy constructor called" << std::endl;
+//    std::cout << "Copy constructor called" << std::endl;
     *this = other;
 }
 
 Fixed::Fixed(const int value)
 {
-    std::cout << "Int constructor called" << std::endl;
+//    std::cout << "Int constructor called" << std::endl;
     this->_value = value * (1 << _fractionalBits);
 }
 
 Fixed::Fixed(const float value)
 {
-    std::cout << "Float constructor called" << std::endl;
+//    std::cout << "Float constructor called" << std::endl;
     this->_value = roundf(value * (1 << _fractionalBits));
 }
 
 Fixed& Fixed::operator=(const Fixed& other)
 {
-    std::cout << "Copy assignment operator called" << std::endl;
+//    std::cout << "Copy assignment operator called" << std::endl;
     if (this != &other)
         this->_value = other.getRawBits();
     return (*this);
@@ -35,12 +35,12 @@ Fixed& Fixed::operator=(const Fixed& other)
 
 Fixed::~Fixed()
 {
-    std::cout << "Destructor called" << std::endl;
+//    std::cout << "Destructor called" << std::endl;
 }
 
 int Fixed::getRawBits(void) const
 {
-    std::cout << "getRawBits member function called" << std::endl;
+//    std::cout << "getRawBits member function called" << std::endl;
     return (this->_value);
 }
 
