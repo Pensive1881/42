730c821072f7870c2b9d4f3b8102ee9804e2b508
Pensive <lumberjackparade@hotmail.com>
2026-07-08 19:19:52 +0200

    built outline for main.cpp for CPP02/ex03

--- CPP_Modules/CPP02/ex03/main.cpp
+++ CPP_Modules/CPP02/ex03/main.cpp
@@ -3,6 +3,19 @@
 
 int main(void)
 {
-    
+    Point a();
+    Point b();
+    Point c();
+
+    Point inside();
+    Point outside();
+    Point edge();
+    Point vertex();
+
+    std::cout << "" << bsp() << std::endl;
+    std::cout << "" << bsp() << std::endl;
+    std::cout << "" << bsp() << std::endl;
+    std::cout << "" << bsp() << std::endl;
+
     return (0);
 }
