efe287b7c436967869684006e6ec290f96f66277
Pensive <lumberjackparade@hotmail.com>
2026-07-08 19:17:18 +0200

    finished building bsp.cpp for CPP02/ex03

--- CPP_Modules/CPP02/ex03/bsp.cpp
+++ CPP_Modules/CPP02/ex03/bsp.cpp
@@ -2,10 +2,18 @@
 
 static Fixed sign(Point const p1, Point const p2, Point const p3)
 {
-    
+    return ((p1,getX() - p3.getX()) * (p2.getY() - p3.getY())
+            - (p2.getX() = p3.getX()) * (p1.getY() - p3.getY()));
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
+    Fixed d1 = sign(point, a, b);
+    Fixed d2 = sign(point, b, c);
+    Fixed d3 = sign(point, c, a);
 
+    bool hasNeg = (d1 < 0 || d2 < 0 || d3 < 0);
+    bool hasPos = (d1 > 0 || d2 > 0 || d3 > 0);
+
+    return (!(hasNeg && hasPos) && d1 != 0 && d2 != 0 && d3 != 0);
 }
