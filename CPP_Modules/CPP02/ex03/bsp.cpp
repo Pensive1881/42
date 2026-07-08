6c379b51c9946c150f7098647e39784789238601
Pensive <lumberjackparade@hotmail.com>
2026-07-08 18:52:22 +0200

    created bsp.cpp for CPP02/ex03

---
+++ CPP_Modules/CPP02/ex03/bsp.cpp
@@ -0,0 +1,11 @@
+#include "Point.hpp"
+
+static Fixed sign(Point const p1, Point const p2, Point const p3)
+{
+    
+}
+
+bool bsp(Point const a, Point const b, Point const c, Point const point)
+{
+
+}
