c7bbab08efbdf6718836062de58b0e9c85cff63b
Pensive <lumberjackparade@hotmail.com>
2026-07-08 10:16:04 +0200

    finished building Point.hpp for CPP02/ex03

--- CPP_Modules/CPP02/ex03/Point.hpp
+++ CPP_Modules/CPP02/ex03/Point.hpp
@@ -5,7 +5,24 @@
 
 class Point
 {
+    private:
+        const Fixed _x;
+        const Fixed _y;
 
+    public:
+        Point();
+        Point(const float x, const float y);
+        Point(const Point& other);
+        Point& operator=(const Point& other);
+        ~Point();
+
+        Fixed getX(void) const;
+        Fixed getY(coid) const;
 }
+
+bool bsp(Point const a,
+         Point const b,
+         Point const c,
+         Point const point);
 
 #endif
