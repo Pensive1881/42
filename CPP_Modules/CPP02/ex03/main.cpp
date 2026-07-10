f5022ebb5d0660cc2eaaabe86926fcbeccab7903
Pensive <lumberjackparade@hotmail.com>
2026-07-10 08:21:36 +0200

    fixed Inside return issue?

--- CPP_Modules/CPP02/ex03/main.cpp
+++ CPP_Modules/CPP02/ex03/main.cpp
@@ -7,7 +7,7 @@ int main(void)
     Point b(10.0f, 0.0f);
     Point c(5.0f, 10.0f);
 
-    Point inside(5.0f, 0.0f);
+    Point inside(5.0f, 5.0f);
     Point outside(10.0f, 10.0f);
     Point edge(5.0f, 0.0f);
     Point vertex(0.0f, 0.0f);
