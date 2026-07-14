bf9be31a43fb00b156fdb39e1f0bf3ed147d14d1
Pensive <lumberjackparade@hotmail.com>
2026-07-14 13:28:48 +0200

    continued building SlavTrap.cpp for CPP03/ex01

--- CPP_Modules/CPP03/ex01/ScavTrap.cpp
+++ CPP_Modules/CPP03/ex01/ScavTrap.cpp
@@ -1,9 +1,34 @@
-#include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 #include <iostream>
 
-int main()
+ScavTrap::ScavTrap() : ClapTrap()
 {
 
-    return 0;
 }
+
+ScavTrap::ScavTrap(const std::string& name) : ClapTrap(name)
+{
+
+}
+
+ScavTrap& ScavTrap::operator=(const ScavTrap& other)
+{
+
+}
+
+ScavTrap::~ScavTrap()
+{
+
+}
+
+void ScavTrap::attack(const std::string& target)
+{
+    
+}
+
+void ScavTrap::guardGate()
+{
+
+}
+
+
