baed5b0a83b5ce1d99b9ac9dcda1e61b04200ea6
Pensive <lumberjackparade@hotmail.com>
2026-07-15 16:21:41 +0200

    finished building FragTrap.hpp for CPP03/ex02

--- CPP_Modules/CPP03/ex02/FragTrap.hpp
+++ CPP_Modules/CPP03/ex02/FragTrap.hpp
@@ -5,7 +5,13 @@
 class FragTrap : public ClapTrap
 {
     public:
-        
+        FragTrap();
+        FragTrap(const std::string& name);
+        FragTrap(const FragTrap& other);
+        FragTrap& operator=(const FragTrap& other);
+        ~FragTrap();
+
+        void highFivesGuys();
 }
 
 #endif
