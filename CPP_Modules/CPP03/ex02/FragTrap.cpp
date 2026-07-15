40a90907187f2369b3e25a7c724bbd498ebdbf2e
Pensive <lumberjackparade@hotmail.com>
2026-07-15 16:50:29 +0200

    build basic structure of FragTrap.cpp for CPP03/ex02

--- CPP_Modules/CPP03/ex02/FragTrap.cpp
+++ CPP_Modules/CPP03/ex02/FragTrap.cpp
@@ -1,3 +1,35 @@
 #include "FragTrap.hpp"
 #include <iostream>
 
+FragTrap::FragTrap()
+    : ClapTrap()
+{
+
+}
+
+FragTrap::FragTrap(const std::string & name)
+    : ClapTrap(name)
+{
+
+}
+
+FragTrap::FragTrap(const FragTrap& other)
+    : ClapTrap(other)
+{
+
+}
+
+FragTrap& FragTrap::operator=(const FragTrap& other)
+{
+
+}
+
+FragTrap::~FragTrap()
+{
+
+}
+
+void FragTrap::highFivesGuys()
+{
+
+}
