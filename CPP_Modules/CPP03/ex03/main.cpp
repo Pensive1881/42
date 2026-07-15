bb320a450748b7fc9988e9d44719f1731c182f57
Pensive <lumberjackparade@hotmail.com>
2026-07-15 21:46:10 +0200

    finished building main.cpp for CPP03/ex02

--- CPP_Modules/CPP03/ex03/main.cpp
+++ CPP_Modules/CPP03/ex03/main.cpp
@@ -3,7 +3,27 @@
 
 int main()
 {
-    
+    std::cout << "=== DiamondTrap test ===" << std::endl;
+
+    DiamondTrap diamond("Shiny");
+
+    diamond.whoAmI();
+    diamond.attack("Enemy");
+    diamond.takeDamage(30);
+    diamond.beRepaired(10);
+    diamond.guardGat();
+    diamond.highFivesGuys();
+
+    std::cout << std::endl;
+    std::cout << "=== Copy test ===" << std::endl;
+
+    DiamondTrap copy(Diamond);
+
+    copy.whoAmI();
+    copy.attack("Another enemy");
+
+    std::cout << std::endl;
+    std::cout << "=== Destruction ===" << std::endl;
 
     return 0;
 }
