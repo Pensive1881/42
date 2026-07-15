eaf8173e0c869daf36bd66c18c86f0377dfde72e
Pensive <lumberjackparade@hotmail.com>
2026-07-15 18:52:59 +0200

    finished builde DiamondTrap.hpp for CPP03/ex02

--- CPP_Modules/CPP03/ex03/DiamondTrap.hpp
+++ CPP_Modules/CPP03/ex03/DiamondTrap.hpp
@@ -7,10 +7,18 @@
 class DiamondTrap : public ScavTrap, public FragTrap
 {
     privagte:
-
+        std::string _name;
 
     public:
-        
+        DiamondTrap();
+        DiamondTrap(const std::string& name);
+        DiamondTrap(const DiamondTrap& other);
+        DiamondTrap& operator=(const DiamondTrap& other);
+        ~DiamondTrap();
+
+        using ScavTrap::attack;
+
+        void whoAmI();
 }
 
 #endif
