f0b4d917a7ec705b15917dfffb8860a8c09e365d
Pensive <lumberjackparade@hotmail.com>
2026-07-15 18:35:33 +0200

    copied FragTrap.cpp from CPP03/ex02/ to ex03/

---
+++ CPP_Modules/CPP03/ex03/FragTrap.cpp
@@ -0,0 +1,53 @@
+#include "FragTrap.hpp"
+#include <iostream>
+
+FragTrap::FragTrap()
+    : ClapTrap()
+{
+    _name = "Default FragTrap";
+    _hitPoints = 100;
+    _energyPoints = 100;
+    _attackDamage = 30;
+
+    std::cout << "FragTrap default constructor called" << std::endl;
+}
+
+FragTrap::FragTrap(const std::string & name)
+    : ClapTrap(name)
+{
+    _hitPoints = 100;
+    _energyPoints = 100;
+    _attackDamage = 30;
+
+    std::cout << "FragTrap " << _name
+              << " constructor called" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap& other)
+    : ClapTrap(other)
+{
+    std::cout << "FragTrap copy constructor called" << std::endl;
+}
+
+FragTrap& FragTrap::operator=(const FragTrap& other)
+{
+    std::cout << "FragTrap copy assignment operator called" << std::endl;
+
+    if (this != &other)
+        ClapTrap::operator=(other);
+
+    return *this;
+}
+
+FragTrap::~FragTrap()
+{
+    std::cout << "FragTrap " << _name
+              << " destructor called" << std::endl;
+}
+
+void FragTrap::highFivesGuys()
+{
+    std::cout << "FragTrap " << _name
+              << " requests a positive high five!"
+              << std::endl;
+}
