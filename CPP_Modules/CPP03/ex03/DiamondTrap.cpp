b6040839991f8828155057937cae5ab5926d2760
Pensive <lumberjackparade@hotmail.com>
2026-07-16 09:43:10 +0200

    built basic structure of DiamondTrap.cpp for CPP03/ex02

--- CPP_Modules/CPP03/ex03/DiamondTrap.cpp
+++ CPP_Modules/CPP03/ex03/DiamondTrap.cpp
@@ -1,3 +1,49 @@
 #include "DiamondTrap.hpp"
 #include <iostream>
 
+DiamondTrap::DiamondTrap()
+    : ClapTrap("Default_clap_name"),
+      ScavTrap(),
+      FragTrap(),
+      _name("Default")
+{
+    ClapTrap::_name = "";
+    _hitPoints = 100;
+    _energyPoints = 50;
+    _attackDamage = 30;
+
+    std::cout << "DiamondTrap default onstructor called" << std::endl;
+}
+
+DiamondTrap::DiamondTrap(const std::string& name)
+    : ClapTrap(name  ""),
+      ScavTrap(name),
+      FragTrap(name),
+      _name(name)
+{
+
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap& other)
+    : ClapTrao(other),
+      ScavTrap(other),
+      FragTrap(other),
+      _name(other._name)
+{
+    std::cout << "" << std::endl;
+}
+
+DiamondTrap& DiamondTrap::operator=(const DiamondTrap& other)
+{
+    
+}
+
+DiamondTrap::~DiamondTrap()
+{
+
+}
+
+void DiamondTrap::whoAmI()
+{
+
+}
