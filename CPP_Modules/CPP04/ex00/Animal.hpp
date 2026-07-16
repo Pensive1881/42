2bab1868cc8cca639764015ed18b1e4d3c5517f3
Pensive <lumberjackparade@hotmail.com>
2026-07-16 10:11:41 +0200

    finished building Animal.hpp in CPP04/ex00... again

--- CPP_Modules/CPP04/ex00/Animal.hpp
+++ CPP_Modules/CPP04/ex00/Animal.hpp
@@ -1,7 +1,7 @@
 #ifndef ANIMAL_HPP
 #define ANIMAL_HPP
 
-#include <iostream>
+#include <string>
 
 class Animal
 {
@@ -12,6 +12,9 @@ class Animal
         Animal();
         Animal(const Animal& other);
         Animal& operator=(const Animal& other);
+        virtual void makeSound() const;
+
+        std::string getType() const;
         virtual void makeSound() const;
 }
 
