24e11d1254d2bb3568b91efb1bbc8d62c8dcf1a3
Pensive <lumberjackparade@hotmail.com>
2026-07-16 15:37:52 +0200

    finished building Animal.cpp in CPP04/ex00

--- CPP_Modules/CPP04/ex00/Animal.cpp
+++ CPP_Modules/CPP04/ex00/Animal.cpp
@@ -4,31 +4,36 @@
 Animal::Animal()
     : type("Animal")
 {
-
+    std::cout << "Animal default constructor called" << std::endl;
 }
 
 Animal::Animal(const Animal& other)
     : type(other.type)
 {
-
+    std::cout << "Animal copy constructor called" << std::endl;
 }
 
 Animal& Animal::operator=(const Animal& other)
 {
+    std::cout << "Animal copy assignment oeprator called" << std::endl;
 
+    if (this != &other)
+        type = other.type;
+
+    return *this;
 }
 
 Animal::~Animal()
 {
-
+    std::cout << "Animal destructor called" << std::endl;
 }
 
 std::string Animal::getType() const
 {
-
+    return type;
 }
 
 void Animal::makeSound() const
 {
-    
+    std::cout << "Animal makes an indistinct sound" << std::endl;
 }
