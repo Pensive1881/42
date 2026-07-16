89526392653b9dda1a020c021aaf33bd3f38b244
Pensive <lumberjackparade@hotmail.com>
2026-07-16 15:21:18 +0200

    starting making structural outline for Animal.cpp in CPP04/ex00

--- CPP_Modules/CPP04/ex00/Animal.cpp
+++ CPP_Modules/CPP04/ex00/Animal.cpp
@@ -1,3 +1,34 @@
 #include "Animal.hpp"
 #include <iostream>
 
+Animal::Animal()
+    : type("Animal")
+{
+
+}
+
+Animal::Animal(const Animal& other)
+    : type(other.type)
+{
+
+}
+
+Animal& Animal::operator=(const Animal& other)
+{
+
+}
+
+Animal::~Animal()
+{
+
+}
+
+std::string Animal::getType() const
+{
+
+}
+
+void Animal::makeSound() const
+{
+    
+}
