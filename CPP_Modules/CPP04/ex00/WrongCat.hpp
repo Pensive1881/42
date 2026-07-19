88f459fc00e73e9a1f61a8f19215bdbc5c42e1c4
Pensive <lumberjackparade@hotmail.com>
2026-07-19 14:49:03 +0200

    Finished building WrongCat.hpp for CPP04/ex00

--- CPP_Modules/CPP04/ex00/WrongCat.hpp
+++ CPP_Modules/CPP04/ex00/WrongCat.hpp
@@ -3,6 +3,15 @@
 
 #include "WrongAnimal.hpp"
 
+class WrongCat : public WrongAnimal
+{
+    public:
+        WrongCat();
+        WrongCat(const WrongCat& other);
+        WrongCat& operator=(const WrongCat& other);
+        ~WrongCat();
 
+        void makeSound() const;
+}
 
 #endif
