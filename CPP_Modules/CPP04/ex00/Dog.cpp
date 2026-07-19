bbf09ad5e5ffadd527248fb746daf32f24e4ed7c
Pensive <lumberjackparade@hotmail.com>
2026-07-19 11:25:56 +0200

    final error correction in Dog.cpp in CPP04/ex00

--- CPP_Modules/CPP04/ex00/Dog.cpp
+++ CPP_Modules/CPP04/ex00/Dog.cpp
@@ -5,7 +5,7 @@ Dog::Dog()
     : Animal()
 {
     type = "Dog";
-    std::cout << "Dog default constructor called" std::endl;
+    std::cout << "Dog default constructor called" << std::endl;
 }
 
 Dog::Dog(const Dog& other)
