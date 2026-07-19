e38410693a38c86b7a5fdb9a6962a4aa6e7f47fd
Pensive <lumberjackparade@hotmail.com>
2026-07-19 13:00:56 +0200

    finished building WrongAnimal.hpp for CPP04/ex00

--- CPP_Modules/CPP04/ex00/WrongAnimal.hpp
+++ CPP_Modules/CPP04/ex00/WrongAnimal.hpp
@@ -5,7 +5,17 @@
 
 class WrongAnimal
 {
-    
+    protected:
+        std::string type;
+
+    public:
+        WrongAnimal();
+        WrongAnimal(const WrongAnimal& other);
+        WrongAnimal& operator=(const WrongAnimal& other);
+        virtual ~WrongAnimal();
+
+        std::string getType() const;
+        void makeSound() const;
 }
 
 #endif
