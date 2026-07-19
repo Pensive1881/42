424890b8c45fcd69593c0a12aed1cce521fe640f
Pensive <lumberjackparade@hotmail.com>
2026-07-19 11:07:23 +0200

    make skelital outline for Dog.cpp in CPP04/ex00

--- CPP_Modules/CPP04/ex00/Dog.cpp
+++ CPP_Modules/CPP04/ex00/Dog.cpp
@@ -1,3 +1,29 @@
 #include "Dog.hpp"
 #include <iostream>
 
+Dog::Dog()
+    : Animal()
+{
+
+}
+
+Dog::Dog(const Dog& other)
+    : Animal(other)
+{
+
+}
+
+Dog& Dog::operator=()
+{
+
+}
+
+Dog::~Dog()
+{
+
+}
+
+void Dog::makeSound() const
+{
+    
+}
