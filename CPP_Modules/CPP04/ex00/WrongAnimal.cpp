4e04fa6882a48c09ffb2cfa89a804faf067ad985
Pensive <lumberjackparade@hotmail.com>
2026-07-19 13:30:58 +0200

    created skeletal outline for WrongAnimal.cpp for  CPP04/ex00

--- CPP_Modules/CPP04/ex00/WrongAnimal.cpp
+++ CPP_Modules/CPP04/ex00/WrongAnimal.cpp
@@ -1,4 +1,34 @@
 #include "WrongAnimal.hpp"
 #include <iostream>
 
+WrongAnimal::WrongAnimal()
+    : type("WrongAnimal")
+{
 
+}
+
+WrongAnimal::WrongAnimal(const WrongAnimal& other)
+    : type(other.type)
+{
+
+}
+
+WrongAnimal& WrongAnimal::operator=(const WrongAnimal& other)
+{
+
+}
+
+WrongAnimal::~WrongAnimal()
+{
+
+}
+
+std::string WrongAnimal::getType() const
+{
+
+}
+
+void WrongAnimal::makeSound() const
+{
+    
+}
