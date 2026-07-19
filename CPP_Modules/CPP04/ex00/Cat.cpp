cc2665bcb0ecdef4f176a608b3d6c434704cf1eb
Pensive <lumberjackparade@hotmail.com>
2026-07-19 12:26:54 +0200

    created skeletal outline for  Cat.cpp for  CPP04/ex00

--- CPP_Modules/CPP04/ex00/Cat.cpp
+++ CPP_Modules/CPP04/ex00/Cat.cpp
@@ -1,4 +1,29 @@
 #include "Cat.hpp"
 #include <iostream>
 
+Cat::Cat()
+    : Animal()
+{
+    
+}
 
+Cat::Cat(const Cat& other)
+    : Animal(other)
+{
+
+}
+
+Cat& Cat::operator=(const Cat& other)
+{
+
+}
+
+Cat::~Cat()
+{
+
+}
+
+void Cat::makeSound() const
+{
+    
+}
