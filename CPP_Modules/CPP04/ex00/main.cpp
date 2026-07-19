7828b265b24763ac8a82293c12cf103314ea9306
Pensive <lumberjackparade@hotmail.com>
2026-07-19 16:11:11 +0200

    Finished building main.cpp for CPP04/ex00

--- CPP_Modules/CPP04/ex00/main.cpp
+++ CPP_Modules/CPP04/ex00/main.cpp
@@ -7,6 +7,44 @@
 
 int main()
 {
+    std::cout << "=== Correct polymorphism ===" << std::endl;
+    
+    const Animal* animal = new Animal();
+    const Animal* dog = new Dog();
+    const Animal* cat = new Cat();
+
+    std::cout << "Type: " << animal->getType() << std::endl;
+    std::cout << "Type: " << dog->getType() << std::endl;
+    std::cout << "Type: " << cat->getType() << std::endl;
+
+    animal->makeSound();
+    dog->makeSound();
+    cat->makeSound();
+
+    delete animal;
+    delete dog;
+    delete cat;
+
+    std::cout << std::endl;
+    std::cout << "=== Wrong polymorphism ===" << std::endl;
+
+    const WrongAnimal* wrongAnimal = new WrongAnimal();
+    const WrongAnimal* wrongCat = new WrongCat();
+
+    std::cout << "Type: " << wrongAnimal->getType() << endl::endl;
+    std::cout << "Type: " << wrongCat->getType() << std::endl;
+
+    wrongAnimal->makeSound();
+    wrongCat->makeSound();
+
+    std::cout << std::endl;
+    std::cout << "=== Direct WrongCat call ==="
+
+    WrongCat directWrongCat;
+    directWrongCat.makeSound();
+
+    delete wrongAnimal;
+    delete wrongCat;
 
     return 0;
 }
