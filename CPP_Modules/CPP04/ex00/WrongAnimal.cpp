69fcc7efc0d18634165aeef73de9caeb7183bcec
Pensive <lumberjackparade@hotmail.com>
2026-07-19 14:30:46 +0200

    Finished building WrongAnimal.cpp for CPP04/ex00

--- CPP_Modules/CPP04/ex00/WrongAnimal.cpp
+++ CPP_Modules/CPP04/ex00/WrongAnimal.cpp
@@ -4,31 +4,36 @@
 WrongAnimal::WrongAnimal()
     : type("WrongAnimal")
 {
-
+    std::cout << "WrongAnimal default construtor called" << std::endl;
 }
 
 WrongAnimal::WrongAnimal(const WrongAnimal& other)
     : type(other.type)
 {
-
+    std::cout << "WrongAnimal copy constructor called" << std::endl;
 }
 
 WrongAnimal& WrongAnimal::operator=(const WrongAnimal& other)
 {
+    std::cout << "WrongAnimal coppy assignment oeprator called" << std::endl;
 
+    if (this != &other)
+        type = other.type;
+    
+    return *this;
 }
 
 WrongAnimal::~WrongAnimal()
 {
-
+    std::cout << "WrongAnimal destructor called" << std::endl;
 }
 
 std::string WrongAnimal::getType() const
 {
-
+    return type;
 }
 
 void WrongAnimal::makeSound() const
 {
-    
+    std::cout << "WrongAnimal makes an indistrinct stound" << std::endl;
 }
