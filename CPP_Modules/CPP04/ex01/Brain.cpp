d1b359fc4c46bbc00e2229e8bc3377ae15507168
Pensive <lumberjackparade@hotmail.com>
2026-07-20 17:50:59 +0200

    created skeletal outline for Brain.cpp for CPP04/ex01

--- CPP_Modules/CPP04/ex01/Brain.cpp
+++ CPP_Modules/CPP04/ex01/Brain.cpp
@@ -1,3 +1,32 @@
 #include "Brain.hpp"
 #include <iostream>
 
+Brain:Brain()
+{
+    
+}
+
+Brain::Brain(const Brain& other)
+{
+
+}
+
+Brain& Brain::operator=(conwst Brain& other)
+{
+
+}
+
+Brain::~Brain()
+{
+
+}
+
+void Brain::setIdea(unsigned int index, const std::string& idea)
+{
+
+}
+
+std::string Brain::getIdea(unsigned int index) const
+{
+
+}
