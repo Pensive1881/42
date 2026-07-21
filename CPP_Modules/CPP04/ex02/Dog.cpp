b61d824ae9a6b1afe89750639907aee42a4a75eb
Pensive <lumberjackparade@hotmail.com>
2026-07-21 20:48:47 +0200

    copied Dog header and cpp from CP04/ex01 to ex02

---
+++ CPP_Modules/CPP04/ex02/Dog.cpp
@@ -0,0 +1,49 @@
+#include "Dog.hpp"
+#include <iostream>
+
+Dog::Dog()
+    : Animal(), brain(new Brain())
+{
+    type = "Dog";
+    std::cout << "Dog default constructor called" << std::endl;
+}
+
+Dog::Dog(const Dog& other)
+    : Animal(other), brain(new Brain(*other.brain))
+{
+    std::cout << "Dog copy constructor called" << std::endl;
+}
+
+Dog& Dog::operator=(const Dog& other)
+{
+    std::cout << "Dog copy assignment operator called" << std::endl;
+
+    if (this != &other)
+    {
+        Animal::operator=(other);
+        *brain = *other.brain;
+    }
+
+    return *this;
+}
+
+Dog::~Dog()
+{
+    delete brain;
+    std::cout << "Dog destructor called" << std::endl;
+}
+
+void Dog::makeSound() const
+{
+    std::cout << "Woof!" << std::endl;
+}
+
+Brain* Dog::getBrain()
+{
+    return brain;
+}
+
+const Brain* Dog::getBrain() const
+{
+    return brain;
+}
