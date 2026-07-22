e17d7779c761c0e61f9033a160c09a92a2a8ab07
Pensive <lumberjackparade@hotmail.com>
2026-07-22 15:29:12 +0200

    finished building ICharacgter.hpp for CPP04/ex03

--- CPP_Modules/CPP04/ex03/ICharacter.hpp
+++ CPP_Modules/CPP04/ex03/ICharacter.hpp
@@ -3,6 +3,17 @@
 
 #include <string>
 
+class AMateria;
 
+class ICharacter
+{
+    public:
+        virtual ~ICharacter() {}
+
+        virtual const std::string& getName() const = 0;
+        virtual void equip(AMateria* materia) = 0;
+        virtual void unequip(int index) = 0;
+        virtual void use(int index, ICharacter& target) = 0;
+}
 
 #endif
