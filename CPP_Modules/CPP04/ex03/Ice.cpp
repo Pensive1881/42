3121a62565bfcdcb2e63d95673f71a779df6e191
Pensive <lumberjackparade@hotmail.com>
2026-07-22 23:22:43 +0200

    Finished building Ice.cpp for CPP04/ex03

--- CPP_Modules/CPP04/ex03/Ice.cpp
+++ CPP_Modules/CPP04/ex03/Ice.cpp
@@ -14,7 +14,10 @@ Ice::Ice(const Ice& other)
 
 Ice& Ice:: operator=(Const Ice& other)
 {
+    if (this != &other)
+        AMateria::operator=(other);
 
+    return *this;
 }
 
 Ice::~Ice()
@@ -23,10 +26,12 @@ Ice::~Ice()
 
 AMateria* Ice::clone() const
 {
-
+    return new Ice(*this);
 }
 
 void Ice::use(ICharacter& target)
 {
-
+    std::cout << "* shoots an ice bolt at "
+              << target.getName()
+              << " *" << std::endl;
 }
