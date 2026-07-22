459903df8524723efdefd6af2ed770ff4ffb6dd7
Pensive <lumberjackparade@hotmail.com>
2026-07-22 09:47:10 +0200

    finished building Amateria.hpp for CPP04/ex03

--- CPP_Modules/CPP04/ex03/Amateria.hpp
+++ CPP_Modules/CPP04/ex03/Amateria.hpp
@@ -3,6 +3,23 @@
 
 #include <string>
 
+class ICharacter;
 
+class AMateria
+{
+    protected:
+        std::string type;
+
+    public:
+        AMateria(const std::string& type);
+        AMateria(const AMateria& other);
+        AMateria& operator=(const AMateria& other);
+        virtual ~AMateria();
+
+        const std::string& getType() const;
+
+        virtual AMateria* clone() const = 0;
+        virtual void use(ICharacter& target);
+}
 
 #endif
