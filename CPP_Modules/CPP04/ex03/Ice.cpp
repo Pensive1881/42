907ab30e8a910859f6dd61be7ea8d911f6e51d7e
Pensive <lumberjackparade@hotmail.com>
2026-07-22 15:54:08 +0200

    created skeletal outline of Ice.cpp for CPP04/ex03

--- CPP_Modules/CPP04/ex03/Ice.cpp
+++ CPP_Modules/CPP04/ex03/Ice.cpp
@@ -2,3 +2,31 @@
 #include "ICharacter.hpp"
 #include <iostream>
 
+Ice::Ice()
+    : AMaterial("ice")
+{
+}
+
+Ice::Ice(const Ice& other)
+    : AMateria(other)
+{
+}
+
+Ice& Ice:: operator=(Const Ice& other)
+{
+
+}
+
+Ice::~Ice()
+{
+}
+
+AMateria* Ice::clone() const
+{
+
+}
+
+void Ice::use(ICharacter& target)
+{
+
+}
