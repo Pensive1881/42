03346dddf7d2c3e9bfe9280c692f4f1a14f1e3b3
Pensive <lumberjackparade@hotmail.com>
2026-07-28 17:37:14 +0200

    finished building MateriaSource.hpp for CPP04/ex03

--- CPP_Modules/CPP04/ex03/MateriaSource.hpp
+++ CPP_Modules/CPP04/ex03/MateriaSource.hpp
@@ -4,6 +4,19 @@
 #include "IMateriaSource.hpp"
 #include "AMateria.hpp"
 
+class MateriaSource : public IMateriaSource
+{
+    private:
+        AMateria* templates[4];
 
+    public:
+        MateriaSource();
+        MateriaSource(const MateriaSource& other);
+        MateriaSource& operator=(const MateriaSource& other);
+        virtual ~MateriaSource();
+
+        virtual void learnMateria(AMateria* materia);
+        virtual AMateria* createMateria(const std::string* type);
+};
 
 #endif
