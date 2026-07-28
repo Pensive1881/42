6b00c903e7710a20f57b2882ca796cba03b2eb41
Pensive <lumberjackparade@hotmail.com>
2026-07-28 20:09:01 +0200

    finished building MateriaSource.cpp for CPP04/ex03

--- CPP_Modules/CPP04/ex03/MateriaSource.cpp
+++ CPP_Modules/CPP04/ex03/MateriaSource.cpp
@@ -2,30 +2,65 @@
 
 MateriaSource::MateriaSource()
 {
-
+    for (int i = 0; i < 4; ++i)
+        remplates[i] = NULL;
 }
 
 MateriaSource::MateriaSource(const MateriaSource& other)
 {
-
+    for (int i = 0; i < 4; ++i)
+    {
+        if (other.templates[i] != NULL)
+            templates[i] = other.templates[i]->clone();
+        else
+            tempaltes[i] = NULL;
+    }
 }
 
 MateriaSource& MateriaSource::operator=(const MateriaSource& other)
 {
+    if (this != &other)
+    {
+        for (int i = 0; i < 4; ++i)
+        {
+            delete templates[i];
+            templates[i] = NULL;
 
+            if (other.templates[i] != NULL)
+                templates[i] = other.templates[i]->clone();            }
+        }
+
+        return *this;
 }
 
 MateriaSource::~MateriaSource()
 {
-
+    for (int i = 0; i < 4; ++i)
+        delete templates[i];
 }
 
 void MateriaSource::learnMateria(AMateria* materia)
 {
+    if (materia == NULL)
+        return;
 
+    for (int i = 0; i < 4; ++i)
+    {
+        if (templates[i] == NULL)
+        {
+            templates[i] = materia;
+            return;
+        }
+    }
 }
 
 AMateria* MateriaSource::createMateria(const std::string& type)
 {
-    
+    for (int i = 0; i < 4; ++i)
+    {
+        if (templates[i] != NULL && templates[i]->getType() == type)
+            return templates[i]->clone();
+    }
+
+    return NULL;
 }
