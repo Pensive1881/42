b9896e87872f00d0c25dd28ac790339807c8be40
Pensive <lumberjackparade@hotmail.com>
2026-07-28 17:48:11 +0200

    constructed Skeletal archetecture for MateriaSource.cpp for CPP04/ex03

--- CPP_Modules/CPP04/ex03/MateriaSource.cpp
+++ CPP_Modules/CPP04/ex03/MateriaSource.cpp
@@ -1,2 +1,31 @@
 #include "MateriaSource.hpp"
 
+MateriaSource::MateriaSource()
+{
+
+}
+
+MateriaSource::MateriaSource(const MateriaSource& other)
+{
+
+}
+
+MateriaSource& MateriaSource::operator=(const MateriaSource& other)
+{
+
+}
+
+MateriaSource::~MateriaSource()
+{
+
+}
+
+void MateriaSource::learnMateria(AMateria* materia)
+{
+
+}
+
+AMateria* MateriaSource::createMateria(const std::string& type)
+{
+    
+}
