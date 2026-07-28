7dca86ca79824ee4b446eb19df5ef590760549de
Pensive <lumberjackparade@hotmail.com>
2026-07-28 13:21:06 +0200

    finished building Character.hpp for CPP04/ex03

--- CPP_Modules/CPP04/ex03/Character.hpp
+++ CPP_Modules/CPP04/ex03/Character.hpp
@@ -5,6 +5,22 @@
 #include "AMateria.hpp"
 #include <string>
 
+class Character : public ICharacter
+{
+    private:
+        std::string name;
+        AMateria* inventory[4];
 
+    public:
+        Character(const std::string& name);
+        Character(const Character& other);
+        Character& other=(const Character& other);
+        virtual ~Character();
+
+        virtual const std::string& getName() const;
+        virtual void equip(AMateria* materia);
+        virtual void unquip(int index);
+        virtual void use(int index, ICharacter& target);
+};
 
 #endif
