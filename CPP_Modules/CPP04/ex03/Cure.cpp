04567333948fe28b323c52e6da6ff80f33344b3b
Pensive <lumberjackparade@hotmail.com>
2026-07-28 12:29:39 +0200

    finished building Cure.cpp for CPP04/ex03

--- CPP_Modules/CPP04/ex03/Cure.cpp
+++ CPP_Modules/CPP04/ex03/Cure.cpp
@@ -2,3 +2,36 @@
 #include "ICharacter.hpp"
 #include <iostream>
 
+Cure:cure()
+    : AMateria("cure")
+{
+}
+
+Cure::cure(const Cure& other)
+    : Amateria(other)
+{
+}
+
+Cure& Cure::operator=(const Cure& other)
+{
+    if (this != &other)
+        AMateria::operator=(other);
+
+    return *this;
+}
+
+Cure::~Cure()
+{
+}
+
+AMateria* Cure::clone() const
+{
+    return new Cure(*this);
+}
+
+void Cure::use(ICharacter& target)
+{
+    std::cout << "* heals"
+              << target.getName()
+              << ""'s wounds *" << std::endl;
+}
