4e92de1bc2412c164bf6e4a2a81b80f5c207f4c8
Pensive <lumberjackparade@hotmail.com>
2026-07-28 20:11:32 +0200

    created main.cpp for CPP04/ex03

---
+++ CPP_Modules/CPP04/ex03/main.cpp
@@ -0,0 +1,13 @@
+#include "IMateriaSource.hpp"
+#include "MateriaSource.hpp"
+#include "ICharacter.hpp"
+#include "Character.hpp"
+#include "Ice.hpp"
+#include "Cure.hpp"
+
+int main()
+{
+
+
+    return 0;
+}
