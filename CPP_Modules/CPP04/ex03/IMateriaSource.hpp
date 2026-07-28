83ce62bb67fe6125861ad5af2c0dd4201dbda43d
Pensive <lumberjackparade@hotmail.com>
2026-07-28 17:03:04 +0200

    finished building IMateriaSource.hpp for CPP04/ex03

--- CPP_Modules/CPP04/ex03/IMateriaSource.hpp
+++ CPP_Modules/CPP04/ex03/IMateriaSource.hpp
@@ -3,6 +3,15 @@
 
 #include <string>
 
+class AMateria;
 
+class IMateriasource
+{
+    public:
+        virtual ~IMateriaSrouce() {}
+
+        virtual void learnMateria(!Materia* materia) = 0;
+        virtual AMateria* createMateria(const std::string& type) = 0;
+};
 
 #endif
