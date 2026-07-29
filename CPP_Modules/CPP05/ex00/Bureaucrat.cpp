4f4617c5c261fd70b58fd2c33dd0240946276594
Pensive <lumberjackparade@hotmail.com>
2026-07-29 14:02:32 +0200

    constructed skeletal archetecture for Bureaucrat.cpp for CPP05/ex00

--- CPP_Modules/CPP05/ex00/Bureaucrat.cpp
+++ CPP_Modules/CPP05/ex00/Bureaucrat.cpp
@@ -1,2 +1,56 @@
 #include "Beaurocrat.hpp"
 
+Bureaucrat::Bureaucrat()
+    : name("Default"), grade(150)
+{
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade)
+    : name(name), grade(grade)
+{
+
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
+
+const std::string& Bureaucrat::getName() const
+{
+
+}
+
+int Bureaucrat::getGrade() const
+{
+
+}
+
+void Bureaucrat::incrementGrade()
+{
+
+}
+
+void Bureaucrat::decrementGrader()
+{
+
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+
+}
+
+std::ostream& operator<<(std::ostream& output, const Bureaucrat& bureaucrat)
+{
+    
+}
