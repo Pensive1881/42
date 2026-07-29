a8c719fb1434b9d663f4e3a090ed3610872042e4
Pensive <lumberjackparade@hotmail.com>
2026-07-29 10:52:17 +0200

    finished building Bureaucrat.hpp for CPP05/ex00

--- CPP_Modules/CPP05/ex00/Bureaucrat.hpp
+++ CPP_Modules/CPP05/ex00/Bureaucrat.hpp
@@ -5,6 +5,30 @@
 #include <iostream>
 #include <exception>
 
+class Bureaucrat
+{
+    private:
+        const std::string name;
+        int grade;
 
+    public:
+        Bureaucrat();
+        Bureaucrat(const std::string& name, int grade);
+        Bureaucrat(const Bureaucrat& other);
+        Bureaucrat& operator=(constg Bureaucrat& other);
+        ~Bureaucrat();
+
+        const std::string& getName() const;
+        int getGrade() const;
+
+        void incrementGrade();
+        void decrementGrade();
+
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
+};
 
 #endif
