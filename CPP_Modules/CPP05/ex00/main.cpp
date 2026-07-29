c1098e231bb4603ce84ccfff38dfdc070fcb17ba
Pensive <lumberjackparade@hotmail.com>
2026-07-29 16:48:25 +0200

    finished building main.cpp for CPP05/ex00

--- CPP_Modules/CPP05/ex00/main.cpp
+++ CPP_Modules/CPP05/ex00/main.cpp
@@ -3,6 +3,68 @@
 
 int main()
 {
-    
+    std::cout << "=== Valid bureaucrat ===" << std::endl;
+
+    try
+    {
+        bureaucrat bob("Bob", 2);
+
+        std::cout << bob << std::endl;
+
+        bob.incrementGrade();
+        std::cout << bob << std::endl;
+
+        bob.incrementGrade();
+    }
+    catch (const std::exception& exception)
+    {
+        std::cout << "Exception: " << exception.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << "=== Grade too high ===" << std::endl;
+
+    try
+    {
+        Bureaucrat invalid("TooHigh", 0);
+        std::cout << invalid << std::endl;
+    }
+    catch (const std::exception& exception)
+    {
+        std::cout << "Exception: " << exception.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << "== Grade too low ===" << std::endl;
+
+    try
+    {
+        Bureaucrat invalid("TooLow", 151);
+        std::cout << invalid << std::endl;
+    }
+    catch (const std::exception& exception)
+    {
+        std::cout << "Exception: " << exception.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << "" << std::endl;
+
+    try
+    {
+        Bureaucrat alice("Alice", 149);
+
+        std::cout << alice << std::endl;
+
+        alice.decrementGrade();
+        std::cout << alice << std::endl;
+
+        alice.decrementGrade();
+    }
+    catch (const std::exception& exception)
+    {
+        std::cout << "Exception: " << exception.what() << std::endl;
+    }
+
     return 0;
 }
