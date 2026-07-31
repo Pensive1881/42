5cee4d0f890dbfcbafea1c11eb219fc81d3f7551
Pensive <lumberjackparade@hotmail.com>
2026-07-31 15:24:55 +0200

    added updated functions to Bureaucrat.hpp for CPP05/ex01

--- CPP_Modules/CPP05/ex01/Bureaucrat.hpp
+++ CPP_Modules/CPP05/ex01/Bureaucrat.hpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <exception>
 
+class Form;
+
 class Bureaucrat
 {
     private:
@@ -23,6 +25,7 @@ class Bureaucrat
 
         void incrementGrade();
         void decrementGrade();
+        void signForm(Form& form);
 
         class GradeTooHighException : public std::exception
         {
