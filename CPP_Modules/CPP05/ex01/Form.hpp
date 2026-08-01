ba009c62655b45ae980ec2e48ab751ad115a2167
Pensive <lumberjackparade@hotmail.com>
2026-08-01 12:48:52 +0200

    finished building Form.hpp for CPP05/ex01

--- CPP_Modules/CPP05/ex01/Form.hpp
+++ CPP_Modules/CPP05/ex01/Form.hpp
@@ -9,7 +9,41 @@ class Bureaucrat;
 
 class Form
 {
+    private:
+        const std::string name;
+        bool isSigned;
+        const int gradeToSign;
+        const int gradeToExecute;
 
+    public:
+        Form();
+        Form(const std::string& name,
+             int gradeToSign,
+             int gradeToExecute);
+        Form(const Form& other);
+        Form& operator=(const Form& other);
+        ~Form();
+
+        const std::string& getName() const;
+        bool getIsSigned() const;
+        int getGradeToSign() const;
+        int getGradeToExecute() const;
+
+        void beSigned(const Bureaucrat& bureaucrat);
+
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
 };
+
+std::ostream& operator<<(std::ostream& output, const Form& form)
 
 #endif
