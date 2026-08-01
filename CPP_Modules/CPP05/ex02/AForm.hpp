747256870ba836e495690ad353a98919655b16c6
Pensive <lumberjackparade@hotmail.com>
2026-08-02 01:38:23 +0200

    Created AForm.hpp for CPP05/ex02

---
+++ CPP_Modules/CPP05/ex02/AForm.hpp
@@ -0,0 +1,22 @@
+#ifndef AFORM_HPP
+#define AFORM_HPP
+
+#include <string>
+#include <iostream>
+#include <exception>
+
+class Bureaucrat;
+
+class AForm
+{
+    private:
+        
+
+    protected:
+
+
+    public:
+
+};
+
+#endif
