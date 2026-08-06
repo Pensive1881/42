37d36cd9d8571579fa9baffc033abfbb5527075b
Pensive <lumberjackparade@hotmail.com>
2026-08-06 18:43:59 +0200

    fixed typos in RobotomyRequestForm.hpp for CPP05/ex02... again...

--- CPP_Modules/CPP05/ex02/RobotomyRequestForm.hpp
+++ CPP_Modules/CPP05/ex02/RobotomyRequestForm.hpp
@@ -15,7 +15,7 @@ class RobotomyRequestForm : public AForm
         RobotomyRequestForm(const RobotomyRequestForm& other);
         RobotomyRequestForm& operator=(
             const RobotomyRequestForm& other);
-        Virtual ~RobotomyRequestForm()l
+        virtual ~RobotomyRequestForm();
 
         void execute(const Bureaucrat& executor) const;
 };
