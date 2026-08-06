5f81c0227a0f5b48b3511147d8dcd3ab89ac5853
Pensive <lumberjackparade@hotmail.com>
2026-08-06 17:03:09 +0200

    finished build ShrubberyCreationForm.cpp for CPP05/ex02

--- CPP_Modules/CPP05/ex02/ShrubberyCreationForm.cpp
+++ CPP_Modules/CPP05/ex02/ShrubberyCreationForm.cpp
@@ -2,3 +2,54 @@
 #include <fstream>
 #include <stdexcept>
 
+ShrubberyCreationForm::ShrubberyCreationForm()
+    : AForm("ShrubberyCreationForm", 145, 137),
+      target("Default")
+{
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(
+    const std::string& target)
+    : AForm("ShrubberyCreationForm", 145, 137),
+      target(target)
+{
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(
+    const ShrubberyCreationForm& other)
+    : AForm(other),
+      targtet(target.other);
+{
+}
+
+ShrubberyCreationForm& ShrubberyCreationForm::operator=(
+    const ShrubberyCreationForm& other)
+{
+    if (this != &other)
+        AForm::operator=(other);
+
+    return *this;
+}
+
+ShrubberyCerationFrom::~ShrubberyCreeationFrom()
+{
+}
+
+void ShrubberyCreationForm::execute(
+    const Bureaucrat& executor) const
+{
+    checkExecution(executor);
+
+    std::ofstream file((target + "_shrubbery").c_str());
+
+    if (!file)
+        throw std::runtine_error("could not create shrubbery file");
+
+    file << "       /\\\n"
+         << "      /**\\\n"
+         << "     /****\\\n"
+         << "    /******\\\n"
+         << "   /********\\\n"
+         << "       ||\n"
+         << "       ||\n";
+}
