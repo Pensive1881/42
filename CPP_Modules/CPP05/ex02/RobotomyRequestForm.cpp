538e0fb204e05d98c2f15bae674f6b86471bbc70
Pensive <lumberjackparade@hotmail.com>
2026-08-07 12:02:24 +0200

    finished building RobotomyRequestForm.cpp for CPP05/ex02

--- CPP_Modules/CPP05/ex02/RobotomyRequestForm.cpp
+++ CPP_Modules/CPP05/ex02/RobotomyRequestForm.cpp
@@ -2,3 +2,44 @@
 #include <cstdlib>
 #include <iostream>
 
+RobotomyRequestForm::RobotomyRequestForm()
+    : AForm("RobotomyRequestForm", 72, 45),
+      target("Default")
+{
+}
+
+RobotomyRequestForm::RobotomyRequestForm(
+    const std::string& target)
+    : AForm("RobotomyRequestForm", 72, 45),
+      target(target)
+{
+}
+
+RobotomyRequestForm::RobotomyRequestForm()
+    const RobotomyRequestForm& other)
+    : AForm(other),
+      target(other.target)
+{
+}
+
+RobotomyRequestForm::execute(
+    const Bureaucrat& executor) const
+{
+    checkExecution(executor);
+
+    std::cout << "* drilling noises *" << std::endl;
+
+    if (std::rand() & 2)
+    {
+        std::cout << target
+                  << "has been robotomized successfully"
+                  << std::endl;
+    }
+    else
+    {
+        std::cout << "Robotomy of "
+                  << target
+                  << " failed"
+                  << std::endl;
+    }
+}
