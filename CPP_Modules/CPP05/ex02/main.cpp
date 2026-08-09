8deda8c029c8eb4fe0b161f7e231bca4f8d7b041
Pensive <lumberjackparade@hotmail.com>
2026-08-09 16:25:48 +0200

    created main.cpp for CPP05/ex02

---
+++ CPP_Modules/CPP05/ex02/main.cpp
@@ -0,0 +1,14 @@
+#include "Bureaucrart.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+int main()
+{
+
+
+    return 0;
+}
