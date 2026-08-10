2e33f24e4e7dcc50ec4d91c4b22587fd34953657
Pensive <lumberjackparade@hotmail.com>
2026-08-10 03:24:21 +0200

    fixed typos in main.cpp for CPP05/ex02

--- CPP_Modules/CPP05/ex02/main.cpp
+++ CPP_Modules/CPP05/ex02/main.cpp
@@ -1,4 +1,4 @@
-#include "Bureaucrart.hpp"
+#include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
@@ -10,35 +10,35 @@ int main()
 {
     std::srand(std::time(NULL));
 
-    Bureaucrat bloss ("Boss", 1);
+    Bureaucrat boss ("Boss", 1);
     Bureaucrat intern("Intern", 150);
 
     ShrubberyCreationForm shrubbery("garden");
     RobotomyRequestForm robotomy("Bender");
-    PresidentialPardonForm pardon("Arthru Dent");
+    PresidentialPardonForm pardon("Arthur Dent");
 
-    std::cout << boss< std::endl;
+    std::cout << boss << std::endl;
     std::cout << intern << std::endl;
 
-    std::cout << "\n--- Unsigned execution ===" << std:endl;
+    std::cout << "\n--- Unsigned execution ---" << std:endl;
     boss.executeForm(shrubbery);
     
     std::cout << "\n--- Grade too low to sign ---" <<std::endl;
     boss.signForm(shrubbery);
 
-    std::cout << "\n--- Shrubbery ===" << std::endl;
+    std::cout << "\n--- Shrubbery ---" << std::endl;
     boss.signForm(robotomy);
     boss.executeForm(robotomy);
 
     std::cout << "\n--- Robotomy ---" << std::endl;
-    boss.signForm(rob otomy);
+    boss.signForm(robotomy);
     boss.executeForm(robotomy);
     boss.executeForm(robotomy);
     boss.executeForm(robotomy);
 
     std::cout << "\n--- Presidential pardon ---" << std::endl;
     boss.signForm(pardon);
-    coss.executeForm(pardon);
+    boss.executeForm(pardon);
 
     return 0;
 }
