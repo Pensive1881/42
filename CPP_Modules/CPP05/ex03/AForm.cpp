17118a97156ce9c460e24e63ac270a79da1ce9ee
Pensive <lumberjackparade@hotmail.com>
2026-08-11 18:52:42 +0200

    copied AForm.cpp from ex02 to CPP05/ex03

---
+++ CPP_Modules/CPP05/ex03/AForm.cpp
@@ -0,0 +1,111 @@
+#include "AForm.hpp"
+#include "Bureaucrat.hpp"
+
+AForm::AForm()
+    : name("Default"),
+      isSigned(false),
+      gradeToSign(150),
+      gradeToExecute(150)
+{
+}
+
+AForm::AForm(const std::string& name,
+             int gradeToSign,
+             int gradeToExecute)
+    : name(name),
+      isSigned(false),
+      gradeToSign(gradeToSign),
+      gradeToExecute(gradeToExecute)
+{
+    if (gradeToSign < 1 || gradeToExecute < 1)
+        throw GradeTooHighException();
+
+    if (gradeToSign > 150 || gradeToExecute > 150)
+        throw GradeTooLowException();
+}
+
+AForm::AForm(const AForm& other)
+    : name(other.name),
+      isSigned(other.isSigned),
+      gradeToSign(other.gradeToSign),
+      gradeToExecute(other.gradeToExecute)
+{
+}
+
+AForm& AForm::operator=(const AForm& other)
+{
+    if (this != &other)
+        isSigned = other.isSigned;
+
+    return *this;
+}
+
+AForm::~AForm()
+{
+}
+
+const std::string& AForm::getName() const
+{
+    return name;
+}
+
+bool AForm::getIsSigned() const
+{
+    return isSigned;
+}
+
+int AForm::getGradeToSign() const
+{
+    return gradeToSign;
+}
+
+int AForm::getGradeToExecute() const
+{
+    return gradeToExecute;
+}
+
+void AForm::beSigned(const Bureaucrat& bureaucrat)
+{
+    if (bureaucrat.getGrade() > gradeToSign)
+        throw GradeTooLowException();
+
+    isSigned = true;
+}
+
+void AForm::checkExecution(const Bureaucrat& executor) const
+{
+    if (!isSigned)
+        throw FormNotSignedException();
+
+    if (executor.getGrade() > gradeToExecute)
+        throw GradeTooLowException();
+}
+
+const char* AForm::GradeTooHighException::what() const throw()
+{
+    return "AForm grade is too high";
+}
+
+const char* AForm::GradeTooLowException::what() const throw()
+{
+    return "AForm grade is too low";
+}
+
+const char* AForm::FormNotSignedException::what() const throw()
+{
+    return "AForm is not signed";
+}
+
+std::ostream& operator<<(std::ostream& output, const AForm& form)
+{
+    output << form.getName()
+           << ", signed: "
+           << (form.getIsSigned() ? "yes" : "no")
+           << ", grade required to sign: "
+           << form.getGradeToSign()
+           << ", grade required to execute: "
+           << form.getGradeToExecute()
+           << ".";
+
+    return output;
+}
