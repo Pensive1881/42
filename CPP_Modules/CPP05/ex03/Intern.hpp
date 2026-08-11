e9f76925f89a8fa0eaeabe8047661269d6aae3a5
Pensive <lumberjackparade@hotmail.com>
2026-08-11 18:47:53 +0200

    finished building Intern.hpp for CPP05/ex03

--- CPP_Modules/CPP05/ex03/Intern.hpp
+++ CPP_Modules/CPP05/ex03/Intern.hpp
@@ -3,6 +3,18 @@
 
 #include <string>
 
+class AForm;
 
+class Intern
+{
+    public:
+        Intern();
+        Intern(const Intern& other);
+        Intern& operator=(const Intern& other);
+        ~Intern();
+
+        AForm* makeForm(const std::string& formName,
+                        const std::string& target) const;
+}
 
 #endif
