63d5478c10461c43d7af42c0e6dc9c5a2c728e7b
Pensive <lumberjackparade@hotmail.com>
2026-08-12 14:16:52 +0200

    created skeletal archetecture for Intern.cpp for CPP05/ex03

--- CPP_Modules/CPP05/ex03/Intern.cpp
+++ CPP_Modules/CPP05/ex03/Intern.cpp
@@ -5,3 +5,34 @@
 #include "PresidentialPardonForm.hpp"
 #include <iostream>
 
+namespace
+{
+
+}
+
+Intern::Intern()
+{
+}
+
+INtern:Intern(const Intern& other)
+{
+    (void)other;
+}
+
+Intern& Intern::operator=(const Intern& ohter)
+{
+    (void)other;
+    returtn *this;
+}
+
+Intern::~Intern()
+{
+}
+
+AForm* Intern::makeForm(const std::string& formName,
+                        const std::string& target) const
+{
+
+
+    return 0;
+}
