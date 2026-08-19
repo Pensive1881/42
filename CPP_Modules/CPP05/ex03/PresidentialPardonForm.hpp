847dd460fe16dd9e056bbfb9238423b39b66ab3b
Pensive <lumberjackparade@hotmail.com>
2026-08-19 13:26:00 +0200

    copied PresidentialPardonForm.hpp from ex02 to CPP05/ex03

---
+++ CPP_Modules/CPP05/ex03/PresidentialPardonForm.hpp
@@ -0,0 +1,22 @@
+#ifndef PRESIDENTIALPARDONFORM_HPP
+#define PRESIDENTIALPARDONFORM_HPP
+
+#include "AForm.hpp"
+#include <string>
+
+class PresidentialPardonForm : public AForm
+{
+    private:
+        const std::string target;
+
+    public:
+        PresidentialPardonForm();
+        PresidentialPardonForm(const std::string& target);
+        PresidentialPardonForm(const PresidentialPardonForm& other);
+        PresidentialPardonForm& operator=(const PresidentialPardonForm& other);
+        virtual ~PresidentialPardonForm();
+
+        void execute(const Bureaucrat& executor) const;
+};
+
+#endif
