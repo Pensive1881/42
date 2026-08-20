1b3bc6ee279fedf4ad9f07e981adcc012bded940
Pensive <lumberjackparade@hotmail.com>
2026-08-20 21:43:44 +0200

    finished building ScalarConverter.hpp for CPP06/ex00

--- CPP_Modules/CPP06/ex00/ScalarConverter.hpp
+++ CPP_Modules/CPP06/ex00/ScalarConverter.hpp
@@ -3,6 +3,16 @@
 
 #include <string>
 
+class ScalarConverter
+{
+    private:
+        ScalarConverter();
+        ScalarConverter(const ScalarConverter& other);
+        ScalarConverter& operator=(const ScalarConverter& other);
+        ~ScalarConverter();
 
+    public:
+        static void convert(const std::string& literal);
+};
 
 #endif
