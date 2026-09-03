ea99424adf6485cf38374ef3908f34ebf86ef06a
Pensive <lumberjackparade@hotmail.com>
2026-09-03 14:41:54 -0700

    continued building ScalarConverter.cpp for CPP06/ex01

--- CPP_Modules/CPP06/ex00/ScalarConverter.cpp
+++ CPP_Modules/CPP06/ex00/ScalarConverter.cpp
@@ -7,3 +7,26 @@
 #include <iostream>
 #include <limits>
 
+namespace
+{
+    
+}
+
+void ScalarConverter::convert(const std::string& literal)
+{
+    double value;
+
+    if (!parseLiteral(literal, value))
+    {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "flat: impossible" << std::endl;
+        std::cout << "double: impossible" << std::endl;
+        return;
+    }
+
+    printChar(value);
+    printInt(value);
+    printFloat(value);
+    printDouble(value);
+}
