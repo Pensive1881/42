ec654c217fda511a8c62268b5b0b768673f45cd1
Pensive <lumberjackparade@hotmail.com>
2026-09-06 21:18:04 -0700

    fixed typo in Serializer.cpp for CPP/ex01

--- CPP_Modules/CPP06/ex01/Serializer.cpp
+++ CPP_Modules/CPP06/ex01/Serializer.cpp
@@ -5,7 +5,7 @@ uintptr_t Serializer::serialize(Data* ptr)
     return reinterpret_cast<uintptr_t>(ptr);
 }
 
-Data* Serializer::deserializer(uintptr_t raw)
+Data* Serializer::deserialize(uintptr_t raw)
 {
     return reinterpret_cast<Data*>(raw);
 }
