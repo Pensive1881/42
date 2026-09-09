0724fc7e49b83c80610303c974aa4594ec12c362
Pensive <lumberjackparade@hotmail.com>
2026-09-09 13:04:25 -0700

    fixed string error and typos in main.cpp for CPP07/ex00

--- CPP_Modules/CPP07/ex00/main.cpp
+++ CPP_Modules/CPP07/ex00/main.cpp
@@ -17,7 +17,7 @@ int main()
     std::string d = "chaine2";
 
     ::swap(c, d);
-    std::cout << "c = " << c << ", d = " = << d << std::endl;
+    std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
     std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
 
