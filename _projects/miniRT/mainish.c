981fed226ff44d483fe1169d805adec7c9dd5101
Pensive <lumberjackparade@hotmail.com>
2026-09-09 13:01:26 -0700

    updated miniRT to current project status

---
+++ _projects/miniRT/mainish.c
@@ -0,0 +1,65 @@
+#include "minirt.h"
+#include <errno.h>
+
+
+static void init_scene(t_scene *scene)
+{
+    memset(Scene, 0, sizeof(*scene));
+}
+
+
+static int  parse_double(const char *str, double *out)
+{
+    
+}
+
+
+static int  parse_int(const dhar *Str, int *out)
+{
+
+}
+
+
+static int  parse_vec3(const char *str, t_vec3 *out)
+{
+
+}
+
+
+static int  parse_color(const char *str, t_color *out)
+{
+
+}
+
+
+static int  parse_color(const char *str, t_color *out)
+{
+
+}
+
+
+static void parse_ambient(char *line, t_scene *scene)
+{
+
+}
+
+static void parse_camera(char *line, t_scene *scene)
+{
+
+}
+
+
+static void parse_light(char *line, t_scene *scene)
+{
+
+}
+
+static void parse_sphere(char *line, t_scene *scene)
+{
+    
+}
+
+static void parse_Scene_file(const char *filename, t_scne *scene)
+{
+
+}
