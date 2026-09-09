eb59a2b6508c15fd1ab0ad4736f930367e2f1d8d
Pensive <lumberjackparade@hotmail.com>
2026-09-09 14:12:19 -0700

    changed naming of inter to iter within iter.hpp for CPP07/ex01

--- CPP_Modules/CPP07/ex01/iter.hpp
+++ CPP_Modules/CPP07/ex01/iter.hpp
@@ -1,17 +1,17 @@
-#ifndef INTER_HPP
-#define INTER_HPP
+#ifndef ITER_HPP
+#define ITER_HPP
 
 #include <cstddef>
 
 template <typename T>
-void inter(T* array, std::size_t length, void (*function)(T&))
+void iter(T* array, std::size_t length, void (*function)(T&))
 {
     for (std::size_t index = 0; index < length; index++)
         function(array[index]);
 }
 
 template <typename T>
-void inter(const T* array, std::size_t length, void (*function)(const T&))
+void iter(const T* array, std::size_t length, void (*function)(const T&))
 {
     for (std::size_t index = 0; index < length; index++)
         function(array[index]);
