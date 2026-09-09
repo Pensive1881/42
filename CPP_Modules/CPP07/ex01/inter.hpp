383ff9de2cf7bdf3bea31e3ec450642c81da3a83
Pensive <lumberjackparade@hotmail.com>
2026-09-09 14:09:37 -0700

    finished building  inter.hpp for CPP07/ex01

--- CPP_Modules/CPP07/ex01/inter.hpp
+++ CPP_Modules/CPP07/ex01/inter.hpp
@@ -3,6 +3,18 @@
 
 #include <cstddef>
 
+template <typename T>
+void inter(T* array, std::size_t length, void (*function)(T&))
+{
+    for (std::size_t index = 0; index < length; index++)
+        function(array[index]);
+}
 
+template <typename T>
+void inter(const T* array, std::size_t length, void (*function)(const T&))
+{
+    for (std::size_t index = 0; index < length; index++)
+        function(array[index]);
+}
 
 #endif
