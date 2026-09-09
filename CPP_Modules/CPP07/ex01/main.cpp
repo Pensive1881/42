85c59ca6b0f5dce738a451e5f34d4685e0efecfd
Pensive <lumberjackparade@hotmail.com>
2026-09-09 15:12:21 -0700

    finished buildingmlx_app.c for miniRT

--- CPP_Modules/CPP07/ex01/main.cpp
+++ CPP_Modules/CPP07/ex01/main.cpp
@@ -3,3 +3,32 @@
 
 #include "iter.hpp"
 
+template <typename T>
+void printValue(const T& value)
+{
+    std::cout << value <<std::endl;
+}
+
+void increment(int& value)
+{
+    value++;
+}
+
+int main()
+{
+    int numbers[] = {1, 2, 3, 4, 5};
+    const std::string words[] = {"Hello", "template", "world"}
+
+    std::cout << "Numbers:" <<std::endl;
+    iter(numbers, 5, printValue<int>);
+
+    iter(numbers, 5, increment);
+
+    std::cout << "Incremented numbers:" << std::endl;
+    iter(numbers, 5, printValue<int>);
+
+    std::cout << "Words:" << std::endl;
+    iter(words, 3, printValue<std::string>);
+
+    return 0;
+}
