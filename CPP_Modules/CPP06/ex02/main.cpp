deb46c7bc5d1f8b2892a8e65f28e38a69b98a1b6
Pensive <lumberjackparade@hotmail.com>
2026-09-09 07:22:12 -0700

    started building  main.cpp for CPP/ex02

--- CPP_Modules/CPP06/ex02/main.cpp
+++ CPP_Modules/CPP06/ex02/main.cpp
@@ -7,3 +7,29 @@
 #include "B.hpp"
 #include "C.hpp"
 
+Base* generate()
+{
+
+}
+
+void identify(Base* p)
+{
+
+}
+
+void identify(Base& p)
+{
+
+}
+
+int main()
+{
+    std::srand(std::time(NULL));
+
+    Base* instance = generater();
+
+    identify(instance);
+    identify(*instance);
+    delete instance;
+    return 0;
+}
