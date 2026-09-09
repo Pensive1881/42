d6b2d42fa8f457ecb30551b2652f738f4a8f2163
Pensive <lumberjackparade@hotmail.com>
2026-09-09 12:51:23 -0700

    finished building whatever for CPP07/ex00

--- CPP_Modules/CPP07/ex00/whatever.hpp
+++ CPP_Modules/CPP07/ex00/whatever.hpp
@@ -1,6 +1,28 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
+template <typename T>
+void swap(T& a, T& b)
+{
+    T temporary = a;
+    a = b;
+    b = temporary;
+}
 
+template <typename T>
+const T& min(const T& a, const T& b)
+{
+    if (a < b)
+        return a;
+    return b;
+}
+
+template <typename T>
+const T& max(const T& a, const T& b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
 
 #endif
