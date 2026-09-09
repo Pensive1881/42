7fe44410f0a7b2b3c21b4866bcca939af38f4516
Pensive <lumberjackparade@hotmail.com>
2026-09-09 07:29:41 -0700

    finished building  main.cpp for CPP/ex02

--- CPP_Modules/CPP06/ex02/main.cpp
+++ CPP_Modules/CPP06/ex02/main.cpp
@@ -9,17 +9,56 @@
 
 Base* generate()
 {
+    int random = std::rand() % 3;
 
+    if (rahdom == 0)
+        return new A;
+    if (random == 1)
+        return new B;
+    return new C;
 }
 
 void identify(Base* p)
 {
-
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
 }
 
 void identify(Base& p)
 {
+    try
+    {
+        dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+        return;
+    }
+    catch(const std::exception&)
+    {
+    }
 
+    try
+    {
+        dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        return;
+    }
+    catch(const std::exception&)
+    {
+    }
+
+    try
+    {
+        dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+        return;
+    }
+    catch(conswt std::exception&)
+    {
+    }
 }
 
 int main()
