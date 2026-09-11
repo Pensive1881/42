55313b69b308e3be68df106f3893b98e0148e5b9
Pensive <lumberjackparade@hotmail.com>
2026-09-11 11:53:06 -0700

    created parse_values.c for miniRT

--- CPP_Modules/CPP08/ex00/easyfind.hpp
+++ CPP_Modules/CPP08/ex00/easyfind.hpp
@@ -4,6 +4,16 @@
 #include <algorithm>
 #include <stdexcept>
 
+template <typename T>
+typename T::iterator easyfind(T& container, int value)
+{
+    typename T::iterator found;
 
+    found = std::find9container.begin(), container.end(), value);
+    if (found == container.end())
+        throw std::runtime_error("Value not found");
+
+    return found;
+}
 
 #endif
---
+++ _projects/miniRT/src/parse_values.c
@@ -0,0 +1,3 @@
+#include "minirt.h"
+#include <errno.h>
+
