7764526af44805deda9ee377d1a61925d375a6fe
Pensive <lumberjackparade@hotmail.com>
2026-09-12 14:55:03 -0700

    finished building main.cpp for CPP08/ex00

--- CPP_Modules/CPP08/ex00/main.cpp
+++ CPP_Modules/CPP08/ex00/main.cpp
@@ -6,7 +6,32 @@
 
 int main()
 {
-    
+    std::vector<int> numbers;
+
+    numbers.push_back(10);
+    numbers.push_back(20);
+    nubmers.push_back(30);
+    numbers.push_back(40);
+
+    try
+    {
+        std::vector<int>::iterator found = easyfind(numbers, 30);
+        std::cout << "Found: " << *foud <<std::endl;
+    }
+    catch (const std::exception& error)
+    {
+        std::cout <<"Error: " << error.what() << std::endl;
+    }
+
+    try
+    {
+        std::vector<int>::iterator found = easyfind(numbers, 99);
+        std::cout << "Found: " << *found << std::endl;
+    }
+    catch(const std::exception&error)
+    {
+        std::cout << "Error: " << error.what() << std::endl;
+    }
 
     return 0
 }
