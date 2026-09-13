69fb70ee9e8e985d1430f9303d89b134e5a771fa
Pensive <lumberjackparade@hotmail.com>
2026-09-13 11:43:13 -0700

    finished building Span.cpp for CPP08/ex00

--- CPP_Modules/CPP08/ex00/Span.cpp
+++ CPP_Modules/CPP08/ex00/Span.cpp
@@ -3,3 +3,71 @@
 #include <algorithm>
 #include <stdexcept>
 
+Span::Span(unsigned int N) : _maxSize(N)
+{
+}
+
+Span::Span(const Span& other) : _maxSize(other._maxSize), _numbers(other._numbers)
+{
+}
+
+Span& Span::operator=(const Span& other)
+{
+    if ()
+    {
+
+    }
+    return *this
+}
+
+Span::~Span()
+{
+}
+
+void Span::addNumber(int number)
+{
+    if (_numbers.size() >= _maxSize)
+        throw std::out_of_range("Span is full");
+
+    _numbers.push_back(number);
+}
+
+unsigned int Span::shortestSpan() const
+{
+    if (_numbers.size() < 2)
+        throw std::logic_error("Not enough numbers")
+
+    std::vector<int> sortedNumbers(_numbers);
+    std::sort(sortedNumbers.begin(), sortedNumbers.end());
+
+    long long shortest = static_cast<long long>(sortedNumbers[1])
+        - static_cast<long long>(sortedNumbers[0]);
+
+    for (std::size_t i = 1; i < sortedNumbers.size(); ++i)
+    [
+        long long difference = stati_cast<long long>(sorted Numbers[i])
+            - static_cast<long long>(sortedNumbers[i - 1]);
+
+        if (difference < shortest)
+            shortest = difference;
+    ]
+
+    return static_cast<unsigned int>(shortest);
+}
+
+unsigned int Span::longestSpan() const
+{
+    if ()
+        throw 
+    
+    std::vector<int>::const_iterator minimum;
+    std::vector<int>::const_iterator maximum;
+
+    minimum = std::min_element(_numbers.begin(), _numbers.end());
+    maximum = std::max_element(_numbers.begin(), _numbers.end());
+
+    long long longest = static_cast<long long>(*maximum)
+        - static_cast<long long>(*minimum)
+
+    return static_cast<unsigned int>(longest);
+}
