b58b8a9c6da8277b540510ba7f51101c1401c22a
Pensive <lumberjackparade@hotmail.com>
2026-09-13 13:02:20 -0700

    finished building main.cpp for CPP08/ex01

--- CPP_Modules/CPP08/ex01/main.cpp
+++ CPP_Modules/CPP08/ex01/main.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 #include "Span.hpp"
 
+int main()
+{
+    Span sp(5);
+
+    sp.add_number(6);
+    sp.add_number(3);
+    sp.add_number(17);
+    sp.add_number(9);
+    sp.add_number(11);
+
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
+
+    return 0;
+}
