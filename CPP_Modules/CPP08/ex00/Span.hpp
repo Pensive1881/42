2e0dde0bdf462c7944e2dcb3e5589764f1fb0282
Pensive <lumberjackparade@hotmail.com>
2026-09-12 23:06:34 -0700

    fixed typos in Span.hpp for CPP08/ex00

--- CPP_Modules/CPP08/ex00/Span.hpp
+++ CPP_Modules/CPP08/ex00/Span.hpp
@@ -9,7 +9,7 @@ class Span
 {
     private:
         unsigned int _maxSize;
-        std::vector<int< _numbers;
+        std::vector<int> _numbers;
 
     public:
         Span(unsigned int N);
@@ -20,18 +20,18 @@ class Span
         void addNumber(int number);
 
         template <typename InputIterator>
-        void addNumber(InputIterator first, last);
+        void addNumber(InputIterator first, InputIterator last)
         {
             std::size_t count = std::distance(first, last);
 
             if (_numbers.size() + count > _maxSize)
                 throw std::out_of_range("Span is full");
 
-            _numbers.insert(_numbers.end(), first, last)
+            _numbers.insert(_numbers.end(), first, last);
         }
 
-        uinsigned int shortestSpan() const;
-        unsigned in longestSpan() const;
-}
+        unsigned int shortestSpan() const;
+        unsigned int longestSpan() const;
+};
 
 #endif
