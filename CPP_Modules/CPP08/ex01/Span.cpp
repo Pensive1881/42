ad0947ea24aa6733ad2a0a8e0b3243fb19c59e0d
Pensive <lumberjackparade@hotmail.com>
2026-09-14 09:52:07 -0700

    finished building Span.cpp for CPP08/ex01

--- CPP_Modules/CPP08/ex01/Span.cpp
+++ CPP_Modules/CPP08/ex01/Span.cpp
@@ -3,3 +3,38 @@
 #include <algorithm>
 #include <stdexcept>
 
+class Span
+{
+    private:
+        unsigned int        _maxSize;
+        std::vector<int>    _numbers;
+    
+    public:
+        Span(unsigned int maxSize);
+        Span(const Span& other);
+        Span& operator=(const Span& other);
+        ~Span();
+
+        void addNumber(int number);
+
+        template <typename InputIterator>
+        void addNumber(InputIterator first, InputIterator last)
+        {
+            std::vector<int> newNumbers(first, last);
+
+            if (newNumbers.size() > static_cast<std::size_t>(_maxSize) - _numbers.size())
+            {
+                threw strd::out_of_range("Span is full");
+            }
+
+            _numbers.insert(_numbers.end(),
+                            newNumbers.begin(),
+                            newNumbers.end()
+            );
+        }
+
+        unsigned int shortestSpan() const;
+        unsigned int longestSpan() const;
+};
+
+#endif
