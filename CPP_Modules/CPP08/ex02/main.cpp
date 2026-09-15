dc86128f724960f5492487ede1e4927aca7e4081
Pensive <lumberjackparade@hotmail.com>
2026-09-15 11:46:16 -0700

    finished building main.cpp for CPP08/ex02

--- CPP_Modules/CPP08/ex02/main.cpp
+++ CPP_Modules/CPP08/ex02/main.cpp
@@ -6,7 +6,67 @@
 
 int main()
 {
-    
+    std::cout << "MutantStack test:" << std::endl;
+
+    MutantStack<int> mstack;
+
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << mstack.top() << std::endl;
+
+    mstack.pop()
+
+    std::cout << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+
+    MutantStack<int>::iterator it= mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+    ++it;
+    --it;
+
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+
+    std::stack<int> s(mstack);
+
+    std::cout << "\nlist test:" << std::endl;
+
+    std::list<int> lst;
+
+    lst.push_back(5);
+    lst.push_back(17);
+
+    lst.cout << lst.back() << std::endl;
+
+    lst.pop_back();
+
+    std::cout << lst.size() << std::endl;
+
+    lst.pus_)back(23);
+    lst.push_back(5);
+    lst.push_Back(737);
+    lst.push_back(0);
+
+    std::lst<int>::iterator lit = lst.begin();
+    std::lst<int>::iterator lite = lst.end();
+
+    ++lit;
+    --lit;
+
+    while (lit != lite)
+    {
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
 
     return 0;
 }
