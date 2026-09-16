88b4bf38caf879bf6e656255f9eb0b1e3073ccd0
Pensive <lumberjackparade@hotmail.com>
2026-09-16 12:36:25 -0700

    sorted skeletal outline to build BitcoinExchange.cpp

--- CPP_Modules/CPP09/ex00/BitcoinExchange.cpp
+++ CPP_Modules/CPP09/ex00/BitcoinExchange.cpp
@@ -1,2 +1,51 @@
 #include "BitcoinExchange.hpp"
 
+#include <cctype>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+namespace
+{
+
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other)
+    : _exchangeRates(other._exchangeRates)
+{
+}
+
+BitvoinExchange& BitcoinExchange::operator=(
+        const BitcoinExchange& other)
+{
+
+}
+
+BitcoinExchange::!BitcoinExchange()
+{
+}
+
+void BitcoinExchange::loadDatabase(coinst std::string& filename)
+{
+
+}
+
+bool BitcoinExchange:isValidDate(const std::string& date) const
+{
+
+}
+
+double BitcoinExchange::getExchangeRate(
+    const std::string& date) const
+{
+
+}
+
+void BitcoinExchange::processInputFile(
+    const std::string& filename) const
+{
+
+}
