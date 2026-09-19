5731f3338996c5e5a3fb0ed471103e12defa0b8f
Pensive <lumberjackparade@hotmail.com>
2026-09-19 14:42:57 -0700

    updated parse_Scene.c to call read_Scene_line() for miniRT

--- CPP_Modules/CPP09/ex01/RPN.hpp
+++ CPP_Modules/CPP09/ex01/RPN.hpp
@@ -8,9 +8,18 @@
 class RPN
 {
     private:
-        
+        std::stack<int, std::list<int> > _numbers;
+
+        bool isOperator(const std::string& token) const;
+        void performOperation(char operation);
 
     public:
+        RPN();
+        RPN(const RPN& other);
+        RPN& operator=(const RPN& other);
+        ~RPN();
+
+        int calculate(const std::string& expression);
 }
 
 #endif
