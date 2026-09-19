422e01525d4acaa0d955b2f19e79c2b9080b5569
Pensive <lumberjackparade@hotmail.com>
2026-09-19 13:41:23 -0700

    finished building main.cpp for CPP09/ex00

--- CPP_Modules/CPP09/ex00/main.cpp
+++ CPP_Modules/CPP09/ex00/main.cpp
@@ -5,5 +5,25 @@
 
 int main(int ac, char** av)
 {
-    
+    if (ac != 2)
+    {
+        std::cout << "Error/: could not open file."
+                  << std::endl;
+        return 1;
+    }
+
+    try
+    {
+        BitcoingExchange exchange;
+
+        exchange.loadDatabase("data.csv");
+        exchange.processInputFile(av[1])
+    }
+    catch (const std::exception& exception)
+    {
+        std::cout << exception.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
