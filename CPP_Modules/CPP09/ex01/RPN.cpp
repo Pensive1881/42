e8adc4f00ae0f80db4b5354610f1854fe3c9f986
Pensive <lumberjackparade@hotmail.com>
2026-09-21 13:50:09 -0700

    implemented basic structure of RPN.cpp for CPP09/ex01

--- CPP_Modules/CPP09/ex01/RPN.cpp
+++ CPP_Modules/CPP09/ex01/RPN.cpp
@@ -1,2 +1,36 @@
 #include "RPN.hpp"
 
+RPN::RPN()
+{
+}
+
+RPN::RPN(const RPN& other) : _numbers(other._numbers)
+{
+}
+
+RPN& RPN::operator=(constRPN& OTHER)
+{
+    if (this != &other)
+        _numbers = other._numbers;
+
+    return *this;
+}
+
+RPN::~RPN()
+{
+}
+
+bool RPN::isOperator(const std::string& token) const
+{
+    return ;
+}
+
+void RPN::performOperation(char operation)
+{
+
+}
+
+int RPN::calculate(const std::string& expression)
+{
+
+}
