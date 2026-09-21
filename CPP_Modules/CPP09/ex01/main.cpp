95e612e79d89835af21d2a2f3f998468a4bd2514
Pensive <lumberjackparade@hotmail.com>
2026-09-21 15:40:44 -0700

    finished building main.cpp for CPP09/ex01

--- CPP_Modules/CPP09/ex01/main.cpp
+++ CPP_Modules/CPP09/ex01/main.cpp
@@ -1,2 +1,24 @@
 #include "RPN.hpp"
 
+int main(int ac, char **av)
+{
+    if (ac != 2)
+    {
+        std::cerr << "Error" << std::endl;
+        return 1;
+    }
+
+    try
+    {
+        RPN rpn;
+
+        std::cout << rpn.calculate(av[1]) << std::endl;
+    }
+    catch (const std::exception& exception)
+    {
+        std::cerr << exception.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
