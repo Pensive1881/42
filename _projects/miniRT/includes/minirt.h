71ad701bc4815ad4f98a15610971e4fe24df62de
Pensive <lumberjackparade@hotmail.com>
2026-09-22 16:47:30 -0700

    added parse_plane() & parse_cylinder() in minirt.h for miniRT

--- _projects/miniRT/includes/minirt.h
+++ _projects/miniRT/includes/minirt.h
@@ -11,7 +11,61 @@
 
 # define WINDOW_WIDTH 800
 # define WINDOW_HEIGHT 600
-# define ESC_KEY 53
+# define ESC_KEY 65307
+
+typedef struct s_vec3
+{
+    double  x;
+    double  y;
+    double  z;
+}   t_vec3;
+
+typedef	enum	e_type
+{
+	SPHERE,
+	PLANE,
+	CYLINDER
+}	t_type;
+
+typedef struct s_color
+{
+    int r;
+    int g;
+    int b;
+}   t_color;
+
+typedef struct s_sphere
+{
+    t_vec3  center;
+    double  diameter;
+    t_color color;
+}   t_sphere;
+
+typedef	struct	s_plane
+{
+	t_vec3	point; //any point that lies on the plane
+	t_vec3	normal;
+}	t_plane;
+typedef	struct s_cylinder
+{
+	t_vec3	center;
+	t_vec3	axis;
+	double	radius;
+	double	height;
+}	t_cylinder;
+
+typedef	struct s_object
+{
+	t_type	type;//sphere, plane, or cylinder
+	union
+    {
+		t_sphere	sp;
+		t_plane	pl;
+		t_cylinder	cy;
+	};
+	t_color	color;
+	struct	s_object	*next;
+}	t_object;
 
 typedef struct s_mlx
 {
@@ -25,20 +79,6 @@ typedef struct s_mlx
     int     width;
     int     height;
 }   t_mlx;
-
-typedef struct s_vec3
-{
-    double  x;
-    double  y;
-    double  z;
-}   t_vec3;
-
-typedef struct s_color
-{
-    int r;
-    int g;
-    int b;
-}   t_color;
 
 typedef struct s_ambient
 {
@@ -62,19 +102,13 @@ typedef struct s_light
     t_color color;
 }   t_light;
 
-typedef struct s_sphere
-{
-    t_vec3  center;
-    double  diameter;
-    t_color color;
-}   t_sphere;
-
 typedef struct s_scene
 {
     t_ambient   ambient;
     t_camera    camera;
     t_light     light;
     t_sphere    sphere;
+    t_object	*objects; //list of all shapes in the scene
     int         has_ambient;
     int         has_camera;
     int         has_light;
@@ -89,6 +123,13 @@ typedef	struct	s_ray
 	t_vec3	dir; //always normalised
 }	t_ray;
 
+typedef	struct	s_hit
+{
+	double	t;//distance along the ray to the hit
+	t_vec3	point;//exact 3D coordinate of the hit
+	t_vec3	normal;//surface direction at the hit point
+	t_object	*object;//which object was hit(NULL = miss)
+}	t_hit;
 //parse_values.c
 int     parse_double(const char *str, double *out);
 int     parse_int(const char *str, int *out);
@@ -100,6 +141,8 @@ int     parse_ambient(char *line, t_scene *scene);
 int     parse_camera(char *line, t_scene *scene);
 int     parse_light(char *line, t_scene *scene);
 int     parse_sphere(char *line, t_scene *scene);
+int     parse_plane(char *line, t_scene *scene);
+int     parse_cylinder(char *line, t_scene *scene);
 
 // parse_scene.c
 int     parse_scene(const char *filename, t_scene *scene);
@@ -136,4 +179,17 @@ void    mlx_put_pixel(t_mlx *mlx, int x, int y, int color);
 int     mlx_close(t_mlx *mlx);
 int     handle_key(int keycode, t_mlx *mlx);
 
+//lighting.c
+t_vec3	shade(t_scene *scene, t_color obj_colour, t_vec3 hit_point, t_vec3 normal);
+t_vec3	calc_ambient(t_scene *scene, t_color obj_colour);
+t_vec3	calc_diffuse(t_scene *scene, t_color obj_colour, t_vec3 hit_point, t_vec3 normal);
+
+//objects.c
+t_object    *create_object(t_type type, t_color color);
+void        add_object(t_scene *scene, t_object *obj);
+void        free_objects(t_object *objects);
+
+//plane_stub.c
+double	intersect_plane(t_ray ray, t_plane *pl);
+t_vec3	plane_normal(t_plane *pl);
 #endif
