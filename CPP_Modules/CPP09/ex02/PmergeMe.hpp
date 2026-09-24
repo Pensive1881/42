ca3b66e9ee4d138a3da0773d8de46b7aab1bc247
Pensive <lumberjackparade@hotmail.com>
2026-09-23 17:26:56 -0700

    finished building PmergeMe.hpp for CPP09/ex02

--- CPP_Modules/CPP09/ex02/PmergeMe.hpp
+++ CPP_Modules/CPP09/ex02/PmergeMe.hpp
@@ -1,6 +1,33 @@
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 
+class PmergeMe
+{
+    private:
+        std::vector<int> _vector;
+        std::deque<int> _deque;
+        double _vectorTime;
+        double _dequeTime;
 
+        int parse_number(const std::string& argument) const;
+
+        void fillVector(int ac, char** av);
+        void fillDeque(int ac, char** av);
+
+        void fordJohnsonVector(Std::vector<int>& sequence);
+        void fordJohnsonVector(std::queue<int>& sequence);
+
+        void displaySequence(
+            const std::string& label,
+            const std::vector<int>& sequence) const;
+
+    public:
+        PmergeMe();
+        PmergeMe(const PmergeMe& other);
+        PmergeMe& operator=(const PmergeMe& other);
+        ~PmergeMe();
+
+        void process(int ac, char** av);
+};
 
 #endif
